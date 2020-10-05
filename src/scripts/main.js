893dfc1ad1248595c222d6de94a7ec37fe4b8433
Dima Diachenko <zarg129@gmail.com>
2020-10-05 10:44:35 +0300

    Some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,127 +2,64 @@
 
 const bodyOfTable = document.querySelector('tbody');
 const headOfTable = document.querySelector('thead');
-const form = document.createElement('form');
-let sortDirection = 'asc';
-let clickCell = 0;
+let sortedColumn;
 
-function sorting() {
-  headOfTable.addEventListener('click', (event) => {
-    const workers = [...bodyOfTable.children];
-    const sortBy = event.target.innerText;
-
-    switch (sortBy) {
-      case 'Name':
-        if (clickCell === 0 && sortDirection !== 'asc') {
-          workers.sort((a, b) => {
-            return b.children[0].innerText
-              .localeCompare(a.children[0].innerText);
-          });
-          sortDirection = 'asc';
-          clickCell = event.target.cellIndex;
-        } else {
-          workers.sort((a, b) => {
-            return a.children[0].innerText
-              .localeCompare(b.children[0].innerText);
-          });
-          sortDirection = 'desc';
-          clickCell = event.target.cellIndex;
-        }
-
-        break;
-      case 'Position':
-        if (clickCell === 1 && sortDirection !== 'asc') {
-          [...bodyOfTable.children].sort((a, b) => {
-            return b.children[1].innerText
-              .localeCompare(a.children[1].innerText);
-          });
-          sortDirection = 'asc';
-          clickCell = event.target.cellIndex;
-        } else {
-          workers.sort((a, b) => {
-            return a.children[1].innerText
-              .localeCompare(b.children[1].innerText);
-          });
-          sortDirection = 'desc';
-          clickCell = event.target.cellIndex;
-        }
-
-        break;
-      case 'Office':
-        if (clickCell === 2 && sortDirection !== 'asc') {
-          [...bodyOfTable.children].sort((a, b) => {
-            return b.children[2].innerText
-              .localeCompare(a.children[2].innerText);
-          });
-          sortDirection = 'asc';
-          clickCell = event.target.cellIndex;
-        } else {
-          [...bodyOfTable.children].sort((a, b) => {
-            return a.children[2].innerText
-              .localeCompare(b.children[2].innerText);
-          });
-          sortDirection = 'desc';
-          clickCell = event.target.cellIndex;
-        }
-
-        break;
-      case 'Age':
-        if (clickCell === 3 && sortDirection !== 'asc') {
-          workers.sort((a, b) => {
-            return +b.children[3].innerText - +a.children[3].innerText;
-          });
-          sortDirection = 'asc';
-          clickCell = event.target.cellIndex;
-        } else {
-          workers.sort((a, b) => {
-            return +a.children[3].innerText - +b.children[3].innerText;
-          });
-          sortDirection = 'desc';
-          clickCell = event.target.cellIndex;
-        }
-
-        break;
-      case 'Salary':
-        if (clickCell === 4 && sortDirection !== 'asc') {
-          workers.sort((a, b) => {
-            return (+b.children[4].innerText.slice(1).split(',').join('')
-              - +(a.children[4].innerText.slice(1).split(',').join('')));
-          });
-          sortDirection = 'asc';
-          clickCell = event.target.cellIndex;
-        } else {
-          workers.sort((a, b) => {
-            return (+a.children[4].innerText.slice(1).split(',').join('')
-              - +(b.children[4].innerText.slice(1).split(',').join('')));
-          });
-          sortDirection = 'desc';
-          clickCell = event.target.cellIndex;
-        }
-
-        break;
-    }
-    bodyOfTable.append(...workers);
-  });
-}
-sorting();
-
-function selectWorker() {
+function sortTable(event) {
+  const columnIndex = event.target.cellIndex;
   const workers = [...bodyOfTable.children];
 
-  for (let i = 0; i < workers.length; i++) {
-    workers[i].addEventListener('click', (event) => {
-      for (let j = 0; j < workers.length; j++) {
-        workers[j].classList.remove('active');
+  if (event.target.textContent === sortedColumn) {
+    workers.reverse();
+  } else {
+    sortedColumn = event.target.textContent;
+
+    workers.sort((a, b) => {
+      if (!toNumber(a.cells[columnIndex].innerText)) {
+        return a.cells[columnIndex].innerText
+          .localeCompare(b.cells[columnIndex].innerText);
       }
-      event.target.parentElement.classList.add('active');
+
+      return toNumber(a.cells[columnIndex].innerText)
+    - toNumber(b.cells[columnIndex].innerText);
     });
-  };
+  }
+
+  bodyOfTable.append(...workers);
+}
+
+function toNumber(value) {
+  if (+value) {
+    return +value;
+  } else {
+    return +value.slice(1).split(',').join('');
+  }
+};
+
+headOfTable.addEventListener('click', sortTable);
+
+function selectWorker() {
+  const rows = [...bodyOfTable.children];
+
+  bodyOfTable.addEventListener('click', (event) => {
+    const clickedPoint = event.target.parentNode;
+
+    rows.map(worker => worker.classList.remove('active'));
+    clickedPoint.classList.add('active');
+  });
 }
 selectWorker();
 
 function CreateForm() {
+  const form = document.createElement('form');
+  const offices = ['Tokyo', 'Singapore', 'London',
+    'New York', 'Edinburgh', 'San Francisco'];
+  let officeOption = '';
+
+  for (const office in offices) {
+    officeOption += `<option value="${office}">${offices[office]}</option>`;
+  }
+
   form.classList = 'new-employee-form';
-  document.body.append(form);
 
   form.insertAdjacentHTML('afterbegin', `
     <label> Name:
@@ -143,12 +80,7 @@ function CreateForm() {
     
     <label> Office:
       <select name="office" id = "office">
-        <option value="Tokyo">Tokyo</option>
-        <option value="Singapore">Singapore</option>
-        <option value="London">London</option>
-        <option value="New York">New York</option>
-        <option value="Edinburgh">Edinburgh</option>
-        <option value="San Francisco">San Francisco</option>
+        ${officeOption}
       </select>
     </label>
     
@@ -171,8 +103,70 @@ function CreateForm() {
         Save to table
     </button>
   `);
-}
 
+  function employerAddAndValidator() {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const tr = document.createElement('tr');
+      const column = ['name', 'position', 'office', 'age', 'salary'];
+
+      for (let i = 0; i < column.length; i++) {
+        const td = document.createElement('td');
+        let salary = document.querySelector('input[name=salary]').value;
+        const age = document.querySelector('input[name=age]').value;
+        const name = document.querySelector('input[name=name]').value;
+
+        td.textContent = document.getElementById(column[i]).value;
+        tr.append(td);
+
+        if (column[i] === 'salary') {
+          salary = salary.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+          td.textContent = `$${salary}`;
+        }
+
+        if (name.length < 4) {
+          pushNotification('10px', '10px', 'Error!',
+            `Name can't be less then 4 letters`,
+            `error`
+          );
+
+          return;
+        };
+
+        if (age < 18) {
+          pushNotification('10px', '10px', 'Error!',
+            `Age can't be less then 18`,
+            `error`
+          );
+
+          return;
+        };
+
+        if (age > 90) {
+          pushNotification('10px', '10px', 'Error!',
+            `Age can't be bigger then 90`,
+            `error`
+          );
+
+          return;
+        }
+      }
+
+      bodyOfTable.append(tr);
+      sortedColumn = null;
+
+      pushNotification('10px', '10px', 'Congratulations',
+        `You are the new employer`,
+        `success`
+      );
+    });
+  }
+  employerAddAndValidator();
+
+  document.body.append(form);
+}
 CreateForm();
 
 const pushNotification = (top, right, title, description, type) => {
@@ -200,66 +194,6 @@ const pushNotification = (top, right, title, description, type) => {
   }, 5000);
 };
 
-function employerAddValidator() {
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const tr = document.createElement('tr');
-    const column = ['name', 'position', 'office', 'age', 'salary'];
-
-    for (let i = 0; i < column.length; i++) {
-      const td = document.createElement('td');
-      let salary = document.querySelector('input[name=salary]').value;
-      const age = document.querySelector('input[name=age]').value;
-      const name = document.querySelector('input[name=name]').value;
-
-      td.textContent = document.getElementById(column[i]).value;
-      tr.append(td);
-
-      if (column[i] === 'salary') {
-        salary = salary.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-
-        td.textContent = `$${salary}`;
-      }
-
-      if (name.length < 4) {
-        pushNotification('10px', '10px', 'Error!',
-          `Name can't be less then 4 letters`,
-          `error`
-        );
-
-        return;
-      };
-
-      if (age < 18) {
-        pushNotification('10px', '10px', 'Error!',
-          `Age can't be less then 18`,
-          `error`
-        );
-
-        return;
-      };
-
-      if (age > 90) {
-        pushNotification('10px', '10px', 'Error!',
-          `Age can't be bigger then 90`,
-          `error`
-        );
-
-        return;
-      }
-    }
-
-    bodyOfTable.append(tr);
-
-    pushNotification('10px', '10px', 'Congratulations',
-      `You are the new employer`,
-      `success`
-    );
-  });
-}
-employerAddValidator();
-
 function saveValue(td, input, defaultValue) {
   if (!input.value) {
     td.textContent = defaultValue;
@@ -272,22 +206,22 @@ function saveValue(td, input, defaultValue) {
 function editTable() {
   bodyOfTable.addEventListener('dblclick', (event) => {
     const input = document.createElement('input');
-    const cell = event.target;
-    const defaultValue = cell.innerText;
+    const clickedCell = event.target;
+    const defaultValue = clickedCell.innerText;
 
     input.classList.add('cell-input');
     input.value = defaultValue;
-    cell.textContent = '';
-    cell.append(input);
+    clickedCell.textContent = '';
+    clickedCell.append(input);
     input.focus();
 
     input.addEventListener('blur', () =>
-      saveValue(cell, input, defaultValue)
+      saveValue(clickedCell, input, defaultValue)
     );
 
     input.addEventListener('keydown', (e) => {
       if (e.code === 'Enter') {
-        saveValue(cell, input, defaultValue);
+        saveValue(clickedCell, input, defaultValue);
       }
     }
     );
