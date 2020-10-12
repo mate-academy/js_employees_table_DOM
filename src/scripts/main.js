510db4f9436310e496fb145b1f99da3d47bbb7d5
Bohdan Polienko <mignatov756@gmail.com>
2020-10-12 20:34:56 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,66 +10,34 @@ const employeess = [...body.rows];
 
 head.addEventListener('click', (event) => {
   const target = event.target;
+  const columnIndex = event.target.cellIndex;
 
-  switch (target.textContent) {
-    case 'Name':
+  function sortBySalary() {
+    employeess.sort((a, b) => {
+      return Number(a.children[4].textContent.replace(/[$,]/g, ''))
+        - Number(b.children[4].textContent.replace(/[$,]/g, ''));
+    });
+  }
+
+  switch (true) {
+    case columnIndex < 4:
       if (secondClick !== target.textContent) {
         secondClick = target.textContent;
 
         employeess.sort((a, b) => {
-          return (a.children[0].textContent
-            .localeCompare(b.children[0].textContent));
-        });
-      } else {
-        employeess.reverse();
-      }
-      break;
-    case 'Position':
-      if (secondClick !== target.textContent) {
-        secondClick = target.textContent;
-
-        employeess.sort((a, b) => {
-          return (a.children[1].textContent
-            .localeCompare(b.children[1].textContent));
-        });
-      } else {
-        employeess.reverse();
-      }
-      break;
-    case 'Office':
-      if (secondClick !== target.textContent) {
-        secondClick = target.textContent;
-
-        employeess.sort((a, b) => {
-          return (a.children[2].textContent
-            .localeCompare(b.children[2].textContent));
+          return a.cells[columnIndex].innerText
+            .localeCompare(b.cells[columnIndex].innerText);
         });
       } else {
         employeess.reverse();
       }
       break;
 
-    case 'Age':
+    case columnIndex === 4:
       if (secondClick !== target.textContent) {
         secondClick = target.textContent;
 
-        employeess.sort((a, b) => {
-          return (a.children[3].textContent
-            .localeCompare(b.children[3].textContent));
-        });
-      } else {
-        employeess.reverse();
-      }
-      break;
-
-    case 'Salary':
-      if (secondClick !== target.textContent) {
-        secondClick = target.textContent;
-
-        employeess.sort((a, b) => {
-          return Number(a.children[4].textContent.replace(/[$,]/g, ''))
-            - Number(b.children[4].textContent.replace(/[$,]/g, ''));
-        });
+        sortBySalary();
       } else {
         employeess.reverse();
       }
@@ -88,27 +56,24 @@ body.addEventListener('click', (event) => {
 
 form.classList.add('new-employee-form');
 
+const citys = ['Tokyo', 'Singapore', 'London', 'New York',
+  'Edinburgh', 'San Francisco'];
+
 form.insertAdjacentHTML(`afterbegin`,
   `<label>Name: <input name="name" type="text" required></label>
   <label>Position: <input name="position" type="text" required></label>
   <label>Office:
     <select name="office">
-      <option value="Tokyo">Tokyo</option>
-      <option value="Singapore">Singapore</option>
-      <option value="London">London</option>
-      <option value="New York">New York</option>
-      <option value="Edinburgh">Edinburgh</option>
-      <option value="San Francisco">San Francisco</option>
+      ${citys.map((city) => `<option>${city}</option>`)}
     </select>
   </label>
   <label>Age: <input name="age" type="number" required></label>
   <label>Salary: <input name="salary" type="number" required></label>
-  <button type="submit">Save to table</button>`);
+  <button type="submit" onsubmit="return validate()">Save to table</button>`);
 
 document.body.append(form);
 
 function createNotification(title, description, type, color) {
-  div.style.cssText = (`top: 470px; right: 270px`);
   div.style.backgroundColor = color;
   div.classList = `notification ${type}`;
   div.innerHTML = `<h2>${title}</h2>` + `<p>${description}</p>`;
@@ -118,9 +83,12 @@ function createNotification(title, description, type, color) {
 
 function addNewEmployee() {
   const employee = document.createElement('tr');
-  // eslint-disable-next-line no-undef
-  const data = new FormData(form);
-  const { name, position, office, age, salary } = Object.fromEntries(data);
+
+  const name = form.name.value;
+  const position = form.position.value;
+  const office = form.office.value;
+  const age = form.age.value;
+  const salary = form.salary.value;
 
   employee.insertAdjacentHTML('afterbegin', `
     <td>${name}</td>
@@ -130,35 +98,41 @@ function addNewEmployee() {
     <td>$${(+salary).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</td>
   `);
 
-  employeess.push(...[employee]);
+  employeess.push(employee);
   body.append(employee);
 }
 
-function notification() {
-  // eslint-disable-next-line no-undef
-  const data = new FormData(form);
-
-  const { name, age } = Object.fromEntries(data);
+function isValid() {
+  const name = form.name.value;
+  const age = form.age.value;
 
   switch (true) {
     case (name.length < 4):
       createNotification('Error!'
-        , `Name must be more than 4 characters`, 'error', 'red');
+        , `Name must be more than 4 characters`
+        , 'error'
+        , 'red');
 
       return false;
     case (age < 18):
       createNotification('Error!'
-        , `Age can't be less then 18`, 'error', 'red');
+        , `Age can't be less then 18`
+        , 'error'
+        , 'red');
 
       return false;
     case (age > 90):
       createNotification('Error!'
-        , `Age can't be greater then 90`, 'error', 'red');
+        , `Age can't be greater then 90`
+        , 'error'
+        , 'red');
 
       return false;
     default:
       createNotification('Congratulations!'
-        , 'We now have a new employee', 'success', 'green');
+        , 'We now have a new employee'
+        , 'success'
+        , 'green');
 
       return true;
   };
@@ -169,11 +143,11 @@ form.addEventListener('submit', (event) => {
 
   const target = event.target;
 
-  if (!notification(target)) {
+  if (!isValid()) {
     return;
   }
 
-  addNewEmployee(target);
+  addNewEmployee();
   target.reset();
 });
 
