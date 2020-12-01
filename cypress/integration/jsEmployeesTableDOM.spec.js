d2ee0f8c2635e8aa2b2d612f1dcb639503a6530e
Ihor Oliinyk <i.oliinyk@mate.academy>
2020-12-01 15:03:47 +0200

    refactor

--- cypress/integration/jsEmployeesTableDOM.spec.js
+++ cypress/integration/jsEmployeesTableDOM.spec.js
@@ -10,11 +10,67 @@ Cypress.Commands.add('fillNewEmployeeForm',
     cy.get('button').contains('Save to table').click();
   });
 
-Cypress.Commands.add('checkData',
+Cypress.Commands.add('checkDataDoesntExist',
   (fullName, salary) => {
     cy.get('tbody').contains(fullName).should('not.exist');
     cy.get('tbody').contains(salary).should('not.exist');
   });
+
+Cypress.Commands.add('hasData', (inputName, compareValue) => {
+  cy.contains(inputName).parent().should(($column) => {
+    const array = [...$column].map((td) => td.innerText.split('\t'));
+
+    for (let i = 0; i < 5; i++) {
+      expect(array[0][i]).to.equal(compareValue[i]);
+    }
+  });
+});
+
+Cypress.Commands.add('isSortedASC', (columnName, columnNumber) => {
+  cy.contains(columnName).click();
+
+  cy.get(`tr:nth-child(n) td:nth-child(${columnNumber})`).then(($column) => {
+    const columns = [...$column].map((column) =>
+      column.innerText.replace('$', '').replace(',', ''));
+    let counter = 0;
+
+    for (let i = 0; i < columns.length; i++) {
+      if (isNaN(Number(columns[i]))) {
+        if (columns[i].localeCompare(columns[i + 1]) === -1) {
+          counter += 1;
+        }
+      }
+
+      if (Number(columns[i + 1]) >= Number(columns[i])) {
+        counter += 1;
+      }
+    }
+    expect(counter).to.equal(columns.length - 1);
+  });
+});
+
+Cypress.Commands.add('isSortedDESC', (columnName, columnNumber) => {
+  cy.contains(columnName).dblclick();
+
+  cy.get(`tr:nth-child(n) td:nth-child(${columnNumber})`).then(($column) => {
+    const columns = [...$column].map((column) =>
+      column.innerText.replace('$', '').replace(',', ''));
+    let counter = 0;
+
+    for (let i = 0; i < columns.length; i++) {
+      if (isNaN(Number(columns[i]))) {
+        if (columns[i].localeCompare(columns[i + 1]) === 1) {
+          counter += 1;
+        }
+      }
+
+      if (Number(columns[i + 1]) <= Number(columns[i])) {
+        counter += 1;
+      }
+    }
+    expect(counter).to.equal(columns.length - 1);
+  });
+});
 
 describe('Employees table', () => {
   beforeEach(() => {
@@ -22,90 +78,56 @@ describe('Employees table', () => {
   });
 
   it('should sort by name ASC', () => {
-    cy.get('th').contains('Name').click();
-
-    cy.get('tbody > :nth-child(1) > :nth-child(1)')
-      .contains('Airi Satou');
+    cy.isSortedASC('Name', 1);
   });
 
   it('should sort by name DESC', () => {
-    cy.get('th').contains('Name').dblclick();
-
-    cy.get('tbody > :nth-child(1) > :nth-child(1)')
-      .contains('Zorita Serrano');
-  });
-
-  it('should sort by position ASC', () => {
-    cy.get('th').contains('Position').click();
-
-    cy.get('tbody > :nth-child(1) > :nth-child(2)')
-      .contains('Accountant');
-  });
-
-  it('should sort by position DESC', () => {
-    cy.get('th').contains('Position').dblclick();
-
-    cy.get('tbody > :nth-child(1) > :nth-child(2)')
-      .contains('Technical Author');
-  });
-
-  it('should sort by office ASC', () => {
-    cy.get('th').contains('Office').click();
-
-    cy.get('tbody > :nth-child(1) > :nth-child(3)')
-      .contains('Edinburgh');
-  });
-
-  it('should sort by office DESC', () => {
-    cy.get('th').contains('Office').dblclick();
-
-    cy.get('tbody > :nth-child(1) > :nth-child(3)')
-      .contains('Tokyo');
+    cy.isSortedDESC('Name', 1);
   });
 
   it('should sort by age ASC', () => {
-    cy.get('th').contains('Age').click();
-
-    cy.get('tbody > :nth-child(1) > :nth-child(4)')
-      .contains(20);
+    cy.isSortedASC('Age', 4);
   });
 
   it('should sort by age DESC', () => {
-    cy.get('th').contains('Age').dblclick();
-
-    cy.get('tbody > :nth-child(1) > :nth-child(4)')
-      .contains(66);
+    cy.isSortedDESC('Age', 4);
   });
 
   it('should sort by salary ASC', () => {
-    cy.get('th').contains('Salary').click();
-
-    cy.get('tbody > :nth-child(1) > :nth-child(5)')
-      .contains('$98,540');
+    cy.isSortedASC('Salary', 5);
   });
 
   it('should sort by salary DESC', () => {
-    cy.get('th').contains('Salary').dblclick();
+    cy.isSortedDESC('Salary', 5);
+  });
 
-    cy.get('tbody > :nth-child(1) > :nth-child(5)')
-      .contains('$452,500');
+  it('should have proper values in rows after the sorting ASC', () => {
+    cy.get('th').contains('Name').click();
+
+    cy.hasData('Airi Satou',
+      ['Airi Satou', 'Accountant', 'Tokyo', '33', '$162,700']);
+  });
+
+  it('should have proper values in rows after the sorting DESC', () => {
+    cy.get('th').contains('Name').dblclick();
+
+    cy.hasData('Zorita Serrano', ['Zorita Serrano', 'Software Engineer',
+      'San Francisco', '56', '$115,000']);
   });
 
   it('row should have class active after click', () => {
     cy.contains('Airi Satou').parent().should('not.have.class', 'active');
-    cy.get('tbody > :nth-child(1) > :nth-child(1)').click();
-    cy.get('tr').should('have.class', 'active');
+    cy.contains('Airi Satou').click();
+    cy.contains('Airi Satou').parent().should('have.class', 'active');
   });
 
   it('should be able to add a new employee', () => {
-    cy.fillTable('Adam', 'QA Engineer', 'San Francisco', 18, 50000);
+    cy.fillNewEmployeeForm('Adam', 'QA Engineer', 'San Francisco', 18, 50000);
 
     cy.get('[data-qa="notification"]').should('have.class', 'success');
 
-    cy.get('tbody').contains('Adam');
-    cy.get('tbody').contains('QA Engineer');
-    cy.get('tbody').contains('18');
-    cy.get('tbody').contains('$50,000');
+    cy.hasData('Adam',
+      ['Adam', 'QA Engineer', 'San Francisco', '18', '$50,000']);
   });
 
   it(`should have warning notification on name field
@@ -113,7 +135,7 @@ describe('Employees table', () => {
     cy.fillTable('Ada', 'QA Engineer', 'San Francisco', 18, 50000);
 
     cy.get('[data-qa="notification"]').should('have.class', 'error');
-    cy.checkData('Ada', 50000);
+    cy.checkDataDoesntExist('Ada', 50000);
   });
 
   it(`should have warning notification on position 
@@ -125,7 +147,7 @@ describe('Employees table', () => {
     cy.get('button').contains('Save to table').click();
 
     cy.get('[data-qa="notification"]').should('have.class', 'error');
-    cy.checkData('Adam', 50000);
+    cy.checkDataDoesntExist('Adam', 50000);
   });
 
   it(`should have warning notification on age field
@@ -133,7 +155,7 @@ describe('Employees table', () => {
     cy.fillTable('Adam', 'QA Engineer', 'San Francisco', 17, 50000);
 
     cy.get('[data-qa="notification"]').should('have.class', 'error');
-    cy.checkData('Adam', 50000);
+    cy.checkDataDoesntExist('Adam', 50000);
   });
 
   it(`should have warning notification on age field
@@ -141,6 +163,6 @@ describe('Employees table', () => {
     cy.fillTable('Adam', 'QA Engineer', 'San Francisco', 91, 50000);
 
     cy.get('[data-qa="notification"]').should('have.class', 'error');
-    cy.checkData('Adam', 50000);
+    cy.checkDataDoesntExist('Adam', 50000);
   });
 });
