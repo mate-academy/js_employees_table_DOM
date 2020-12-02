d462da1b2641fd3ef1db25cc5b4dc4823ad899a0
Ihor Oliinyk <i.oliinyk@mate.academy>
2020-12-02 16:30:13 +0200

    Update cypress/integration/jsEmployeesTableDOM.spec.js
    Co-authored-by: Roman Apostol <roman@mate.academy>

--- cypress/integration/jsEmployeesTableDOM.spec.js
+++ cypress/integration/jsEmployeesTableDOM.spec.js
@@ -19,7 +19,7 @@ Cypress.Commands.add('checkDataDoesntExist',
 Cypress.Commands.add('compareRowValuesAfterSort', (employeeName, rowValues) => {
   cy.contains('tr', employeeName).find('td').should(($rowValues) => {
     for (let i = 0; i < 5; i++) {
-      expect($rowValue.get(i).innerText).to.equal(rowValue[i]);
+      expect($rowValue.get(i).innerText).to.equal(rowValues[i]);
     }
   });
 });
