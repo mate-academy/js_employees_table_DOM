ca77559ec676a9c25c6228c0cd192ad9a29ed72b
Oleksandr Orel <oleksandrorel@ukr.net>
2020-12-30 19:57:53 +0200

    Solution after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,105 +3,41 @@
 const table = document.querySelector(`table`);
 const bodyOfTable = document.querySelector(`tbody`);
 const people = bodyOfTable.querySelectorAll(`tr`);
+const headers = table.querySelector(`thead`).querySelectorAll(`th`);
 const peopleArr = [...people];
-let previousClickedEl;
-let firstClick = true;
+const headersArr = [...headers];
+let sortedBy;
 
 // Headers sort
+
+const tableSort = function(header) {
+  if (sortedBy !== header) {
+    sortedBy = header;
+
+    const index = headersArr.findIndex(el => {
+      return el.textContent === header;
+    });
+
+    peopleArr.sort((a, b) => {
+      const elA = a.children[index].textContent.replace(/[$,]/g, ``);
+      const elB = b.children[index].textContent.replace(/[$,]/g, ``);
+
+      if (header === `Salary` || header === `Age`) {
+        return elA - elB;
+      }
+
+      return elA.localeCompare(elB);
+    });
+  } else {
+    peopleArr.reverse();
+  }
+};
+
 table.addEventListener(`click`, (event1) => {
   if (event1.target.tagName !== `TH`) {
     return;
   }
-
-  if (event1.target.textContent === `Name`) {
-    peopleArr.sort((a, b) => {
-      const nameA = a.children[0].textContent;
-      const nameB = b.children[0].textContent;
-
-      if (previousClickedEl !== `Name`) {
-        firstClick = true;
-
-        return nameA.localeCompare(nameB);
-      }
-
-      return firstClick === true ? nameA.localeCompare(nameB)
-        : nameB.localeCompare(nameA);
-    });
-    previousClickedEl = event1.target.textContent;
-    firstClick = !firstClick;
-  }
-
-  if (event1.target.textContent === `Position`) {
-    peopleArr.sort((a, b) => {
-      const positionA = a.children[1].textContent;
-      const positionB = b.children[1].textContent;
-
-      if (previousClickedEl !== `Position`) {
-        firstClick = true;
-
-        return positionA.localeCompare(positionB);
-      }
-
-      return firstClick === true ? positionA.localeCompare(positionB)
-        : positionB.localeCompare(positionA);
-    });
-    previousClickedEl = event1.target.textContent;
-    firstClick = !firstClick;
-  }
-
-  if (event1.target.textContent === `Office`) {
-    peopleArr.sort((a, b) => {
-      const positionA = a.children[2].textContent;
-      const positionB = b.children[2].textContent;
-
-      if (previousClickedEl !== `Office`) {
-        firstClick = true;
-
-        return positionA.localeCompare(positionB);
-      }
-
-      return firstClick === true ? positionA.localeCompare(positionB)
-        : positionB.localeCompare(positionA);
-    });
-    previousClickedEl = event1.target.textContent;
-    firstClick = !firstClick;
-  }
-
-  if (event1.target.textContent === `Age`) {
-    peopleArr.sort((a, b) => {
-      const ageA = a.children[3].textContent;
-      const ageB = b.children[3].textContent;
-
-      if (previousClickedEl !== `Age`) {
-        firstClick = true;
-
-        return Number(ageA) - Number(ageB);
-      }
-
-      return firstClick === true ? Number(ageA) - Number(ageB)
-        : Number(ageB) - Number(ageA);
-    });
-    previousClickedEl = event1.target.textContent;
-    firstClick = !firstClick;
-  }
-
-  if (event1.target.textContent === `Salary`) {
-    peopleArr.sort((a, b) => {
-      const sallaryA = a.children[4].textContent.replace(/[$,]/g, ``);
-      const sallaryB = b.children[4].textContent.replace(/[$,]/g, ``);
-
-      if (previousClickedEl !== `Salary`) {
-        firstClick = true;
-
-        return Number(sallaryA) - Number(sallaryB);
-      }
-
-      return firstClick === true ? Number(sallaryA) - Number(sallaryB)
-        : Number(sallaryB) - Number(sallaryA);
-    });
-    previousClickedEl = event1.target.textContent;
-    firstClick = !firstClick;
-  }
+  tableSort(event1.target.textContent);
 
   for (const el of peopleArr) {
     bodyOfTable.append(el);
@@ -117,71 +53,71 @@ bodyOfTable.addEventListener(`click`, (event1) => {
   const arr = [...bodyOfTable.children];
 
   for (const el of arr) {
-    el.className = el.className === `active` ? `` : ``;
+    el.className = `non-active`;
   }
   event1.target.parentNode.className = `active`;
 });
 
 // Created form
-const body = document.querySelector(`body`);
 const form = document.createElement(`form`);
 
 form.method = `GET`;
 form.className = `new-employee-form`;
-body.append(form);
+document.body.append(form);
 
 const label = document.createElement(`label`);
 const input = document.createElement(`input`);
+const labelForSelect = document.createElement(`label`);
 
 label.append(input);
 
-form.append(label.cloneNode(true), label.cloneNode(true), label.cloneNode(true),
-  label.cloneNode(true));
+labelForSelect.innerHTML = `
+Office:
+<select data-qa="office">
+  <option value="Tokyo">Tokyo</option>
+  <option value="Singapore">Singapore</option>
+  <option value="London">London</option>
+  <option value="New York">New York</option>
+  <option value="Edinburgh">Edinburgh</option>
+  <option value="San Francisco">San Francisco</option>
+</select>
+`;
+
+form.append(label.cloneNode(true), label.cloneNode(true),
+  labelForSelect, label.cloneNode(true), label.cloneNode(true));
 
 const labels = document.querySelectorAll(`label`);
 
-labels[0].children[0].setAttribute(`name`, `name`);
-labels[0].children[0].setAttribute(`type`, `text`);
-labels[0].insertAdjacentHTML(`afterbegin`, `Name:`);
-labels[0].children[0].dataset.qa = `name`;
-labels[1].children[0].setAttribute(`position`, `position`);
-labels[1].children[0].setAttribute(`type`, `text`);
-labels[1].insertAdjacentHTML(`afterbegin`, `Position:`);
-labels[1].children[0].dataset.qa = `position`;
-labels[2].children[0].setAttribute(`age`, `age`);
-labels[2].children[0].setAttribute(`type`, `number`);
-labels[2].insertAdjacentHTML(`afterbegin`, `Age:`);
-labels[2].children[0].dataset.qa = `age`;
-labels[3].children[0].setAttribute(`salary`, `salary`);
-labels[3].children[0].setAttribute(`type`, `number`);
-labels[3].insertAdjacentHTML(`afterbegin`, `Salary:`);
-labels[3].children[0].dataset.qa = `salary`;
+let i = 0;
 
-const select = document.createElement(`select`);
+for (const el of headers) {
+  if (el.textContent === `Office`) {
+    i++;
+    continue;
+  }
 
-select.dataset.qa = `office`;
+  labels[i].children[0].setAttribute(el.textContent.toLowerCase(),
+    el.textContent.toLowerCase());
 
-select.innerHTML = `
-<option value="Tokyo">Tokyo</option>
-<option value="Singapore">Singapore</option>
-<option value="London">London</option>
-<option value="New York">New York</option>
-<option value="Edinburgh">Edinburgh</option>
-<option value="San Francisco">San Francisco</option>
-`;
+  labels[i].insertAdjacentHTML(`afterbegin`,
+    `${el.textContent.toLowerCase()}:`);
+  labels[i].children[0].dataset.qa = el.textContent.toLowerCase();
 
-const labelForSelect = document.createElement(`label`);
+  if (el.textContent === `Name` || el.textContent === `Position`) {
+    labels[i].children[0].setAttribute(`type`, `text`);
+    i++;
+  }
 
-labelForSelect.textContent = `Office:`;
-labelForSelect.append(select);
-
-form.insertBefore(labelForSelect, labels[2]);
+  if (el.textContent === `Age` || el.textContent === `Salary`) {
+    labels[i].children[0].setAttribute(`type`, `number`);
+    i++;
+  }
+}
 
 const button = document.createElement(`button`);
 
 form.append(button);
 button.textContent = `Save to table`;
-
 button.type = `submit`;
 
 // handler for form
@@ -217,9 +153,9 @@ form.addEventListener(`submit`, (event1) => {
   <td>${obj.age}</td>
   <td>$${obj.salary.toLocaleString()}</td>
   `;
-
   peopleArr.push(tr);
   bodyOfTable.append(tr);
+  form.reset();
 });
 
 // Notification function
