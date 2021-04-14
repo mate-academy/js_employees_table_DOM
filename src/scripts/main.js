0fc588c92a3b6fa83a92b6668a70118057f25608
Alex Fedorchenko <mralex.fed@gmail.com>
2021-04-14 22:09:02 +0300

    Solution after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,123 +1,213 @@
 'use strict';
 
-const head = document.querySelector('thead').querySelector('tr');
 let nameSort = true;
 let positionSort = true;
 let officeSort = true;
 let ageSort = true;
 let salarySort = true;
+const officeArr = ['Tokyo', 'Singapore', 'London',
+  'New York', 'Edinburgh', 'San Francisco'];
+const head = document.querySelector('thead').querySelector('tr');
+const body = document.querySelector('tbody');
 
-head.addEventListener('click', (e) => {
-  const body = document.querySelector('tbody');
+const mySort = (arr, index, order) => {
+  if (index === 4) {
+    if (order) {
+      arr.sort((a, b) => {
+        const first = Number(a.children[index].textContent
+          .slice(1).split(',').join(''));
+        const second = Number(b.children[index].textContent
+          .slice(1).split(',').join(''));
+
+        return first - second;
+      });
+    } else {
+      arr.sort((a, b) => {
+        const first = Number(a.children[index].textContent
+          .slice(1).split(',').join(''));
+        const second = Number(b.children[index].textContent
+          .slice(1).split(',').join(''));
+
+        return second - first;
+      });
+    }
+  } else if (index === 3) {
+    if (order) {
+      arr.sort((a, b) => {
+        return a.children[index].textContent - b.children[index].textContent;
+      });
+    } else {
+      arr.sort((a, b) => {
+        return b.children[index].textContent - a.children[index].textContent;
+      });
+    }
+  } else {
+    if (order) {
+      arr.sort((a, b) => {
+        return a.children[index].textContent
+          .localeCompare(b.children[index].textContent);
+      });
+    } else {
+      arr.sort((a, b) => {
+        return b.children[index].textContent
+          .localeCompare(a.children[index].textContent);
+      });
+    }
+  }
+};
+
+document.querySelector('thead').addEventListener('click', (e) => {
+  const item = e.target;
   const sortedBody = [...body.querySelectorAll('tr')];
-  const sortTarget = e.target.closest('th');
+  const index = [...head.querySelectorAll('th')].findIndex(td => {
+    return item.textContent === td.textContent;
+  });
 
-  if (sortTarget.textContent === 'Name' && nameSort) {
-    sortedBody.sort((a, b) => {
-      return a.children[0].textContent.localeCompare(b.children[0].textContent);
-    });
-    body.prepend(...sortedBody);
-    nameSort = !nameSort;
-  } else if (sortTarget.textContent === 'Name') {
-    sortedBody.sort((a, b) => {
-      return b.children[0].textContent.localeCompare(a.children[0].textContent);
-    });
-    body.prepend(...sortedBody);
-    nameSort = !nameSort;
+  switch (item.textContent) {
+    case 'Name':
+      mySort(sortedBody, index, nameSort);
+      nameSort = !nameSort;
+      break;
+
+    case 'Position':
+      mySort(sortedBody, index, positionSort);
+      positionSort = !positionSort;
+      break;
+
+    case 'Office':
+      mySort(sortedBody, index, officeSort);
+      officeSort = !officeSort;
+      break;
+
+    case 'Age':
+      mySort(sortedBody, index, ageSort);
+      ageSort = !ageSort;
+      break;
+
+    case 'Salary':
+      mySort(sortedBody, index, salarySort);
+      salarySort = !salarySort;
+      break;
   }
 
-  if (sortTarget.textContent === 'Position' && positionSort) {
-    sortedBody.sort((a, b) => {
-      return a.children[1].textContent.localeCompare(b.children[1].textContent);
-    });
-    body.prepend(...sortedBody);
-    positionSort = !positionSort;
-  } else if (sortTarget.textContent === 'Position') {
-    sortedBody.sort((a, b) => {
-      return b.children[1].textContent.localeCompare(a.children[1].textContent);
-    });
-    body.prepend(...sortedBody);
-    positionSort = !positionSort;
-  }
-
-  if (sortTarget.textContent === 'Office' && officeSort) {
-    sortedBody.sort((a, b) => {
-      return a.children[2].textContent.localeCompare(b.children[2].textContent);
-    });
-    body.prepend(...sortedBody);
-    officeSort = !officeSort;
-  } else if (sortTarget.textContent === 'Office') {
-    sortedBody.sort((a, b) => {
-      return b.children[2].textContent.localeCompare(a.children[2].textContent);
-    });
-    body.prepend(...sortedBody);
-    officeSort = !officeSort;
-  }
-
-  if (sortTarget.textContent === 'Age' && ageSort) {
-    sortedBody.sort((a, b) => {
-      return a.children[3].textContent - b.children[3].textContent;
-    });
-    body.prepend(...sortedBody);
-    ageSort = !ageSort;
-  } else if (sortTarget.textContent === 'Age') {
-    sortedBody.sort((a, b) => {
-      return b.children[3].textContent - a.children[3].textContent;
-    });
-    body.prepend(...sortedBody);
-    ageSort = !ageSort;
-  }
-
-  if (sortTarget.textContent === 'Salary' && salarySort) {
-    sortedBody.sort((a, b) => {
-      const first = Number(a.children[4].textContent
-        .slice(1).split(',').join(''));
-      const second = Number(b.children[4].textContent
-        .slice(1).split(',').join(''));
-
-      return first - second;
-    });
-    body.prepend(...sortedBody);
-    salarySort = !salarySort;
-  } else if (sortTarget.textContent === 'Salary') {
-    sortedBody.sort((a, b) => {
-      const first = Number(a.children[4].textContent
-        .slice(1).split(',').join(''));
-      const second = Number(b.children[4].textContent
-        .slice(1).split(',').join(''));
-
-      return second - first;
-    });
-    body.prepend(...sortedBody);
-    salarySort = !salarySort;
-  }
+  body.prepend(...sortedBody);
 });
 
 const list = document.querySelector('tbody');
 const table = document.querySelector('table');
 
-table.insertAdjacentHTML('afterend', `
-<form class="new-employee-form">
-<label>Name: <input name="name" type="text" data-qa="name" required></label>
-<label>Position: <input name="position" 
-type="text" data-qa="position" required></label>
-<label>Office: <select name="office" data-qa="office">
-<option>Tokyo</option>
-<option>Singapore</option>
-<option>London</option>
-<option>New York</option>
-<option>Edinburgh</option>
-<option>San Francisco</option>
-</select>
-</label>
-<label>Age: <input name="age" type="number" data-qa="age" required></label>
-<label>Salary: <input name="salary" 
-type="number" data-qa="salary" required></label>
-<button type="submit">Save to table</button>
-</form>
-`);
+const createForm = () => {
+  const newForm = document.createElement('form');
 
-const form = document.querySelector('form');
+  newForm.className = 'new-employee-form';
+
+  const labelName = document.createElement('label');
+  const inputName = document.createElement('input');
+
+  inputName.setAttribute('name', 'name');
+  inputName.setAttribute('type', 'text');
+  inputName.setAttribute('required', true);
+  inputName.setAttribute('data-qa', 'name');
+  labelName.textContent = 'Name: ';
+  labelName.append(inputName);
+  newForm.append(labelName);
+
+  const labelPos = document.createElement('label');
+  const inputPos = document.createElement('input');
+
+  inputPos.setAttribute('name', 'position');
+  inputPos.setAttribute('type', 'text');
+  inputPos.setAttribute('required', true);
+  inputPos.setAttribute('data-qa', 'position');
+  labelPos.textContent = 'Position: ';
+  labelPos.append(inputPos);
+  newForm.append(labelPos);
+
+  const labelOffice = document.createElement('label');
+  const selectOffice = document.createElement('select');
+
+  officeArr.forEach(item => {
+    const selOff = document.createElement('option');
+
+    selOff.textContent = item;
+    selectOffice.append(selOff);
+  });
+  selectOffice.setAttribute('name', 'office');
+  selectOffice.setAttribute('data-qa', 'office');
+  labelOffice.textContent = 'Office: ';
+  labelOffice.append(selectOffice);
+  newForm.append(labelOffice);
+
+  const labelAge = document.createElement('label');
+  const inputAge = document.createElement('input');
+
+  inputAge.setAttribute('name', 'age');
+  inputAge.setAttribute('type', 'number');
+  inputAge.setAttribute('required', true);
+  inputAge.setAttribute('data-qa', 'age');
+  labelAge.textContent = 'Age: ';
+  labelAge.append(inputAge);
+  newForm.append(labelAge);
+
+  const labelSalary = document.createElement('label');
+  const inputSalary = document.createElement('input');
+
+  inputSalary.setAttribute('name', 'salary');
+  inputSalary.setAttribute('type', 'number');
+  inputSalary.setAttribute('required', true);
+  inputSalary.setAttribute('data-qa', 'salary');
+  labelSalary.textContent = 'Salary: ';
+  labelSalary.append(inputSalary);
+  newForm.append(labelSalary);
+
+  const myButton = document.createElement('button');
+
+  myButton.setAttribute('type', 'submit');
+  myButton.textContent = 'Save to table';
+
+  newForm.append(myButton);
+
+  return newForm;
+};
+
+const notification = (type, descriptions, notClass) => {
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  div.className = notClass;
+  p.textContent = descriptions;
+  h2.textContent = type;
+  h2.className = 'title';
+  div.append(h2, p);
+
+  return div;
+};
+
+const addRow = (data) => {
+  const tr = document.createElement('tr');
+  let count = 0;
+
+  for (const value of data.values()) {
+    const td = document.createElement('td');
+
+    if (count === 4) {
+      td.textContent = `$${value.match(/.{1,3}/g).join(',')}`;
+      tr.append(td);
+    } else {
+      td.textContent = value;
+      tr.append(td);
+    }
+    count++;
+  }
+
+  return tr;
+};
+
+const form = createForm();
+
+table.parentNode.append(form);
+
 const bodyNode = document.querySelector('body');
 
 form.addEventListener('submit', (e) => {
@@ -127,44 +217,23 @@ form.addEventListener('submit', (e) => {
 
   if (data.get('name').length < 4 || data.get('age') < 18
   || data.get('age') > 90) {
-    bodyNode.insertAdjacentHTML('afterbegin', `
-    <div class="notification error">
-    <h2 class="title">Error!</h2>
-    <p>Enter valid data, please</p>
-  </div>
-    `);
-
-    setTimeout(() => {
-      bodyNode.children[0].remove();
-    }, 2000);
+    bodyNode.prepend(notification('Error!', 'Enter valid data, please',
+      'notification error'));
   } else {
-    bodyNode.insertAdjacentHTML('afterbegin', `
-    <div class="notification success">
-    <h2 class="title">Success!</h2>
-    <p>Congratulations, new employer has been added!</p>
-  </div>
-    `);
-
-    setTimeout(() => {
-      bodyNode.children[0].remove();
-    }, 2000);
-
-    list.insertAdjacentHTML('beforeend', `
-  <tr>
-    <td>${data.get('name')}</td>
-    <td>${data.get('position')}</td>
-    <td>${data.get('office')}</td>
-    <td>${data.get('age')}</td>
-    <td>$${data.get('salary').match(/.{1,3}/g).join(',')}</td>
-  </tr>
-  `);
+    bodyNode.prepend(notification('Success!',
+      'Congratulations, new employer has been added!', 'notification success'));
+    list.append(addRow(data));
     form.reset();
   }
+
+  setTimeout(() => {
+    bodyNode.children[0].remove();
+  }, 2000);
 });
 
-const selectTr = document.querySelector('tbody');
+// const selectTr = document.querySelector('tbody');
 
-selectTr.addEventListener('click', (e) => {
+body.addEventListener('click', (e) => {
   if (document.querySelector('.active') !== null) {
     document.querySelector('.active').classList.remove('active');
   }
@@ -172,7 +241,7 @@ selectTr.addEventListener('click', (e) => {
   e.target.closest('tr').classList.add('active');
 });
 
-selectTr.addEventListener('dblclick', (e) => {
+body.addEventListener('dblclick', (e) => {
   const td = e.target;
   const text = td.textContent;
 
