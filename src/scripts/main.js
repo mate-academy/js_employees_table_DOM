dff19386152fafaada85189c6b37b8bfdc52147b
Inna <inna_temis@ukr.net>
2021-07-06 17:06:25 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,7 @@
 const table = document.querySelector('table');
 const tHead = table.querySelector('thead');
 const tBody = table.querySelector('tbody');
-const rows = tBody.querySelectorAll('tr');
-
+const rows = table.tBodies[0].rows;
 let clicked;
 let sort;
 
@@ -74,30 +73,30 @@ const form = document.createElement('form');
 form.classList.add('new-employee-form');
 
 form.insertAdjacentHTML('afterbegin', `
- <label>Name:
- <input name="name" type="text" data-qa="name" required>
- </label>
+  <label>Name:
+  <input name="name" type="text" data-qa="name" required>
+  </label>
   <label>Position:
   <input name="position" type="text" data-qa="position" required>
- </label>
- <label>Office:
- <select name="office" data-qa="office" required>
- <option value="Tokyo">Tokyo</option>
- <option value="Singapore">Singapore</option>
- <option value="London">London</option>
- <option value="New York">New York</option>
- <option value="Edinburgh">Edinburgh</option>
- <option value="San Francisco">San Francisco</option>
- </select>
- </label>
- <label>Age:
- <input name="age" type="number" data-qa="age" required>
- </label>
- <label>Salary:
- <input name="salary" type="number" data-qa="salary" required>
- </label>
- <button type="submit">Save to table</button>
- `);
+  </label>
+  <label>Office:
+  <select name="office" data-qa="office" required>
+  <option value="Tokyo">Tokyo</option>
+  <option value="Singapore">Singapore</option>
+  <option value="London">London</option>
+  <option value="New York">New York</option>
+  <option value="Edinburgh">Edinburgh</option>
+  <option value="San Francisco">San Francisco</option>
+  </select>
+  </label>
+  <label>Age:
+  <input name="age" type="number" data-qa="age" required>
+  </label>
+  <label>Salary:
+  <input name="salary" type="number" data-qa="salary" required>
+  </label>
+  <button type="submit">Save to table</button>
+`);
 
 document.body.append(form);
 
@@ -114,39 +113,45 @@ btn.addEventListener('click', (e) => {
   const age = formData.children[3].firstElementChild;
   const salary = formData.children[4].firstElementChild;
 
-  if (names.value.length < 4) {
-    getNotification(
+  if (names.value.length < 4 || !names.value) {
+    return getNotification(10, 30,
       'Too short name',
       'Your name should be more then 4 letters',
       'error');
-  } else if (!position.value.length) {
-    getNotification(
+  }
+
+  if (!position.value.length) {
+    return getNotification(110, 30,
       'Fill your position',
       'This field should be fill',
       'error');
-  } else if (+age.value < 18 || +age.value > 90) {
-    getNotification(
+  }
+
+  if (+age.value < 18 || +age.value > 90 || !age.value) {
+    return getNotification(220, 30,
       'Error',
       'Your age < 18 or more then 90',
       'error');
-  } else if (salary.value.length < 2) {
-    getNotification(
+  }
+
+  if (salary.value.length < 2 || !salary.value) {
+    return getNotification(330, 30,
       'This salary is too small',
       'Fill salary field',
       'error');
-  } else {
-    addNewEmployer(names, position, office, age, salary);
-
-    getNotification(
-      'Success',
-      'New Employee added',
-      'success');
-
-    names.value = '';
-    position.value = '';
-    age.value = '';
-    salary.value = '';
   }
+
+  addNewEmployer(names, position, office, age, salary);
+
+  getNotification(10, 30,
+    'Success',
+    'New Employee added',
+    'success');
+
+  names.value = '';
+  position.value = '';
+  age.value = '';
+  salary.value = '';
 });
 
 // function for adding new employer
@@ -156,49 +161,37 @@ function addNewEmployer(
   office,
   age,
   salary) {
-  const newRow = tBody.insertRow(-1);
-  let newCell = newRow.insertCell(0);
-  const nameForm = document.createTextNode(names.value);
+  const tr = document.createElement('tr');
+  const salaryForm = '$'
+  + salary.value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
-  newCell.appendChild(nameForm);
-  newCell = newRow.insertCell(1);
+  tr.insertAdjacentHTML('beforeend', `
+  <td>${names.value}</td>
+  <td>${position.value}</td>
+  <td>${office.value}</td>
+  <td>${age.value}</td>
+  <td>${salaryForm}</td> 
+  `);
 
-  const positionForm = document.createTextNode(position.value);
-
-  newCell.appendChild(positionForm);
-  newCell = newRow.insertCell(2);
-
-  const officeForm = document.createTextNode(office.value);
-
-  newCell.appendChild(officeForm);
-  newCell = newRow.insertCell(3);
-
-  const ageForm = document.createTextNode(age.value);
-
-  newCell.appendChild(ageForm);
-  newCell = newRow.insertCell(4);
-
-  const salaryForm = document.createTextNode('$'
-  + salary.value.replace(/\B(?=(\d{3})+(?!\d))/g, ','));
-
-  newCell.appendChild(salaryForm);
+  tBody.append(tr);
 };
 
 // Function for notification
-function getNotification(title, description, type) {
-  const body = document.querySelector('body');
+function getNotification(positionTop, positionRight, title, description, type) {
   const div = document.createElement('div');
 
-  div.className = `notification ${type}`;
   div.dataset.qa = 'notification';
+  div.classList.add('notification', type);
 
-  div.insertAdjacentHTML('afterbegin', `
-  <h2>${title}</h2> 
+  div.insertAdjacentHTML('beforeend', `
+  <h2 class='title'>${title}</h2>
   <p>${description}</p>
   `);
 
-  body.append(div);
+  div.style.top = positionTop + 'px';
+  div.style.right = positionRight + 'px';
 
+  document.body.append(div);
   setTimeout(() => div.remove(), 2000);
 };
 
