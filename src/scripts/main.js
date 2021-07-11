b57f1bf2ef415c9f47dc54b63c35ff567dc56e51
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-11 22:23:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const table = document.querySelector('table');
 const tableHead = document.querySelector('thead');
 const tableBody = document.querySelector('tbody');
 let click;
@@ -76,4 +77,199 @@ tableBody.addEventListener('click', e => {
   } else {
     selected.classList.remove('active');
   }
+});
+
+table.insertAdjacentHTML('afterend', `
+  <form
+    class= "new-employee-form"
+    action= "/"
+    method= "POST"
+  >
+    <label>
+    Name:
+      <input
+        name="name"
+        type="text"
+        data-qa="name"
+        required
+      >
+    </label>
+
+    <label>
+    Position:
+      <input
+        name="position"
+        type="text"
+        data-qa="position"
+        required
+      >
+    </label>    
+    
+    <label>
+    Office:
+      <select
+        name="office"
+        data-qa="office"
+        required
+      >
+        <option
+          value=""
+          selected
+          disabled
+        >Choose:</option>
+        <option value="Tokyo">Tokyo</option>
+        <option value="Singapore">Singapore</option>
+        <option value="London">London</option>
+        <option value="New York">New York</option>
+        <option value="Edinburgh">Edinburgh</option>
+        <option value="San Francisco">San Francisco</option>
+      </select>
+    </label>
+
+    <label>
+    Age:
+      <input
+        name="age"
+        type="number"
+        data-qa="age"
+        required
+      >
+    </label>
+
+    <label>
+    Salary:
+      <input
+        name="number"
+        type="text"
+        data-qa="salary"
+        required
+      >
+    </label>
+    <button class="sbumit-employee-form" type="submit">Save to table</button>
+  </form>
+`);
+
+const form = document.querySelector('.new-employee-form');
+const employeeNameInput = document.querySelector('[data-qa = "name"]');
+const employeePositionInput = document.querySelector('[data-qa = "position"]');
+const employeeOfficeInput = document.querySelector('[data-qa = "office"]');
+const employeeAgeInput = document.querySelector('[data-qa = "age"]');
+const employeeSalaryInput = document.querySelector('[data-qa = "salary"]');
+
+function makeNotification(type, message) {
+  const notification = document.createElement('div');
+  const title = type[0] + type.slice(1);
+
+  notification.classList.add('notification');
+  notification.dataset.qa = 'notification';
+
+  notification.classList.add(type);
+
+  notification.insertAdjacentHTML('beforeend', `
+    <h2>${title}</h2>
+    <p>${message}</p>
+  `);
+
+  return notification;
+};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (employeeNameInput.value.length < 5) {
+    document.body.append(
+      makeNotification('error', 'Name is less than 4 letters!')
+    );
+  } else if (employeeAgeInput.value < 18 || employeeAgeInput.value > 90) {
+    document.body.append(
+      makeNotification('error', 'Age is less than 18 or more than 90!')
+    );
+  } else if (
+    !employeeNameInput.value
+    || !employeePositionInput.value
+    || !employeeOfficeInput.value
+    || !employeeAgeInput.value
+    || !employeeSalaryInput.value
+  ) {
+    document.body.append(
+      makeNotification('error', 'You have to fill all fields in the form!')
+    );
+  } else {
+    tableBody.append(
+      addToTable(
+        employeeNameInput.value,
+        employeePositionInput.value,
+        employeeOfficeInput.value,
+        employeeAgeInput.value,
+        employeeSalaryInput.value
+      )
+    );
+
+    document.body.append(
+      makeNotification(
+        'success',
+        'Information was successfully added to table!'
+      )
+    );
+    employeeNameInput.value = '';
+    employeePositionInput.value = '';
+    employeeOfficeInput.value = '';
+    employeeAgeInput.value = '';
+    employeeSalaryInput.value = '';
+  }
+
+  setTimeout(() => {
+    document.querySelector('.notification').remove();
+  }, 2000);
+});
+
+function numberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+function addToTable(employeeName, position, office, age, salary) {
+  const tableRow = document.createElement('tr');
+  const validSalary = `$${numberWithCommas(salary)}`;
+
+  tableRow.insertAdjacentHTML('beforeend', `
+    <td>${employeeName}</td>
+    <td>${position}</td>
+    <td>${office}</td>
+    <td>${age}</td>
+    <td>${validSalary}</td>
+  `);
+
+  return tableRow;
+};
+
+const inputCell = document.createElement('input');
+
+inputCell.classList.add('cell-input');
+
+tableBody.addEventListener('dblclick', e => {
+  const item = e.target;
+  const information = item.innerText;
+
+  setTimeout(() => {
+    inputCell.focus();
+  }, 20);
+
+  item.innerText = '';
+  item.append(inputCell);
+
+  inputCell.addEventListener('blur', () => {
+    if (inputCell.value.length > 0) {
+      inputCell.remove();
+      item.innerText = inputCell.value;
+    } else {
+      inputCell.remove();
+      item.innerText = information;
+    }
+  });
+
+  inputCell.addEventListener('keydown', ev => {
+    if (ev.key === 'Enter') {
+      inputCell.blur();
+    }
+  });
 });
