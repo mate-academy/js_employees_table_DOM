fad3bde32b25708db34abc8885cb7a81ac1904de
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2021-09-25 12:46:23 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,51 +1,32 @@
 'use strict';
 
 const tableHeader = document.querySelector('thead');
-const columns = tableHeader.rows[0].cells.length;
+const columns = tableHeader.rows[0].cells;
 const tableBody = document.querySelector('tbody');
-const sortIndex = [];
-
-for (let i = 0; i < columns; i++) {
-  sortIndex[i] = false;
-}
+let sortIndex = -1;
 
 tableHeader.addEventListener('click', e => {
   const column = e.target.closest('th').cellIndex;
   const tableRows = [...tableBody.rows];
 
-  if (sortIndex[column]) {
-    sortIndex[column] = false;
-
-    tableRows.sort((a, b) => {
-      if (!isNaN(+a.cells[column].innerText)) {
-        return +b.cells[column].innerText - +a.cells[column].innerText;
-      }
-
-      if (a.cells[column].innerText.includes('$')) {
-        return b.cells[column].innerText.slice(1).split(',').join('')
-          - a.cells[column].innerText.slice(1).split(',').join('');
-      }
-
-      return b.cells[column].innerText.localeCompare(a.cells[column].innerText);
-    });
-  } else {
-    tableRows.sort((a, b) => {
-      for (let i = 0; i < columns; i++) {
-        sortIndex[i] = false;
-      }
-      sortIndex[column] = true;
-
-      if (!isNaN(+a.cells[column].innerText)) {
-        return +a.cells[column].innerText - +b.cells[column].innerText;
-      }
-
-      if (a.cells[column].innerText.includes('$')) {
+  tableRows.sort((a, b) => {
+    switch (columns[column].innerText) {
+      case 'Age':
+        return a.cells[column].innerText - b.cells[column].innerText;
+      case 'Salary':
         return a.cells[column].innerText.slice(1).split(',').join('')
           - b.cells[column].innerText.slice(1).split(',').join('');
-      }
+      default:
+        return a.cells[column].innerText
+          .localeCompare(b.cells[column].innerText);
+    }
+  });
 
-      return a.cells[column].innerText.localeCompare(b.cells[column].innerText);
-    });
+  if (sortIndex === column) {
+    sortIndex = -1;
+    tableRows.reverse();
+  } else {
+    sortIndex = column;
   }
 
   tableBody.append(...tableRows);
@@ -63,130 +44,123 @@ tableBody.addEventListener('click', e => {
 });
 
 const form = document.createElement('form');
-
-document.body.append(form);
-form.classList.add('new-employee-form');
-
-const nameLabel = document.createElement('label');
-const positionLabel = document.createElement('label');
-const officeLabel = document.createElement('label');
-const ageLabel = document.createElement('label');
-const salaryLabel = document.createElement('label');
 const saveButonn = document.createElement('button');
 
-form.append(nameLabel, positionLabel, officeLabel, ageLabel,
-  salaryLabel, saveButonn);
-
-nameLabel.innerText = 'Name:';
-positionLabel.innerText = 'Position:';
-officeLabel.innerText = 'Office:';
-ageLabel.innerText = 'Age:';
-salaryLabel.innerText = 'Salary:';
+form.classList.add('new-employee-form');
 saveButonn.innerText = 'Save to table';
-
-const nameInput = document.createElement('input');
-const positionInput = document.createElement('input');
-const officeInput = document.createElement('select');
-const ageInput = document.createElement('input');
-const salaryInput = document.createElement('input');
-const tokyoOption = document.createElement('option');
-const singaporeOption = document.createElement('option');
-const londonOption = document.createElement('option');
-const newYorkOption = document.createElement('option');
-const edinburghOption = document.createElement('option');
-const sanFranciscoOption = document.createElement('option');
-
-nameLabel.append(nameInput);
-nameInput.name = 'name';
-nameInput.type = 'text';
-nameInput.dataset.qa = 'name';
-positionLabel.append(positionInput);
-positionInput.name = 'position';
-positionInput.type = 'text';
-positionInput.dataset.qa = 'position';
-officeLabel.append(officeInput);
-officeInput.name = 'office';
-officeInput.dataset.qa = 'office';
-tokyoOption.value = 'Tokyo';
-tokyoOption.innerText = 'Tokyo';
-singaporeOption.value = 'Singapore';
-singaporeOption.innerText = 'Singapore';
-londonOption.value = 'London';
-londonOption.innerText = 'London';
-newYorkOption.value = 'New York';
-newYorkOption.innerText = 'New York';
-edinburghOption.value = 'Edinburgh';
-edinburghOption.innerText = 'Edinburgh';
-sanFranciscoOption.value = 'San Francisco';
-sanFranciscoOption.innerText = 'San Francisco';
-
-officeInput.append(tokyoOption, singaporeOption, londonOption, newYorkOption,
-  edinburghOption, sanFranciscoOption);
-ageLabel.append(ageInput);
-ageInput.name = 'age';
-ageInput.type = 'number';
-ageInput.dataset.qa = 'age';
-salaryLabel.append(salaryInput);
-salaryInput.name = 'salary';
-salaryInput.type = 'number';
-salaryInput.dataset.qa = 'salary';
 saveButonn.type = 'submit';
+
+for (const cell of columns) {
+  const label = document.createElement('label');
+  let input;
+
+  label.innerText = cell.innerText + ':';
+
+  switch (cell.innerText) {
+    case 'Age':
+      input = document.createElement('input');
+      input.name = 'age';
+      input.type = 'number';
+      input.dataset.qa = 'age';
+      break;
+    case 'Salary':
+      input = document.createElement('input');
+      input.name = 'salary';
+      input.type = 'number';
+      input.dataset.qa = 'salary';
+      break;
+    case 'Office':
+      input = document.createElement('select');
+      input.name = 'office';
+
+      input.innerHTML = `<option>Tokyo</option><option>Singapore</option>
+        <option>London</option><option>New York</option><option>Edinburgh
+        </option><option>San Francisco</option>`;
+      input.dataset.qa = 'office';
+      break;
+    default:
+      input = document.createElement('input');
+      input.name = cell.innerText.toLowerCase();
+      input.type = 'text';
+      input.dataset.qa = cell.innerText.toLowerCase();
+      break;
+  }
+
+  label.append(input);
+  form.append(label);
+}
+
+form.append(saveButonn);
+document.body.append(form);
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-
-  let salary;
-
-  if (+salaryInput.value > 0) {
-    salary = +salaryInput.value;
-
-    salary = '$' + salary.toLocaleString('en-US');
-  }
-
-  addEmploee(nameInput.value, positionInput.value, officeInput.value,
-    ageInput.value, salary);
-
-  form.reset();
+  addEmploee();
 });
 
-function addEmploee(employeeName, position, office, age, salary) {
-  if (employeeName.length < 4) {
-    return pushNotification('Employee Name error',
-      'Employee Name should be 4 letters or more', 'error');
-  }
-
-  if (age < 18 || age > 90) {
-    return pushNotification('Employee Age error',
-      'Employee Age should be between 18 and 90', 'error');
-  }
-
+function addEmploee() {
   const row = document.createElement('tr');
 
-  for (let i = 0; i < columns; i++) {
+  for (const cells of columns) {
+    const input = form.querySelector(`[name=${cells.innerText.toLowerCase()}]`);
     const cell = document.createElement('td');
 
-    if (!arguments[i]) {
-      return pushNotification('Invalid input',
-        'Invalid input', 'error');
+    switch (cells.innerText.toLowerCase()) {
+      case 'name':
+        if (input.value.length < 4) {
+          return pushNotification(150, 10, 'Employee Name error',
+            'Employee Name should be 4 letters or more', 'error');
+        } else {
+          cell.innerText = input.value;
+        }
+        break;
+      case 'age':
+        if (input.value < 18 || input.value > 90) {
+          return pushNotification(250, 10, 'Employee Age error',
+            'Employee Age should be between 18 and 90', 'error');
+        } else {
+          cell.innerText = input.value;
+        }
+        break;
+      case 'salary':
+        if (input.value < 1) {
+          return pushNotification(350, 10, 'Employee Salary error',
+            'Employee Salary should be bigger than 0', 'error');
+        } else {
+          cell.innerText = '$' + (+input.value).toLocaleString('en-US');
+        }
+        break;
+      default:
+        if (input.value === '') {
+          return pushNotification(450, 10, 'Invalid input',
+            'Input all fields', 'error');
+        } else {
+          cell.innerText = input.value;
+        }
+        break;
     }
-    cell.innerText = arguments[i];
+
     row.append(cell);
   }
 
   tableBody.append(row);
 
-  pushNotification('Employee successfully added',
+  pushNotification(10, 10, 'Employee successfully added',
     'New Employee was successfully added to the table', 'success');
+
+  form.reset();
 }
 
-function pushNotification(title, description, type) {
+function pushNotification(posTop, posRight, title, description, type) {
   const message = document.createElement('div');
   const messageTitle = document.createElement('h2');
   const messageDescription = document.createElement('p');
 
   message.classList.add('notification', type);
-  messageTitle.classList.add('title');
   message.dataset.qa = 'notification';
+  messageTitle.classList.add('title');
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
   messageTitle.innerText = title;
   messageDescription.innerText = description;
   message.append(messageTitle, messageDescription);
@@ -194,44 +168,107 @@ function pushNotification(title, description, type) {
 
   setTimeout(function() {
     message.hidden = true;
-  }, 3000);
-};
+  }, 2000);
+}
 
 tableBody.addEventListener('dblclick', e => {
+  if (e.target.matches('input') || e.target.matches('select')) {
+    return;
+  }
+
   const cell = e.target.closest('td');
-  const cellInput = document.createElement('input');
+  const index = cell.cellIndex;
+  let cellInput;
   const oldData = cell.innerText;
 
+  switch (columns[index].innerText) {
+    case 'Age':
+      cellInput = document.createElement('input');
+      cellInput.type = 'number';
+      cell.innerText = '';
+      cellInput.value = oldData;
+      break;
+    case 'Salary':
+      cell.innerText = '';
+      cellInput = document.createElement('input');
+      cellInput.type = 'number';
+      cellInput.value = oldData.slice(1).split(',').join('');
+      break;
+    case 'Office':
+      cell.innerText = '';
+      cellInput = document.createElement('select');
+
+      cellInput.innerHTML = `<option>Tokyo</option><option>Singapore</option>
+        <option>London</option><option>New York</option><option>Edinburgh
+        </option><option>San Francisco</option>`;
+      cellInput.value = oldData;
+      break;
+    default:
+      cellInput = document.createElement('input');
+      cellInput.type = 'text';
+      cell.innerText = '';
+      cellInput.value = oldData;
+      break;
+  }
+
   cellInput.classList.add('cell-input');
-  cell.innerText = '';
   cell.append(cellInput);
   cellInput.focus();
-  cellInput.type = 'text';
-
-  if (oldData.includes('$')) {
-    cellInput.type = 'number';
-  }
-
-  if (!isNaN(+oldData)) {
-    cellInput.type = 'number';
-    cellInput.min = '18';
-    cellInput.max = '90';
-  }
 
   cellInput.addEventListener('blur', () => {
-    let newData = cellInput.value;
-
-    if (newData === '') {
-      newData = oldData;
-    } else {
-      if (oldData.includes('$')) {
-        newData = +newData;
-        newData = '$' + newData.toLocaleString('en-US');
-      }
-    }
+    const newData = cellInput.value;
 
     cellInput.remove();
-    cell.innerText = newData;
+
+    switch (columns[index].innerText) {
+      case 'Name':
+        if (newData.length < 4) {
+          cell.innerText = oldData;
+
+          return pushNotification(150, 10, 'Employee Name error',
+            'Employee Name should be 4 letters or more', 'error');
+        } else {
+          cell.innerText = newData;
+        }
+        break;
+      case 'Age':
+        if (newData < 18 || newData > 90) {
+          cell.innerText = oldData;
+
+          return pushNotification(250, 10, 'Employee Age error',
+            'Employee Age should be between 18 and 90', 'error');
+        } else {
+          cell.innerText = newData;
+        }
+        break;
+      case 'Salary':
+        if (newData < 1) {
+          cell.innerText = oldData;
+
+          return pushNotification(350, 10, 'Employee Salary error',
+            'Employee Salary should be bigger than 0', 'error');
+        } else {
+          cell.innerText = '$' + newData.toLocaleString('en-US');
+        }
+        break;
+      default:
+        if (newData === '') {
+          cell.innerText = oldData;
+
+          return pushNotification(450, 10, 'Invalid input',
+            'Input can\'t be empy', 'error');
+        } else {
+          cell.innerText = newData;
+        }
+        break;
+    }
+
+    if (newData === oldData) {
+      return;
+    }
+
+    pushNotification(10, 10, 'Data successfully chenged',
+      'Data was successfully chenged', 'success');
   });
 
   cellInput.addEventListener('keydown', enter => {
