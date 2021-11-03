5458ca6eb131b2edc7b1468dae42b3e570b7ca61
Mariia Kovach <maria_kovach@ukr.net>
2021-11-03 17:11:36 +0200

    changes applied

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('thead th');
+const tableBody = table.querySelector('tbody');
+const tableRows = [...tableBody.rows];
+
+function toNumber(str) {
+  return +str.replace('$', '').split(',').join('');
+}
+
+headers.forEach(header => {
+  header.addEventListener('click', (e) => {
+    const headerIndex = [...headers].indexOf(e.target);
+
+    tableRows.sort((a, b) => {
+      const x = a.cells[headerIndex].innerText;
+      const y = b.cells[headerIndex].innerText;
+
+      if (e.target.innerText === 'Salary') {
+        return toNumber(x) - toNumber(y);
+      }
+
+      return x - y || x.localeCompare(y);
+    });
+
+    tableBody.append(...tableRows);
+  });
+});
