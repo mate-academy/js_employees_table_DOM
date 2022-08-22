5cf9990e9797ba41b1b73149756e8dad6c52c748
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-08-23 00:26:59 +0300

    First commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,241 @@
 'use strict';
 
-// write code here
+const toNum = (numStr) => {
+  if (numStr[0] !== '$') {
+    return +numStr;
+  }
+
+  return Number(numStr.split(',').join('').slice(1));
+};
+
+const heading = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const tr = document.querySelectorAll('tr');
+const arr = [];
+
+tr.forEach(item => arr.push(item));
+arr.shift();
+arr.pop();
+
+let oldTarget = null;
+
+const handler = e => {
+  const targetItem = e.target.closest('th');
+
+  if (!targetItem || !heading.contains(targetItem)) {
+    return;
+  }
+
+  let index = null;
+
+  switch (targetItem.innerText) {
+    case 'Name':
+      index = 0;
+      break;
+
+    case 'Position':
+      index = 1;
+      break;
+
+    case 'Office':
+      index = 2;
+      break;
+
+    case 'Age':
+      index = 3;
+      break;
+
+    case 'Salary':
+      index = 4;
+      break;
+  }
+
+  arr.sort((a, b) => {
+    let aElement = a.children[index].innerText;
+    let bElement = b.children[index].innerText;
+
+    if (targetItem === oldTarget) {
+      [aElement, bElement] = [bElement, aElement];
+    };
+
+    if (targetItem.innerText === 'Salary' || targetItem.innerText === 'Age') {
+      return toNum(aElement) - toNum(bElement);
+    }
+
+    return aElement.localeCompare(bElement);
+  });
+
+  tbody.innerHTML = arr.map(item => `
+    <tr>
+      <td>${item.children[0].innerText}</td>
+      <td>${item.children[1].innerText}</td>
+      <td>${item.children[2].innerText}</td>
+      <td>${item.children[3].innerText}</td>
+      <td>${item.children[4].innerText}</td>
+    </tr>
+  `).join('');
+
+  oldTarget = targetItem;
+};
+
+let oldTargetRow = null;
+
+const rowHandler = e => {
+  const targetRow = e.target.closest('tr');
+
+  if (oldTargetRow !== null && targetRow.innerText !== oldTargetRow.innerText) {
+    oldTargetRow.classList.remove('active');
+  }
+  targetRow.classList.add('active');
+  oldTargetRow = targetRow;
+};
+
+/* Start script for creating form; */
+const form = document.createElement('form');
+
+form.classList.add('new-employee-form');
+
+form.insertAdjacentHTML('beforeend', `
+  <label>Name: 
+    <input name="name" data-qa="name" type="text" required>
+  </label>
+
+  <label>Position: 
+    <input name="position" data-qa="position" type="text" required>
+  </label>
+
+  <label>Office: 
+    <select name="office" data-qa="office" required>
+      <option value="Tokyo">Tokyo</option>
+      <option value="Singapore">Singapore</option>
+      <option value="London">London</option>
+      <option value="New York">New York</option>
+      <option value="Edinburhg">Edinburgh</option>
+      <option value="San Francisco">San Francisco</option>
+    </select>
+  </label>
+
+  <label>Age: 
+    <input name="age" data-qa="age" type="number" required>
+  </label>
+
+  <label>Salary: 
+    <input  data-qa="salary" name="salary" type="number" required>
+  </label>
+
+  <button type="submit">Save to table</button>
+`);
+document.body.append(form);
+/* End creating form. */
+
+/* Handler for submit + validation */
+function prettySalary(salary) {
+  const strNum = String(salary);
+  const numberArr = strNum.split('');
+  const numberArrCopy = [...numberArr];
+  let count = 1;
+
+  for (let i = numberArr.length - 1; i !== 0; i--) {
+    if (count % 3 === 0 && count !== 0) {
+      numberArrCopy.splice(i, 0, ',');
+    }
+
+    count++;
+  }
+
+  return '$' + numberArrCopy.join('');
+}
+
+const validation = data => {
+  if (data.get('name').length < 4) {
+    return 'Invalid name';
+  }
+
+  if (+data.get('age') < 18 || +data.get('age') > 90) {
+    return 'Invalid age';
+  }
+};
+
+const submitHandler = e => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+  const div = document.createElement('div');
+
+  div.classList.add('notification');
+  div.setAttribute('data-qa', 'notification');
+
+  if (validation(data) === 'Invalid name') {
+    div.classList.add('error');
+
+    div.innerHTML = `
+      <h2 class = "title">Invalid name</h2>
+      <p>You type too short name. Change it.</p>
+    `;
+  } else if (validation(data) === 'Invalid age') {
+    div.classList.add('error');
+
+    div.innerHTML = `
+      <h2 class = "title">Invalid age</h2>
+      <p>You type too big or too small age. Fix it.</p>
+    `;
+  } else {
+    div.classList.add('success');
+
+    div.innerHTML = `
+      <h2 class = "title">Success</h2>
+      <p>Employee was added to the list.</p>
+    `;
+
+    const newTr = document.createElement('tr');
+    const newEmpSalary = prettySalary(data.get('salary'));
+
+    newTr.innerHTML = `
+      <td>${data.get('name')}</td>
+      <td>${data.get('position')}</td>
+      <td>${data.get('office')}</td>
+      <td>${data.get('age')}</td>
+      <td>${newEmpSalary}</td>
+    `;
+
+    arr.push(newTr);
+    tbody.insertAdjacentElement('beforeend', newTr);
+  }
+
+  document.body.append(div);
+
+  setTimeout(() => {
+    div.remove();
+  }, 2000);
+};
+
+/* End handler */
+
+/* Double click on cell handler */
+
+const doubleClickHandler = e => {
+  const targetCell = e.target.closest('td');
+  const initialValue = targetCell.innerText;
+
+  targetCell.innerText = '';
+  targetCell.innerHTML = `<input name="inputVal" class="cell-input">`;
+
+  const targetInput = document.querySelector('.cell-input');
+
+  targetCell.addEventListener('keypress', ev => {
+    if (ev.key === 'Enter') {
+      if (targetInput.value) {
+        targetCell.innerText = targetInput.value;
+      } else {
+        targetCell.innerText = initialValue;
+      }
+    }
+  });
+};
+
+/* End handler */
+
+heading.addEventListener('click', handler);
+tbody.addEventListener('click', rowHandler);
+form.addEventListener('submit', submitHandler);
+tbody.addEventListener('dblclick', doubleClickHandler);
