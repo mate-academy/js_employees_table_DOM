a9272347ece5ba08f2fe8734e549e3c3b1b3f6b3
Oleksii <oleksii@it.com>
2022-09-13 15:23:12 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,195 @@
 'use strict';
 
-// write code here
+const body = document.querySelector('body');
+const table = body.querySelector('table');
+const tHead = table.querySelector('thead');
+const tBody = table.querySelector('tbody');
+
+body.insertAdjacentHTML('beforeend', `
+  <form 
+    action="#"
+    method="get"
+    class="new-employee-form"
+  >
+    <label>Name:
+      <input name="name" type="text" data-qa="name">
+    </label>
+    <label>Position:
+      <input name="position" type="text" data-qa="position">
+    </label>
+    <label>Office:
+      <select name="office" data-qa="office">
+        <option value="Tokyo">Tokyo</option>
+        <option value="Singapore">Singapore</option>
+        <option value="London">London</option>
+        <option value="New York">New York</option>
+        <option value="Edinburgh">Edinburgh</option>
+        <option value="San Francisco">San Francisco</option>
+      </select>
+    </label>
+    <label>Age:
+      <input
+        name="age"
+        type="number"
+        data-qa="age"
+        min=0
+      >
+    </label>
+    <label>Salary:
+      <input
+        name="salary"
+        type="number"
+        data-qa="salary"
+        min=0
+      >
+    </label>
+    <button type="submit">Save to table</button>
+  </form>
+`);
+
+// just NOTIFICATION(function with errors for it)
+function pushNotification(title, description, type) {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="notification ${type}" data-qa="notification">
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+  </div>
+  `);
+
+  setTimeout(() => {
+    body.removeChild(document.querySelector('.notification'));
+  }, 2000);
+}
+
+// just VALIDATION
+function checkFormData(key, value) {
+  if (!value) {
+    pushNotification('An error occured', 'Empty field value', 'error');
+
+    return false;
+  }
+
+  if (key === 'name' && value.length < 4) {
+    pushNotification('An error occured', 'Invalid name value', 'error');
+
+    return false;
+  }
+
+  if (key === 'age' && ((+value < 18 || +value > 90) || isNaN(+value))) {
+    pushNotification('An error occured', 'Invalid age value', 'error');
+
+    return false;
+  }
+
+  return true;
+}
+
+// just ROW SORTING
+tHead.addEventListener('click', (e) => {
+  const index = e.target.cellIndex;
+  const isSorted = e.target.matches('.sorted');
+  const isSalary = e.target.innerText === 'Salary';
+
+  [...tBody.children]
+    .sort((a, b) => {
+      const aValue = isSalary
+        ? +(a.children[index].innerText.replaceAll(/\$|,/g, ''))
+        : a.children[index].innerText;
+      const bValue = isSalary
+        ? +(b.children[index].innerText.replaceAll(/\$|,/g, ''))
+        : b.children[index].innerText;
+
+      return aValue > bValue
+        ? isSorted ? -1 : 1
+        : aValue < bValue
+          ? isSorted ? 1 : -1
+          : 0;
+    })
+    .forEach(item => tBody.append(item));
+
+  e.target.classList.toggle('sorted', !isSorted);
+
+  [...tHead.firstElementChild.children].forEach(item => {
+    if (item !== e.target) {
+      item.classList.remove('sorted');
+    }
+  });
+});
+
+// just ROW SELECTING
+tBody.addEventListener('click', (e) => {
+  if (e.target.closest('tr').matches('.active')) {
+    return;
+  }
+
+  [...tBody.children].forEach(item => {
+    item.classList.remove('active');
+  });
+
+  e.target.closest('tr').classList.toggle('active');
+});
+
+// just FORM VALIDATION
+body.querySelector('.new-employee-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const data = new FormData(e.target);
+
+  const newNode = document.createElement('tr');
+
+  for (const [key, value] of data) {
+    if (!checkFormData(key, value)) {
+      return;
+    }
+
+    if (key === 'salary') {
+      newNode.insertAdjacentHTML('beforeend', `
+        <td>$${Intl.NumberFormat('en-US').format(+value)}</td>
+      `);
+    } else {
+      newNode.insertAdjacentHTML('beforeend', `<td>${value}</td>`);
+    }
+  }
+
+  tBody.append(newNode);
+  e.target.reset();
+  pushNotification('Success', 'New data added', 'success');
+});
+
+// just RAW
+tBody.addEventListener('dblclick', (e) => {
+  const element = e.target.closest('td');
+  const parentElement = element.parentElement;
+  const input = document.createElement('input');
+  const cellIndex = element.cellIndex;
+
+  input.classList.add('cell-input');
+  input.type = 'text';
+  parentElement.replaceChild(input, element);
+  input.focus();
+  input.value = element.innerText;
+
+  input.addEventListener('blur', (elem) => {
+
+  });
+
+  input.addEventListener('blur', (elem) => {
+    const key = tHead.firstElementChild.children[cellIndex]
+      .innerText.toLowerCase();
+
+    if (checkFormData(key, input.value)) {
+      element.innerText = input.value;
+    }
+
+    parentElement.replaceChild(element, input);
+    input.remove();
+  });
+
+  input.addEventListener('keydown', (elem) => {
+    if (elem.type === 'keydown' && elem.code !== 'Enter') {
+      return;
+    }
+
+    input.blur();
+  });
+});
