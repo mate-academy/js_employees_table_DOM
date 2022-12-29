8326182be77e627900a0a148375929c446c0ba11
Vlad Boikov <vladboikov95@hotmail.com>
2022-12-29 12:25:46 +0200

    Sortind added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,68 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('tbody');
+const persons = [...table.rows];
+
+//sorting 
+
+function trimmer(salary) {
+  return salary.replace(/\D/g, '');
+};
+
+function cleaner(coll) {
+  for (const th of coll) {
+    th.removeAttribute('data-sorted');
+  };
+}
+
+document.addEventListener('click', e => {
+  const target = e.target;
+
+  if (!target.dataset.sorted || target.dataset.sorted === 'DESC') {
+    persons.sort((a, b) => {
+      switch (target.innerText) {
+        case 'Name':
+        case 'Position':
+        case 'Office':
+          return a.children[target.cellIndex].innerText
+            .localeCompare(b.children[target.cellIndex].innerText);
+
+        case 'Age':
+          return +a.children[target.cellIndex].innerText
+            - +b.children[target.cellIndex].innerText;
+
+        case 'Salary':
+          return trimmer(a.children[target.cellIndex].innerText)
+            - trimmer(b.children[target.cellIndex].innerText);
+      }
+    });
+
+    cleaner(document.querySelectorAll('th'));
+
+    target.dataset.sorted = 'ASC';
+  } else {
+    persons.sort((a, b) => {
+      switch (target.innerText) {
+        case 'Name':
+        case 'Position':
+        case 'Office':
+          return b.children[target.cellIndex].innerText
+            .localeCompare(a.children[target.cellIndex].innerText);
+
+        case 'Age':
+          return +b.children[target.cellIndex].innerText
+            - +a.children[target.cellIndex].innerText;
+
+        case 'Salary':
+          return trimmer(b.children[target.cellIndex].innerText)
+            - trimmer(a.children[target.cellIndex].innerText);
+      }
+    });
+
+    cleaner(document.querySelectorAll('th'));
+
+    target.dataset.sorted = 'DESC';
+  };
+
+  table.append(...persons);
+});
