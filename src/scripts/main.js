f39401cb59330f6f534d914c325d4fff8ec8ed88
Vasiliy22 <hitretsukrnet@gmail.com>
2023-01-16 14:34:50 +0200

    Solution 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,21 @@ const tableBody = table.querySelector('tbody');
 const headers = table.querySelectorAll('th');
 const rows = tableBody.querySelectorAll('tr');
 const form = document.createElement('form');
+const options = `<label for="office">Office:
+<select id="office" required>
+  <option>Tokyo</option>
+  <option>Singapore</option>
+  <option>London</option>
+  <option>New York</option>
+  <option>Edinburgh</option>
+  <option>San Francisco</option>
+</select>
+</label>`;
 let sortCol;
 let sortAsc = false;
 
 [].forEach.call(headers, (header, index) => {
-  header.addEventListener('click', (events) => {
+  header.addEventListener('click', () => {
     sortColumn(index, false);
   });
 });
@@ -38,7 +48,7 @@ function convertToString(numb) {
 };
 
 const sortColumn = (index) => {
-  const thisSort = index.target;
+  const thisSort = index;
 
   if (sortCol === thisSort) {
     sortAsc = !sortAsc;
@@ -46,11 +56,11 @@ const sortColumn = (index) => {
 
   sortCol = thisSort;
 
-  const newRows = Array.from(rows);
+  const newRows = Array.from(tableBody.rows);
 
   newRows.sort((rowA, rowB) => {
-    const cellA = rowA.querySelectorAll('td')[index].innerHTML;
-    const cellB = rowB.querySelectorAll('td')[index].innerHTML;
+    const cellA = rowA.querySelectorAll('td')[index].innerText;
+    const cellB = rowB.querySelectorAll('td')[index].innerText;
 
     if (Number.isNaN(+cellA)) {
       if (cellA[0] === '$') {
@@ -69,10 +79,6 @@ const sortColumn = (index) => {
     }
   });
 
-  [].forEach.call(rows, (row) => {
-    tableBody.removeChild(row);
-  });
-
   newRows.forEach((newRow) => {
     tableBody.appendChild(newRow);
   });
@@ -83,7 +89,7 @@ form.classList.add('new-employee-form');
 form.innerHTML = `
   <form name="myForm">
   <label for="names">Name:
-    <input name="name" type="text" id="names" data-qa="name" required>
+    <input name="name" type="text" id="names" data-qa="Name" required>
   </label>
   <label for="pos">Position:
      <input name="position" type="text" id="pos" data-qa="Position" required>
@@ -99,10 +105,16 @@ form.innerHTML = `
       </select>
     </label>
   <label for="age">Age:
-    <input name="age" type="number" id="age" data-qa="age" required>
+    <input name="age" type="number" min="0" id="age" data-qa="Age" required>
   </label>
   <label for="salary">Salary:
-     <input name="salary" type="number" id="salary" data-qa="salary" required>
+     <input 
+      name="salary" 
+      type="number" 
+      id="salary" 
+      data-qa="Salary" 
+      min="0"
+      required>
    </label>
 
   <button class="submit" name="submit">Save to table</button>
@@ -111,30 +123,11 @@ form.innerHTML = `
 body.append(form);
 
 const button = document.querySelector('.submit');
-const errorDiv = document.createElement('div');
-const successDiv = document.createElement('div');
 
-successDiv.innerHTML = `
-<h2 class='title'>Success</h2>
-<p>Data added in the Table</p>
-`;
+button.addEventListener('click', (events) => {
+  events.preventDefault();
 
-errorDiv.innerHTML = `
-  <h2 class='title'>Error</h2>
-  <p>add Information</p>
-`;
-
-successDiv.classList.add('notification', 'success');
-successDiv.setAttribute('data-qa', 'notification');
-errorDiv.classList.add('notification', 'error');
-errorDiv.setAttribute('data-qa', 'notification');
-body.append(successDiv);
-body.append(errorDiv);
-
-errorDiv.style.display = 'none';
-successDiv.style.display = 'none';
-
-function validateForm() {
+  const input = document.querySelectorAll('input');
   const newRow = document.createElement('tr');
   const names = document.getElementById('names').value;
   const salary = document.getElementById('salary').value;
@@ -142,71 +135,150 @@ function validateForm() {
   const office = document.getElementById('office').value;
   const pos = document.getElementById('pos').value;
 
+  if (names.length < 4) {
+    pushNotification('Error', 'Name Min. length is 4 letters', 'error');
+
+    return;
+  };
+
+  if (pos.length <= 1) {
+    pushNotification('Error', 'Position shouldn`t empty', 'error');
+
+    return;
+  };
+
+  if (age < 18 || age > 90) {
+    pushNotification('Error', 'Age should be from 18 to 90', 'error');
+
+    return;
+  }
+
+  if (salary === '') {
+    pushNotification('Error', 'Salary shouldn`t empty', 'error');
+
+    return;
+  };
+
   newRow.innerHTML = `
   <tr>
-    <td>${document.getElementById('names').value}</td>
-    <td>${document.getElementById('pos').value}</td>
-    <td>${document.getElementById('office').value}</td>
-    <td>${String(document.getElementById('age').value)}</td>
-    <td>${convertToString(document.getElementById('salary').value)}</td>
+    <td>${names}</td>
+    <td>${pos}</td>
+    <td>${office}</td>
+    <td>${String(age)}</td>
+    <td>${convertToString(+salary)}</td>
   </tr>
   `;
+  tableBody.append(newRow);
 
-  if (names.length < 4 || age < 18 || age > 90) {
-    errorDiv.style.display = 'block';
+  pushNotification('Success', 'New employee was added', 'success');
 
-    setTimeout(() => {
-      errorDiv.style.display = 'none';
-    }, 1500);
+  input.forEach(field => {
+    field.value = '';
+  });
 
-    return false;
-  } else
-  if (salary === '' || office === '' || pos === '') {
-    errorDiv.style.display = 'block';
-
-    setTimeout(() => {
-      errorDiv.style.display = 'none';
-    }, 1500);
-
-    return false;
-  } else {
-    successDiv.style.display = 'block';
-
-    setTimeout(() => {
-      successDiv.style.display = 'none';
-    }, 1500);
-
-    tableBody.append(newRow);
-  };
-}
-
-button.addEventListener('click', (events) => {
-  events.preventDefault();
-  validateForm();
+  sortColumn();
 });
 
-const td = table.querySelectorAll('td');
+const pushNotification = (title, description, type) => {
+  const element = document.createElement('div');
+  const elTitle = document.createElement('h2');
+  const elPar = document.createElement('p');
 
-[...td].map(item => {
-  item.addEventListener('dblclick', () => {
-    const input = document.createElement('input');
-    const valueItem = item;
+  element.classList.add(type, 'notification');
+  element.dataset.qa = 'notification';
 
-    input.classList.add('cell-input');
-    input.value = valueItem.innerText;
-    input.value = '';
-    valueItem.innerText = '';
-    valueItem.append(input);
-    input.focus();
+  elTitle.classList.add('title');
+  elTitle.innerHTML = title;
 
-    input.addEventListener('blur', () => {
-      valueItem.innerText = input.value;
-    });
+  elPar.innerHTML = description;
 
-    input.addEventListener('keydown', (eventKey) => {
-      if (eventKey.key === 'Enter') {
-        valueItem.innerText = input.value;
+  element.append(elTitle, elPar);
+
+  document.body.append(element);
+
+  setTimeout(() => {
+    element.remove();
+  }, 3000);
+};
+
+table.addEventListener('dblclick', e => {
+  if (e.target.tagName !== 'TD') {
+    return;
+  };
+
+  if (table.querySelectorAll('.cell-input').length >= 1) {
+    return;
+  }
+
+  const cellText = e.target.textContent;
+  const newInput = e.target.cellIndex === 2
+    ? document.createElement('select')
+    : document.createElement('input');
+
+  if (newInput.tagName === 'SELECT') {
+    newInput.insertAdjacentHTML('afterbegin', options);
+  };
+
+  newInput.value = cellText;
+  newInput.classList.add('cell-input');
+
+  e.target.firstChild.replaceWith(newInput);
+
+  const checker = function() {
+    const notification = document.body.querySelector('.notification');
+
+    if (document.body.contains(notification)) {
+      notification.remove();
+    };
+
+    if (e.target.cellIndex === 0 || e.target.cellIndex === 1) {
+      if (newInput.value.length < 4) {
+        newInput.replaceWith(cellText);
+
+        pushNotification('Error', 'Min. length is 4 letters', 'error');
+      };
+
+      if (newInput.value && !isNaN(newInput.value)) {
+        newInput.replaceWith(cellText);
+
+        pushNotification('Error', 'Mustn`t be a number', 'error');
+      };
+    };
+
+    if (e.target.cellIndex === 3) {
+      if (isNaN(newInput.value)) {
+        newInput.replaceWith(cellText);
+
+        pushNotification('Erorr', 'Please, enter the number', 'error');
+      };
+
+      if (newInput.value < 18 || newInput.value > 90) {
+        newInput.replaceWith(cellText);
+
+        pushNotification('Erorr', 'Age only from 18 to 90', 'error');
       }
-    });
+    };
+
+    if (e.target.cellIndex === 4) {
+      if (isNaN(newInput.value)) {
+        newInput.replaceWith(cellText);
+
+        pushNotification('Erorr', 'Only numbers please', 'error');
+      }
+
+      newInput.replaceWith(`$${(+newInput.value).toLocaleString('en-US')}`);
+    };
+
+    newInput.replaceWith(newInput.value);
+  };
+
+  newInput.addEventListener('blur', () => {
+    checker();
+  });
+
+  newInput.addEventListener('keydown', ev => {
+    if (ev.code === 'Enter') {
+      checker();
+    }
   });
 });
