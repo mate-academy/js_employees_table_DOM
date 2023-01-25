a3e3bfc9bbd59169ee245241991506b369222efe
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-25 15:00:13 +0200

    Implement sortTable()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
-'use strict';
+"use strict";
 
-// write code here
+const employees = document.querySelector("table");
+
+sortTable(employees);
+
+function sortTable(table) {
+  let isAsc = true;
+
+  document.querySelectorAll("th").forEach((th) =>
+    th.addEventListener("click", () => {
+      const tbody = table.querySelector("tbody");
+
+      Array.from(tbody.querySelectorAll("tr"))
+        .sort(
+          getCompareFunction(
+            Array.from(th.parentNode.children).indexOf(th),
+            isAsc
+          )
+        )
+        .forEach((tr) => tbody.appendChild(tr));
+
+      isAsc = !isAsc;
+    })
+  );
+}
+
+function getCompareFunction(headerIndex, dir) {
+  return function (a, b) {
+    const aCellValue = getRowCellValue(dir ? a : b, headerIndex);
+    const bCellValue = getRowCellValue(dir ? b : a, headerIndex);
+
+    if (
+      aCellValue !== "" &&
+      bCellValue !== "" &&
+      !isNaN(aCellValue) &&
+      !isNaN(bCellValue)
+    ) {
+      return aCellValue - bCellValue;
+    }
+
+    return aCellValue[0] === "$"
+      ? parseInt(aCellValue.slice(1)) - parseInt(bCellValue.slice(1))
+      : aCellValue.toString().localeCompare(bCellValue);
+  };
+}
+
+function getRowCellValue(tr, index) {
+  return tr.children[index].textContent;
+}
