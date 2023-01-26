00b190526ade8cd099cd6d4d1a1bc880a048feb1
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-26 09:54:54 +0200

    Implement createForm()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,10 @@
 'use strict';
 
 const employees = document.querySelector('table');
-let previousSelectedRow;
-
-document.querySelector('tbody').addEventListener('click', (e) => {
-  if (previousSelectedRow) {
-    previousSelectedRow.classList.remove('active');
-  }
-
-  e.target.parentNode.classList.add('active');
-  previousSelectedRow = e.target.parentNode;
-});
 
 sortTable(employees);
+selectRow(employees);
+createForm(employees);
 
 function sortTable(table) {
   let isAsc = true;
@@ -42,15 +34,15 @@ function sortTable(table) {
 }
 
 function getCompareFunction(headerIndex, dir) {
-  return function (a, b) {
+  return function(a, b) {
     const aCellValue = getRowCellValue(dir ? a : b, headerIndex);
     const bCellValue = getRowCellValue(dir ? b : a, headerIndex);
 
     if (
-      aCellValue !== '' &&
-      bCellValue !== '' &&
-      !isNaN(aCellValue) &&
-      !isNaN(bCellValue)
+      aCellValue !== ''
+      && bCellValue !== ''
+      && !isNaN(aCellValue)
+      && !isNaN(bCellValue)
     ) {
       return aCellValue - bCellValue;
     }
@@ -63,4 +55,84 @@ function getCompareFunction(headerIndex, dir) {
 
 function getRowCellValue(tr, index) {
   return tr.children[index].textContent;
+}
+
+function selectRow(table) {
+  let previousSelectedRow;
+
+  table.querySelector('tbody').addEventListener('click', (e) => {
+    if (previousSelectedRow) {
+      previousSelectedRow.classList.remove('active');
+    }
+
+    e.target.parentNode.classList.add('active');
+    previousSelectedRow = e.target.parentNode;
+  });
+}
+
+function createForm(table) {
+  const form = document.createElement('form');
+
+  form.classList.add('new-employee-form');
+
+  form.innerHTML = `
+    <label>
+      Name: 
+      <input 
+        name="name" 
+        type="text"
+        data-qa="name"
+        required
+      >
+    </label>
+
+    <label>
+      Position: 
+      <input 
+        name="position" 
+        type="text"
+        data-qa="position"
+        required 
+      >
+    </label>
+
+    <label>
+      Office:
+      <select 
+        name="office" 
+        data-qa="office"
+        required
+      >
+        <option>Tokyo</option>
+        <option>San Francisco</option>
+        <option>Singapore</option>
+        <option>New York</option>
+        <option>London</option>
+        <option>Edinburgh</option>
+      </select>
+    </label>
+    <label>
+      Age: 
+      <input 
+        name="age" 
+        type="number"
+        data-qa="age"
+        required
+      >
+    </label>
+    <label>
+      Salary: 
+      <input 
+        name="salary" 
+        type="number" 
+        data-qa="salary"
+        required
+      >
+    </label>
+    <button type="submit">
+      Save to table
+    </button>
+  `;
+
+  table.parentNode.insertBefore(form, table.nextSibling);
 }
