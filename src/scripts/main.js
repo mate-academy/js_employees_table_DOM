94c7b89d87a9299fa4a9bc4c3658391df337cead
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-31 08:30:56 +0200

    Use event delegation in sortTable(table)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-const employees = document.querySelector("table");
+const employees = document.querySelector('table');
 
 sortTable(employees);
 selectRow(employees);
@@ -9,47 +9,45 @@ editTableCell(employees);
 
 function sortTable(table) {
   let isAsc = true;
-  let columnTitle = "";
+  let columnTitle = '';
 
-  document.querySelectorAll("th").forEach((th) => {
-    return th.addEventListener("click", (e) => {
-      const tbody = table.querySelector("tbody");
+  return document.querySelector('table').addEventListener('click', (e) => {
+    const tbody = table.querySelector('tbody');
 
-      if (columnTitle !== e.target.textContent) {
-        isAsc = true;
-        columnTitle = e.target.textContent;
-      }
+    if (columnTitle !== e.target.textContent) {
+      isAsc = true;
+      columnTitle = e.target.textContent;
+    }
 
-      Array.from(tbody.querySelectorAll("tr"))
-        .sort(
-          getCompareFunction(
-            Array.from(th.parentNode.children).indexOf(th),
-            th.textContent,
-            isAsc
-          )
+    Array.from(tbody.querySelectorAll('tr'))
+      .sort(
+        getCompareFunction(
+          Array.from(e.target.parentNode.children).indexOf(e.target),
+          e.target.textContent,
+          isAsc
         )
-        .forEach((tr) => tbody.appendChild(tr));
+      )
+      .forEach((tr) => tbody.appendChild(tr));
 
-      isAsc = !isAsc;
-    });
+    isAsc = !isAsc;
   });
 }
 
 function getCompareFunction(headerIndex, headerName, dir) {
-  return function (a, b) {
+  return function(a, b) {
     const aCellValue = getRowCellValue(dir ? a : b, headerIndex);
     const bCellValue = getRowCellValue(dir ? b : a, headerIndex);
 
     switch (headerName) {
-      case "Name":
-      case "Position":
-      case "Office":
+      case 'Name':
+      case 'Position':
+      case 'Office':
         return aCellValue.toString().localeCompare(bCellValue);
 
-      case "Age":
+      case 'Age':
         return aCellValue - bCellValue;
 
-      case "Salary":
+      case 'Salary':
         return parseInt(aCellValue.slice(1)) - parseInt(bCellValue.slice(1));
     }
   };
@@ -62,20 +60,20 @@ function getRowCellValue(tr, index) {
 function selectRow(table) {
   let previousSelectedRow;
 
-  table.querySelector("tbody").addEventListener("click", (e) => {
+  table.querySelector('tbody').addEventListener('click', (e) => {
     if (previousSelectedRow) {
-      previousSelectedRow.classList.remove("active");
+      previousSelectedRow.classList.remove('active');
     }
 
-    e.target.parentNode.classList.add("active");
+    e.target.parentNode.classList.add('active');
     previousSelectedRow = e.target.parentNode;
   });
 }
 
 function createForm(table) {
-  const newEmployeeForm = document.createElement("form");
+  const newEmployeeForm = document.createElement('form');
 
-  newEmployeeForm.classList.add("new-employee-form");
+  newEmployeeForm.classList.add('new-employee-form');
 
   newEmployeeForm.innerHTML = `
     <label>
@@ -142,7 +140,7 @@ function createForm(table) {
 }
 
 function addEmployee(form) {
-  form.querySelector("button").addEventListener("click", (e) => {
+  form.querySelector('button').addEventListener('click', (e) => {
     e.preventDefault();
 
     const employeeName = document.querySelector('[name="name"]');
@@ -155,9 +153,9 @@ function addEmployee(form) {
       pushNotification(
         150,
         10,
-        "Name input error",
-        "Name must not be less than 4 letters",
-        "error"
+        'Name input error',
+        'Name must not be less than 4 letters',
+        'error'
       );
 
       return;
@@ -167,37 +165,37 @@ function addEmployee(form) {
       pushNotification(
         150,
         10,
-        "Position input error",
-        "Position must not be less than 4 letters",
-        "error"
+        'Position input error',
+        'Position must not be less than 4 letters',
+        'error'
       );
 
       return;
     }
 
     if (
-      employeeAge.value.trim() === "" ||
-      parseInt(employeeAge.value) < 18 ||
-      parseInt(employeeAge.value) > 90
+      employeeAge.value.trim() === ''
+      || parseInt(employeeAge.value) < 18
+      || parseInt(employeeAge.value) > 90
     ) {
       pushNotification(
         150,
         10,
-        "Age input error",
-        "Age must not be less than 18 and more than 90",
-        "error"
+        'Age input error',
+        'Age must not be less than 18 and more than 90',
+        'error'
       );
 
       return;
     }
 
-    if (employeeSalary.value.trim() === "") {
+    if (employeeSalary.value.trim() === '') {
       pushNotification(
         150,
         10,
-        "Salary input error",
-        "Salary must not be empty",
-        "error"
+        'Salary input error',
+        'Salary must not be empty',
+        'error'
       );
 
       return;
@@ -209,45 +207,45 @@ function addEmployee(form) {
         <td>${employeePosition.value}</td>
         <td>${employeeOffice.value}</td>
         <td>${employeeAge.value}</td>
-        <td>$${parseInt(employeeSalary.value).toLocaleString("en-US")}</td>
+        <td>$${parseInt(employeeSalary.value).toLocaleString('en-US')}</td>
       </tr>
     `;
 
     document
-      .querySelector("tbody")
-      .insertAdjacentHTML("beforeend", newEmployeeRow);
+      .querySelector('tbody')
+      .insertAdjacentHTML('beforeend', newEmployeeRow);
 
     pushNotification(
       150,
       10,
-      "Success!",
-      "New employee successfully added to the table",
-      "success"
+      'Success!',
+      'New employee successfully added to the table',
+      'success'
     );
 
-    employeeName.value = "";
-    employeePosition.value = "";
-    employeeOffice.value = "Tokyo";
-    employeeAge.value = "";
-    employeeSalary.value = "";
+    employeeName.value = '';
+    employeePosition.value = '';
+    employeeOffice.value = 'Tokyo';
+    employeeAge.value = '';
+    employeeSalary.value = '';
   });
 }
 
 function pushNotification(posTop, posRight, title, description, type) {
-  const body = document.querySelector("body");
-  const message = document.createElement("div");
+  const body = document.querySelector('body');
+  const message = document.createElement('div');
 
   message.classList = `notification ${type}`;
-  message.dataset.qa = "notification";
-  message.style.top = posTop + "px";
-  message.style.right = posRight + "px";
+  message.dataset.qa = 'notification';
+  message.style.top = posTop + 'px';
+  message.style.right = posRight + 'px';
 
-  const messageTitle = document.createElement("h2");
+  const messageTitle = document.createElement('h2');
 
-  messageTitle.className = "title";
+  messageTitle.className = 'title';
   messageTitle.textContent = title;
 
-  const messageDescription = document.createElement("p");
+  const messageDescription = document.createElement('p');
 
   messageDescription.textContent = description;
 
@@ -255,47 +253,47 @@ function pushNotification(posTop, posRight, title, description, type) {
   body.append(message);
 
   setTimeout(() => {
-    message.style.visibility = "hidden";
+    message.style.visibility = 'hidden';
   }, 2000);
 }
 
 function editTableCell(table) {
-  const tbody = table.querySelector("tbody");
-  const input = document.createElement("input");
+  const tbody = table.querySelector('tbody');
+  const input = document.createElement('input');
 
   let isEditing = false;
   let editedCell;
-  let previousTextContent = "";
+  let previousTextContent = '';
 
-  tbody.addEventListener("dblclick", (e) => {
-    if (!isEditing && e.target.tagName === "TD") {
+  tbody.addEventListener('dblclick', (e) => {
+    if (!isEditing && e.target.tagName === 'TD') {
       isEditing = true;
       editedCell = e.target;
       previousTextContent = editedCell.textContent;
-      input.classList = "cell-input";
+      input.classList = 'cell-input';
       input.value = previousTextContent;
-      editedCell.textContent = "";
+      editedCell.textContent = '';
       editedCell.appendChild(input);
       input.focus();
     }
   });
 
-  tbody.addEventListener("keydown", (e) => {
-    if (e.code !== "Enter" || !isEditing) {
+  tbody.addEventListener('keydown', (e) => {
+    if (e.code !== 'Enter' || !isEditing) {
       return;
     }
 
     setEditedCellValue();
   });
 
-  input.addEventListener("blur", (e) => {
+  input.addEventListener('blur', (e) => {
     setEditedCellValue();
   });
 
   function setEditedCellValue() {
     if (isEditing) {
-      editedCell.textContent =
-        input.value.trim() !== "" ? input.value.trim() : previousTextContent;
+      editedCell.textContent
+        = input.value.trim() !== '' ? input.value.trim() : previousTextContent;
       isEditing = false;
     }
   }
