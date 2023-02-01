a305da279f8f319219ac539c655a30e835cfa98a
Vitalii Skomskiy <skomskiy@ukr.net>
2023-02-01 23:26:42 +0200

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,115 +3,67 @@
 const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
 const tr = tbody.rows;
-const th = thead.querySelectorAll('th');
 const ErrorString = 'Error';
 const errorString = 'error';
 
 thead.addEventListener('click', e => {
-  if (e.target.getAttribute('data-sorted') === 'ASC') {
-    if (e.target.textContent === 'Name') {
-      const sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          b.children[0].textContent < a.children[0].textContent ? -1 : 1);
-      };
+  const sortNames = (arr, column) => {
+    if (e.target.getAttribute('data-sorted') === 'ASC') {
+      e.target.setAttribute('data-sorted', 'DESC');
 
-      tbody.append(...sortNames(tr));
-    };
+      return [...arr].sort((a, b) =>
+        b.children[column].textContent < a.children[column].textContent
+          ? -1
+          : 1
+      );
+    } else {
+      e.target.setAttribute('data-sorted', 'ASC');
 
-    if (e.target.textContent === 'Position') {
-      const sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          b.children[1].textContent < a.children[1].textContent ? -1 : 1);
-      };
-
-      tbody.append(...sortNames(tr));
-    };
-
-    if (e.target.textContent === 'Office') {
-      const sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          b.children[2].textContent < a.children[2].textContent ? -1 : 1);
-      };
-
-      tbody.append(...sortNames(tr));
-    };
-
-    if (e.target.textContent === 'Age') {
-      const sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          b.children[3].textContent < a.children[3].textContent ? -1 : 1);
-      };
-
-      tbody.append(...sortNames(tr));
-    };
-
-    if (e.target.textContent === 'Salary') {
-      const sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          +b.children[4].textContent.slice(1).split(',').join('')
-            - +a.children[4].textContent.slice(1).split(',').join(''));
-      };
-
-      tbody.append(...sortNames(tr));
-    };
-
-    for (let i = 0; i < th.length; i++) {
-      th[i].removeAttribute('data-sorted');
+      return [...arr].sort((a, b) =>
+        a.children[column].textContent < b.children[column].textContent
+          ? -1
+          : 1
+      );
     }
+  };
 
-    e.target.setAttribute('data-sorted', 'DESC');
-  } else {
-    if (e.target.textContent === 'Name') {
-      const sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          a.children[0].textContent < b.children[0].textContent ? -1 : 1);
-      };
+  switch (e.target.textContent) {
+    case 'Name':
+      let column = 0;
 
-      tbody.append(...sortNames(tr));
-    };
+      tbody.append(...sortNames(tr, column));
+      break;
+    case 'Position':
+      column = 1;
+      tbody.append(...sortNames(tr, column));
+      break;
+    case 'Office':
+      column = 2;
+      tbody.append(...sortNames(tr, column));
+      break;
+    case 'Age':
+      column = 3;
+      tbody.append(...sortNames(tr, column));
+      break;
+    case 'Salary':
+      const sortSalary = function(arr) {
+        if (e.target.getAttribute('data-sorted') === 'ASC') {
+          e.target.setAttribute('data-sorted', 'DESC');
 
-    if (e.target.textContent === 'Position') {
-      const sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          a.children[1].textContent < b.children[1].textContent ? -1 : 1);
-      };
+          return [...arr].sort((a, b) =>
+            +b.children[4].textContent.slice(1).split(',').join('')
+          - +a.children[4].textContent.slice(1).split(',').join(''));
+        } else {
+          e.target.setAttribute('data-sorted', 'ASC');
 
-      tbody.append(...sortNames(tr));
-    };
-
-    if (e.target.textContent === 'Office') {
-      const sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          a.children[2].textContent < b.children[2].textContent ? -1 : 1);
-      };
-
-      tbody.append(...sortNames(tr));
-    };
-
-    if (e.target.textContent === 'Age') {
-      const sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          a.children[3].textContent < b.children[3].textContent ? -1 : 1);
-      };
-
-      tbody.append(...sortNames(tr));
-    };
-
-    if (e.target.textContent === 'Salary') {
-      const sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          +a.children[4].textContent.slice(1).split(',').join('')
+          return [...arr].sort((a, b) =>
+            +a.children[4].textContent.slice(1).split(',').join('')
             - +b.children[4].textContent.slice(1).split(',').join(''));
+        }
       };
 
-      tbody.append(...sortNames(tr));
-    };
-
-    for (let i = 0; i < th.length; i++) {
-      th[i].removeAttribute('data-sorted');
-    }
-
-    e.target.setAttribute('data-sorted', 'ASC');
+      tbody.append(...sortSalary(tr));
+      break;
   }
 });
 
