87ae605ad41d7910f96ab8488071a0ed611016b3
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-02-20 09:38:15 +0200

    Sorting in two direction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+const rows = [...tbody.querySelectorAll('tr')];
+
+let sortDirection = 'toDown';
+
+table.addEventListener('click', e => {
+  if (e.target.tagName === 'TH') {
+    const headerIndex = [...e.target.parentNode.children].indexOf(e.target);
+
+    rows.sort((a, b) => {
+      const aData = a.children[headerIndex].textContent;
+      const bData = b.children[headerIndex].textContent;
+
+      const sortOrder = sortDirection === 'toDown' ? 1 : -1;
+
+      return sortOrder * aData.localeCompare(bData, undefined, {
+        numeric: true,
+        sensitivity: 'base',
+      });
+    });
+
+    sortDirection === 'toDown'
+      ? sortDirection = 'toUp'
+      : sortDirection = 'toDown';
+
+    rows.forEach((row) => tbody.appendChild(row));
+  }
+});
