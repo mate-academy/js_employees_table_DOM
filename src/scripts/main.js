f5ff50755501d207cb227f7f7b1a22dfdd136dab
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-02-20 14:45:42 +0200

    Form for NewEmployee was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -53,5 +53,80 @@ const selectRow = (e) => {
   }
 };
 
+document.body.insertAdjacentHTML('beforeend', `
+  <form
+    class="new-employee-form"
+    action="#"
+    method="POST"
+  >
+    <label>Name:
+      <input
+        name="name"
+        type="text"
+        data-qa="name"
+      ></label>
+
+    <label>Position:
+      <input
+      name="position"
+      type="text"
+      data-qa="position"
+      ></label>
+
+    <label>Office:
+      <select name="office" data-qa="office">
+        <option value="Tokyo">Tokyo</option>
+        <option value="London">London</option>
+        <option value="New York">New York</option>
+        <option value="Edinburgh">Edinburgh</option>
+        <option value="San Francisco">San Francisco</option>
+      </select>
+    </label>
+
+    <label>Age:
+    <input
+      name="age"
+      type="number"
+      data-qa="age"
+      ></label>
+
+    <label>Salary:
+      <input
+        name="salary"
+        type="number"
+        data-qa="salary"
+      ></label>
+
+      <button type="submit">Save to table</button>
+  </form>
+`);
+
+const form = document.querySelector('form');
+
+const addNewEmployee = (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  const newEmployee = Object.values(Object.fromEntries(data.entries()));
+
+  newEmployee[4] = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(newEmployee[4]);
+
+  const row = tbody.insertRow(-1);
+
+  [...table.rows[0].cells].forEach((_, i) => {
+    row.insertCell(i);
+    row.children[i].innerText = newEmployee[i];
+  });
+
+  form.reset();
+};
+
 table.addEventListener('click', sortTable);
 table.addEventListener('click', selectRow);
+form.addEventListener('submit', addNewEmployee);
