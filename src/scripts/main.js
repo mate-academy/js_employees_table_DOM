67520e4bb9b4fbd3b012e853ae976a41f102fa2f
mykolachupyak <chupyak@gmail.com>
2023-02-25 11:44:28 +0200

    commit 6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,11 @@ const arrayForCompare = [1, 1, 1, 1, 1];
 const asc = ' \u25BC';
 const desc = ' \u25B2';
 
-function convert(stringNumber) {
-  return Number(stringNumber.toLocaleString().replace(/\D/g, ''));
-}
-
 function compareVariables(aa, bb, eventFunc, ascOrDesc) {
+  const convert = (stringNumber) => {
+    return Number(stringNumber.toLocaleString().replace(/\D/g, ''));
+  };
+
   let a = aa.children[eventFunc.target.cellIndex].textContent;
   let b = bb.children[eventFunc.target.cellIndex].textContent;
 
@@ -20,30 +20,28 @@ function compareVariables(aa, bb, eventFunc, ascOrDesc) {
   return a < b ? ascOrDesc : ascOrDesc * (-1);
 }
 
-function ascDesc(table, cellNumber, eventFunc) {
-  for (let i = 0; i < arrayForCompare.length; i++) {
-    arrayForCompare[i] = cellNumber === i ? arrayForCompare[i] * (-1) : 1;
-  }
-
-  table.querySelectorAll('span')[cellNumber]
-    .textContent = arrayForCompare[cellNumber] === 1 ? asc : desc;
-
-  document.querySelector('tbody')
-    .append(...[...document.querySelector('tbody').children].sort((a, b) =>
-      compareVariables(a, b, eventFunc, arrayForCompare[cellNumber])));
-}
-
 function sortingTable(table) {
   table.querySelectorAll('th').forEach(function(item) {
     item.append(document.createElement('span'));
   });
 
   table.addEventListener('click', (eventFunc) => {
+    const cellNumber = eventFunc.target.cellIndex;
+
     document.querySelectorAll('span').forEach((element) => {
       element.textContent = '';
     });
 
-    ascDesc(table, eventFunc.target.cellIndex, eventFunc,);
+    for (let i = 0; i < arrayForCompare.length; i++) {
+      arrayForCompare[i] = cellNumber === i ? arrayForCompare[i] * (-1) : 1;
+    }
+
+    table.querySelectorAll('span')[cellNumber]
+      .textContent = arrayForCompare[cellNumber] === 1 ? asc : desc;
+
+    document.querySelector('tbody')
+      .append(...[...document.querySelector('tbody').children].sort((a, b) =>
+        compareVariables(a, b, eventFunc, arrayForCompare[cellNumber])));
   });
 }
 
