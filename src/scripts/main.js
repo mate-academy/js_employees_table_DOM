f3e06eadc34730be5cc97434eb9cb4912fa19cff
Yevhen-Ivanchuk <e.ivanchyk@gmail.com>
2023-03-13 12:14:34 +0200

    Employees table 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,427 +1,354 @@
 'use strict';
 
-const body = document.querySelector('body');
-const thead = document.querySelector('thead');
-const tbody = document.querySelector('tbody');
-const rows = [...tbody.rows];
-const nameLength = 4;
-const minAge = 18;
-const maxAge = 90;
-const timeoutOfMessage = 3000;
-let compareIndex = -1;
-let isValid;
-let editingArea;
-let isCorrect = true;
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+const tableBody = table.querySelector('tbody');
 
-// table sorting
-
-thead.addEventListener('click', eventClick => {
-  const th = eventClick.target.closest('th');
-  const index = th.cellIndex;
-
-  sortTableAsc(index, th.innerText, compareIndex === index);
-
-  compareIndex = (compareIndex === index) ? -1 : index;
+headers.forEach((header) => {
+  header.dataset.sorted = 'DESC';
 });
 
-function sortTableAsc(index, columnName, dscSorting) {
-  rows.sort((first, second) => {
-    const a = first.children[index].innerHTML;
-    const b = second.children[index].innerHTML;
+function sortColumn(index, isASC) {
+  let sortedRows = [];
 
-    switch (columnName) {
-      case 'Name':
-      case 'Position':
-      case 'Office':
-        return a.localeCompare(b);
+  sortedRows = Array.from(tableBody.rows).sort((a, b) => {
+    const first = a.cells[index].innerHTML;
+    const second = b.cells[index].innerHTML;
 
-      case 'Age':
-        return a - b;
-
-      case 'Salary':
-        return normalizeNumber(a) - normalizeNumber(b);
+    if (isASC) {
+      return index === 4
+        ? (salaryToNumber(first) - salaryToNumber(second))
+        : first.localeCompare(second);
+    } else {
+      return index === 4
+        ? (salaryToNumber(second) - salaryToNumber(first))
+        : second.localeCompare(first);
     }
   });
 
-  if (dscSorting) {
-    rows.reverse();
-  }
-
-  tbody.append(...rows);
-
-  function normalizeNumber(string) {
-    return +string.slice(1).split(',').join('');
-  }
+  table.tBodies[0].append(...sortedRows);
 }
 
-// show active row
-
-tbody.addEventListener('click', eventClick => {
-  const tr = eventClick.target.closest('tr');
-
-  rows.forEach(row => {
-    row.removeAttribute('class');
-  });
-
-  tr.classList.add('active');
-});
-
-// create form and validate data
-
-const addEmployee = document.createElement('form');
-
-addEmployee.classList.add('new-employee-form');
-
-addEmployee.innerHTML = `
-  <label> Name:
-    <input type="text" name = "name" data-qa="name" required>
-  </label>
-  <label> Position:
-    <input type="text" name = "position" data-qa="position">
-  </label>
-  <label> Office:
-    <select name = "office" data-qa="office" required>
-      <option value="" selected disabled>Choose office</option>
-      <option value="Tokyo">Tokyo</option>
-      <option value="Singapore">Singapore</option>
-      <option value="London">London</option>
-      <option value="New York">New York</option>
-      <option value="Edinburgh">Edinburgh</option>
-      <option value="San Francisco">San Francisco</option>
-    </select>
-  </label>
-  <label> Age:
-    <input type="number" name = "age" data-qa="age" required>
-  </label>
-  <label> Salary:
-    <input type="number" name = "salary" data-qa="salary" required>
-  </label>
-  <button type="submit">Save to table</button>
-`;
-
-body.append(addEmployee);
-
-function validation(data) {
-  if (data.get('name').trim().length < nameLength
-    || /\d/.test(data.get('name'))) {
-    pushNotification(
-      580,
-      10,
-      'Enter a correct name!',
-      'Name must contain at least 4 letters and not include numbers',
-      'error'
-    );
-
-    isValid = false;
-  } else if (data.get('age') < minAge
-  || data.get('age') > maxAge) {
-    pushNotification(
-      580,
-      10,
-      'Enter a correct age!',
-      'Your age must be from 18 to 90 years',
-      'error'
-    );
-
-    isValid = false;
-  } else if (data.get('position').trim().length < nameLength
-    || /\d/.test(data.get('position'))) {
-    pushNotification(
-      580,
-      10,
-      'Enter a position!',
-      'You should enter a correct position',
-      'error'
-    );
-
-    isValid = false;
-  } else if (data.get('salary') <= 0) {
-    pushNotification(
-      580,
-      10,
-      'Enter a correct salary!',
-      'Salary must be more than 0',
-      'error'
-    );
-
-    isValid = false;
-  } else {
-    isValid = true;
-  }
+function salaryToNumber(text) {
+  return +text.slice(1).replace(/,/g, '');
 };
 
-addEmployee.addEventListener('submit', submitEvent => {
-  const employeeInfo = new FormData(addEmployee);
-  const salaryToNormalize = employeeInfo.get('salary');
-  let normalizedSalary = salaryToNormalize;
+headers.forEach(function(header, index) {
+  header.addEventListener('click', (e) => {
+    let order;
 
-  if (salaryToNormalize.length > 3) {
-    normalizedSalary
-    = salaryToNormalize.slice(0, -3)
-    + ','
-    + salaryToNormalize.slice(-3, salaryToNormalize.length);
-  }
-
-  const salary = `$${normalizedSalary}`;
-
-  submitEvent.preventDefault();
-  validation(employeeInfo);
-
-  if (!isValid) {
-    return;
-  }
-
-  const newRow = tbody.insertRow();
-
-  newRow.insertCell(0).innerText = employeeInfo.get('name');
-  newRow.insertCell(1).innerText = employeeInfo.get('position');
-  newRow.insertCell(2).innerText = employeeInfo.get('office');
-  newRow.insertCell(3).innerText = employeeInfo.get('age');
-  newRow.insertCell(4).innerText = salary;
-
-  rows.push(newRow);
-
-  addEmployee.reset();
-
-  pushNotification(
-    460,
-    10,
-    'Validation success!',
-    'Congrats! New employee is successfully added into the table',
-    'success');
+    if (e.target.dataset.sorted === 'ASC') {
+      e.target.dataset.sorted = 'DESC';
+      order = false;
+    } else {
+      e.target.dataset.sorted = 'ASC';
+      order = true;
+    }
+    sortColumn(index, order);
+  });
 });
 
 function pushNotification(posTop, posRight, title, description, type) {
-  const message = document.createElement('div');
-  const messageTitle = document.createElement('h2');
-  const messageDescription = document.createElement('p');
+  document.body.insertAdjacentHTML('afterbegin', `
+    <div data-qa="notification" class="notification ${type}"
+      style="top: ${posTop}px; right: ${posRight}px;">
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+    </div>
+    `);
 
-  message.style.top = `${posTop}px`;
-  message.style.right = `${posRight}px`;
+  const notification = document.querySelector('.notification');
 
-  messageTitle.innerText = title;
-  messageDescription.innerText = description;
+  setTimeout(() => notification.remove(), 2000);
+}
 
-  message.classList.add('notification', type);
-  message.setAttribute('data-qa', 'notification');
-  messageTitle.classList.add('title');
+table.insertAdjacentHTML('afterend', `
+  <form class="new-employee-form">
+    <label>Name:
+      <input
+        data-qa="name"
+        name="name"
+        type="text"
+        required
+      >
+    </label>
+    <label>Position:
+      <input
+        data-qa="position"
+        name="position"
+        type="text"
+        required
+      >
+    </label>
+    <label>Office:
+      <select data-qa="office" name="office" type="text">
+        <option disabled selected value="" hidden></option>
+        <option value="Tokyo">Tokyo</option>
+        <option value="Singapore">Singapore</option>
+        <option value="London">London</option>
+        <option value="New York">New York</option>
+        <option value="Edinburgh">Edinburgh</option>
+        <option value="San Francisco">San Francisco</option>
+      </select>
+    </label>
+    <label>Age:
+      <input
+        data-qa="age"
+        name="age"
+        type="number"
+        required
+      >
+    </label>
+    <label>Salary:
+      <input
+        data-qa="salary"
+        name="salary"
+        type="number"
+        required
+      >
+    </label>
+    <button>Save to table</button>
+  </form>
+ `);
 
-  message.append(messageTitle, messageDescription);
-  document.body.append(message);
+const form = document.querySelector('.new-employee-form');
 
-  setTimeout(() => {
-    message.remove();
-  }, timeoutOfMessage);
-};
+const button = form.querySelector('button');
 
-// change, validate and save data in cell-input
+button.addEventListener('click', (e) => {
+  e.preventDefault();
 
-tbody.addEventListener('dblclick', doubleClick => {
-  const target = doubleClick.target.closest('td');
-  const input = document.querySelector('.cell-input');
+  const personName = form.querySelector('[data-qa="name"]');
+  const position = form.querySelector('[data-qa="position"]');
+  const office = form.querySelector('[data-qa="office"]');
+  const age = form.querySelector('[data-qa="age"]');
+  const salary = form.querySelector('[data-qa="salary"]');
 
-  if (input) {
-    pushNotification(
-      700,
-      10,
-      'Unfinished changes',
-      'You cannot change another cell until this one is not finished',
-      'error'
-    );
+  if (formValidation(personName, position, age, salary)) {
+    saveDataToTable(personName, position, office, age, salary);
+    form.reset();
+    unselectTable();
+  };
+});
 
-    return editStart(input.parentElement);
+function formValidation(personName, personPosition, personAge, personSalary) {
+  if (personName.value.trim().length < 4) {
+    const nameStyle = form.querySelector('[data-qa="name"]');
+
+    nameStyle.style.borderColor = '#e25644';
+
+    setTimeout(() => {
+      nameStyle.style.borderColor = '';
+    }, 2000);
+
+    pushNotification(450, 20, 'Something went wrong',
+      `Name: less than 4 characters`, 'error');
+
+    return false;
   }
 
-  if (!target) {
+  if (personPosition.value.trim().length < 1) {
+    const positionStyle = form.querySelector('[data-qa="position"]');
+
+    positionStyle.style.borderColor = '#e25644';
+
+    setTimeout(() => {
+      positionStyle.style.borderColor = '';
+    }, 2000);
+
+    pushNotification(450, 20, 'Something went wrong',
+      `Position: cannot be empty`, 'error');
+
+    return false;
+  }
+
+  if (personAge.value < 18
+    || personAge.value > 90) {
+    const ageStyle = form.querySelector('[data-qa="age"]');
+
+    ageStyle.style.borderColor = '#e25644';
+
+    setTimeout(() => {
+      ageStyle.style.borderColor = '';
+    }, 2000);
+
+    pushNotification(450, 20, 'Something went wrong',
+      'Age: must be between 18 and 90', 'error');
+
+    return false;
+  }
+
+  if (personSalary.value.length < 1 || personSalary.value < 0) {
+    const salaryStyle = form.querySelector('[data-qa="salary"]');
+
+    salaryStyle.style.borderColor = '#e25644';
+
+    setTimeout(() => {
+      salaryStyle.style.borderColor = '';
+    }, 2000);
+
+    pushNotification(450, 20, 'Something went wrong',
+      `Saary cannot be empty or negative`, 'error');
+
+    return false;
+  }
+
+  pushNotification(450, 20, 'Wow - this is a success',
+    'New data is added to the table', 'success');
+
+  return true;
+};
+
+function saveDataToTable(personName, position, office, age, salary) {
+  const tr = document.createElement('tr');
+
+  tr.innerHTML = `
+     <td>${personName.value}</td>
+     <td>${position.value}</td>
+     <td>${office.value}</td>
+     <td>${age.value}</td>
+     <td>$${numberWithComma(salary.value)}</td>
+   `;
+
+  tableBody.append(tr);
+};
+
+function numberWithComma(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+function unselectTable() {
+  const trows = tableBody.querySelectorAll('tr');
+
+  trows.forEach((tr) => {
+    tr.classList.remove('active');
+  });
+};
+
+tableBody.addEventListener('click', (e) => {
+  const row = e.target;
+
+  if (!row || !tableBody.contains(row)) {
     return;
   }
 
-  editStart(target);
+  unselectTable();
+  row.closest('tr').classList.toggle('active');
 });
 
-function editStart(td) {
-  editingArea = {
-    elem: td,
-    data: td.innerHTML,
+tableBody.addEventListener('dblclick', (e) => {
+  const row = e.target;
+
+  if (!row || !tableBody.contains(row)) {
+    return;
+  }
+
+  const trows = tableBody.querySelectorAll('td');
+  const index = Array.from(trows).findIndex(n => n === row) % 5;
+  const tempValue = row.innerText;
+  let input;
+
+  switch (index) {
+    case 1:
+      input = document.createElement('input');
+      input.type = 'text';
+      break;
+    case 2:
+      input = document.createElement('select');
+
+      input.innerHTML = `
+         <select>
+          <option disabled selected value="" hidden></option>
+          <option value="Tokyo">Tokyo</option>
+          <option value="Singapore">Singapore</option>
+          <option value="London">London</option>
+          <option value="New York">New York</option>
+          <option value="Edinburgh">Edinburgh</option>
+          <option value="San Francisco">San Francisco</option>
+         </select>
+       `;
+      break;
+    case 3:
+      input = document.createElement('input');
+      input.type = 'number';
+      input.placeholder = 'age value 18 - 90';
+      break;
+    case 4:
+      input = document.createElement('input');
+      input.type = 'number';
+      input.placeholder = 'just a number';
+      break;
+    default:
+      input = document.createElement('input');
+      input.type = 'text';
+  }
+
+  row.innerText = '';
+  row.append(input);
+  input.focus();
+
+  input.onblur = () => {
+    inputValidation(index);
   };
 
-  if (td.cellIndex === 2) {
-    const cellOffice = document.createElement('select');
-
-    cellOffice.className = 'cell-input';
-
-    cellOffice.innerHTML = `
-      <option value="Choose office" selected disabled>Choose office</option>
-      <option value="Tokyo">Tokyo</option>
-      <option value="Singapore">Singapore</option>
-      <option value="London">London</option>
-      <option value="New York">New York</option>
-      <option value="Edinburgh">Edinburgh</option>
-      <option value="San Francisco">San Francisco</option>
-    `;
-
-    td.innerHTML = '';
-    td.append(cellOffice);
-    cellOffice.focus();
-
-    cellOffice.onblur = () => {
-      const office = cellOffice.selectedIndex;
-
-      td.innerHTML = cellOffice[office].value;
-
-      if (cellOffice.selectedIndex === 0) {
-        td.innerHTML = editingArea.data;
-      }
-
-      pushNotification(
-        460,
-        10,
-        'Changed successfully!',
-        'Congrats! The office was successfully changed.',
-        'success');
-    };
-
-    cellOffice.onkeydown = (enter) => {
-      if (enter.key === 'Enter') {
-        cellOffice.onblur();
-      }
-    };
-  } else {
-    const cellInput = document.createElement('input');
-
-    cellInput.className = 'cell-input';
-    cellInput.innerHTML = td.innerHTML;
-    td.innerHTML = '';
-    td.append(cellInput);
-    cellInput.focus();
-
-    switch (td.cellIndex) {
-      case 0:
-      case 1:
-        cellInput.setAttribute('type', 'text');
-        break;
-
-      case 3:
-      case 4:
-        cellInput.setAttribute('type', 'number');
-        break;
-    }
-
-    cellInput.onblur = (e) => {
-      validate(td.cellIndex, cellInput);
-      editEnd(editingArea.elem, isCorrect);
-      changeIsCorrect();
-    };
-
-    cellInput.onkeydown = (enter) => {
-      if (enter.key === 'Enter') {
-        cellInput.onblur();
-      }
-    };
-  }
-};
-
-function validate(index, cell) {
-  switch (index) {
-    case 0:
-    case 1:
-      if (/\d/.test(cell.value)
-        || cell.value.trim().length < nameLength) {
-        isCorrect = false;
-
-        pushNotification(
-          580,
-          10,
-          'Enter a correct data!',
-          'This field must contain at least 4 letters and not include numbers.',
-          'error');
-      }
-
-      break;
-
-    case 3:
-      if (cell.value < 18
-        || cell.value > 90) {
-        if (cell.value === '') {
-          isCorrect = false;
-
-          return;
-        }
-
-        isCorrect = false;
-
-        pushNotification(
-          580,
-          10,
-          'Enter a correct age!',
-          'The age must be 18+ and less than 90.',
-          'error');
-      }
-
-      break;
-
-    case 4:
-      if (+cell.value <= 0) {
-        if (cell.value === '') {
-          isCorrect = false;
-
-          return;
-        }
-
-        isCorrect = false;
-
-        pushNotification(
-          580,
-          10,
-          'Enter a correct salary!',
-          'The salary must be more than 0.',
-          'error');
-      }
-
-      break;
-  }
-}
-
-function editEnd(td, isOk) {
-  if (isOk) {
-    switch (td.cellIndex) {
-      case 4:
-        td.innerHTML = `${(+td.value)}`;
-
-        pushNotification(
-          460,
-          10,
-          'Changed successfully!',
-          'Congrats! The salary was successfully changed.',
-          'success');
-        break;
-
-      case 0:
-      case 1:
-      case 3:
-        td.innerHTML = td.firstChild.value;
-
-        pushNotification(
-          460,
-          10,
-          'Changed successfully!',
-          'Congrats! The employee data was successfully changed.',
-          'success');
-        break;
-    }
-  } else {
-    if (td.firstChild.value === '') {
-      td.innerHTML = editingArea.data;
-
+  input.addEventListener('keydown', (evt) => {
+    if (evt.code !== 'Enter') {
       return;
     }
 
-    return editStart(td);
-  }
+    inputValidation(index);
+  });
 
-  editingArea = null;
-};
+  const inputValidation = (i) => {
+    if (!input.value) {
+      // eslint-disable-next-line no-return-assign
+      return row.innerText = tempValue;
+    }
 
-function changeIsCorrect() {
-  isCorrect = true;
-}
+    switch (i) {
+      case 0:
+        if (input.value.trim().length < 4) {
+          row.innerText = tempValue;
+
+          pushNotification(450, 20, 'Something went wrong',
+            `Name: less than 4 characters`, 'error');
+          break;
+        }
+        row.innerText = input.value;
+        break;
+      case 1:
+        if (input.value.trim().length < 1) {
+          row.innerText = tempValue;
+
+          pushNotification(450, 20, 'Something went wrong',
+            `Position: cannot be empty`, 'error');
+          break;
+        }
+        row.innerText = input.value;
+        break;
+      case 3:
+        if (input.value < 18 || input.value > 90) {
+          row.innerText = tempValue;
+
+          pushNotification(450, 20, 'Something went wrong',
+            'Age must be between 18 and 90', 'error');
+          break;
+        }
+        row.innerText = input.value;
+        break;
+      case 4:
+        if (input.value < 0) {
+          row.innerText = tempValue;
+
+          pushNotification(450, 20, 'Something went wrong',
+            'Check that the data you entered is correct', 'error');
+          break;
+        }
+        row.innerText = `$${numberWithComma(input.value)}`;
+        break;
+      default:
+        row.innerText = input.value;
+    }
+    input.remove();
+  };
+});
