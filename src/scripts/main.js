e94bb27702b3f851404c18f8e58c62cd8863a4a9
Mykola Huryn <mukola197022@gmail.com>
2023-03-22 20:50:43 +0100

    SAVE TO TABLE

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -43,7 +43,6 @@ const sortTableASC = function(index, type) {
 };
 
 const sortTableDESC = function(index, type) {
-
   const compare = function(rowA, rowB) {
     const rowADate = rowA.cells[index].innerHTML;
     const rowBDate = rowB.cells[index].innerHTML;
@@ -156,4 +155,116 @@ tbody.addEventListener('click', e => {
   [...tr].map(x => x.classList.remove('active'));
 
   e.target.parentNode.classList.add('active');
+});
+
+// PART 3. ADD FORM
+
+table.insertAdjacentHTML('afterend', `
+<form class="new-employee-form" action="get">
+  <label>Name: 
+    <input name="name" type="text" data-qa="name" required>
+  </label>
+  <label>Position: 
+    <input name="position" type="text" data-qa="position" required>
+  </label>
+  <label>Office:
+    <select name="office" data-qa="office" required>
+      <option value="Tokyo">Tokyo</option>
+      <option value="Singapore">Singapore</option>
+      <option value="London">London</option>
+      <option value="New York">New York</option>
+      <option value="Edinburgh">Edinburgh</option>
+      <option value="San Francisco">San Francisco</option>
+    </select>
+  </label>
+  <label>Age: 
+    <input name="age" type="number" data-qa="age" required>
+  </label>
+  <label>Salary: 
+    <input name="salary" type="number" data-qa="salary" required>
+  </label>
+  <button>Save to table</button>
+</form>
+`);
+
+// PART 4. NO RESET
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function handleSubmit(e) {
+  e.preventDefault();
+});
+
+// PART 5. SAVE TO TABLE
+const pushNotification = (title, description, type) => {
+  const body = document.querySelector('body');
+  const notification = document.createElement('div');
+  const header = document.createElement('h2');
+  const content = document.createElement('p');
+
+  body.append(notification);
+  notification.append(header, content);
+
+  header.textContent = title;
+  content.textContent = description;
+
+  notification.classList.add('notification', type);
+  header.classList.add('title');
+  notification.setAttribute('data-qa', 'notification');
+
+  setTimeout(() => {
+    notification.remove();
+  }, 2000);
+};
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', (e) => {
+  const newEmp = document.createElement('tr');
+
+  const inputName = document.getElementsByName('name');
+  const inputPosition = document.getElementsByName('position');
+  const inputAge = document.getElementsByName('age');
+  const inputOffice = document.getElementsByName('office');
+  const inputSalary = document.getElementsByName('salary');
+
+  let isValid = true;
+
+  if (inputName[0].value.length < 4) {
+    isValid = false;
+
+    pushNotification('Error message',
+      'Name should be at least 4 characters long.', 'error');
+  } else if (inputAge[0].value < 18 || inputAge[0].value > 90) {
+    isValid = false;
+
+    pushNotification('Error message',
+      'Age should be between 18 and 90.', 'error');
+  } else if (inputSalary[0].value < 1) {
+    isValid = false;
+
+    pushNotification('Error message',
+      'Salary should be at least $1.', 'error');
+  }
+
+  if (e.target === button && isValid) {
+    tbody.append(newEmp);
+
+    newEmp.insertAdjacentHTML('beforeend', `
+          <td>${inputName[0].value}</td>
+          <td>${inputPosition[0].value}</td>
+          <td>${inputOffice[0].value}</td>
+          <td>${inputAge[0].value}</td>
+          <td>$${inputSalary[0].value.slice(0, 3)},
+          ${inputSalary[0].value.slice(3)}</td>
+        `);
+
+    pushNotification('Success message',
+    'Employee added successfully!', 'success');
+    form.reset();
+  }
 });
