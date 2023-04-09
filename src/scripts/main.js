c65cd28fe3710bb4ef402e18656f593b4a0d416e
Viktor <vasha2020@gmail.com>
2023-04-09 18:27:00 +0100

    task-2 is done!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
+const log = console.log;
+
 // Sorting:
 const tableHead = document.querySelector('thead');
+const allRows = [...document.querySelector('table').rows];
 
 const sortingColumns = (e) => {
   const sortByColumn = e.target.innerText;
-  const allRows = [...document.querySelector('table').rows];
   const tableDataToSort = [];
-
-  // console.log(e.target);
 
   for (let i = 1; i < allRows.length - 1; i++) {
     const cells = allRows[i].cells;
@@ -23,18 +23,32 @@ const sortingColumns = (e) => {
     tableDataToSort.push(person);
   }
 
-  console.log('clicked is: ', e.target.dataset.clicked);
+  if (e.target.dataset.clicked === 'true') {
+    if (sortByColumn === 'Salary') {
+      tableDataToSort.sort((a, b) => {
+        const toNumberA = a.Salary.match(/\d+/g).join('');
+        const toNumberB = b.Salary.match(/\d+/g).join('');
 
-  if (sortByColumn === 'Salary') {
-    tableDataToSort.sort((a, b) => {
-      const toNumberA = a.Salary.match(/\d+/g).join('');
-      const toNumberB = b.Salary.match(/\d+/g).join('');
-
-      return toNumberA - toNumberB;
-    });
+        return toNumberB - toNumberA;
+      });
+    } else {
+      tableDataToSort.sort((a, b) =>
+        b[sortByColumn].localeCompare(a[sortByColumn]));
+    }
+    e.target.dataset.clicked = false;
   } else {
-    tableDataToSort.sort((a, b) =>
-      a[sortByColumn].localeCompare(b[sortByColumn]));
+    if (sortByColumn === 'Salary') {
+      tableDataToSort.sort((a, b) => {
+        const toNumberA = a.Salary.match(/\d+/g).join('');
+        const toNumberB = b.Salary.match(/\d+/g).join('');
+
+        return toNumberA - toNumberB;
+      });
+    } else {
+      tableDataToSort.sort((a, b) =>
+        a[sortByColumn].localeCompare(b[sortByColumn]));
+    }
+    e.target.dataset.clicked = true;
   }
 
   for (let i = 1; i < allRows.length - 1; i++) {
@@ -48,9 +62,29 @@ const sortingColumns = (e) => {
       <td>${tableDataToSort[i - 1].Salary}</td>
     `;
   }
-
-  e.target.dataset['clicked'] = true;
-  console.log(tableDataToSort);
 };
 
 tableHead.addEventListener('click', sortingColumns);
+
+// 2 task
+
+const tableBody = document.querySelector('tbody');
+
+const selectRow = (e) => {
+  const activatedRow = allRows.find(el => el.className === 'active');
+  const pressedRow = e.target.parentElement;
+
+  if (pressedRow.className) {
+    pressedRow.className = '';
+
+    return;
+  }
+
+  if (activatedRow) {
+    activatedRow.className = '';
+  }
+
+  pressedRow.className = 'active';
+};
+
+tableBody.addEventListener('click', selectRow);
