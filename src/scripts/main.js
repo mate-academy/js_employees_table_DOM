45e5d64762afa210186f9e27e5c207cbbcbb6699
AnastassiaH <nastyagurska@gmail.com>
2023-04-17 18:36:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,189 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tableBody = table.children[1];
+const tableHead = table.children[0];
+
+// sort table function
+
+const sortTable = (sortBy, order) => {
+  const index = [...tableHead.firstElementChild.children]
+    .findIndex(item => item.textContent === sortBy);
+  const rows = [...tableBody.children];
+
+  rows.sort((a, b) => {
+    let valueA = a.children[index].textContent;
+    let valueB = b.children[index].textContent;
+
+    valueB = !order ? [valueA, valueA = valueB][0] : valueB;
+
+    if (+valueA) {
+      return valueA - valueB;
+    }
+
+    if (+valueA.slice(1).split(',').join('')) {
+      return +valueA.slice(1).split(',').join('')
+      - +valueB.slice(1).split(',').join('');
+    }
+
+    return valueA > valueB ? 1 : valueA < valueB ? -1 : 0;
+  });
+
+  rows.forEach(item => {
+    tableBody.append(item);
+  });
+};
+
+// sort table by click on header
+
+let onceClicked;
+let clickCount = 0;
+
+tableHead.firstElementChild.onclick = (e) => {
+  onceClicked === e.target ? clickCount++ : clickCount = 1;
+
+  sortTable(e.target.innerText, clickCount % 2);
+  onceClicked = e.target;
+};
+
+// selected row class active
+
+tableBody.onclick = (e) => {
+  [...tableBody.children].forEach(item => {
+    item.classList.remove('active');
+  });
+  e.target.parentElement.classList.add('active');
+};
+
+// new Form
+
+const newEmployeeForm = document.createElement('form');
+
+newEmployeeForm.classList.add('new-employee-form');
+
+newEmployeeForm.insertAdjacentHTML('afterbegin', `
+<label>Name: <input name="name" type="text" data-qa="name" required></label>
+<label>Position:
+  <input name="position" type="text" data-qa="position" required>
+</label>
+<label>Office:
+  <select name="office" data-qa="office" required>
+    <option>Tokyo</option>
+    <option>Singapore</option>
+    <option>London</option>
+    <option>New York</option>
+    <option>Edinburgh</option>
+    <option>San Francisco</option>
+  </select>
+</label>
+<label>Age: <input name="age" type="number" data-qa="age" required></label>
+<label>Salary:
+    <input name="salary" type="number"  data-qa="salary" required>
+</label>
+<button type="submit">Save to table</button>
+`);
+document.body.append(newEmployeeForm);
+
+// validate function
+
+const validateForm = function(elements) {
+  elements.pop();
+
+  switch (true) {
+    case (elements.some(item => !item.value.length)):
+      pushNotification('Error', 'All field are required', 'error');
+
+      return false;
+    case (elements[0].value.length < 4):
+      pushNotification('Error',
+        'Name should have more than 4 letters', 'error');
+
+      return false;
+    case (elements[3].value < 18 || elements[3].value > 90):
+      pushNotification('Error', 'Age should be between 18 end 90', 'error');
+
+      return false;
+    default: return true;
+  }
+};
+
+// add new row
+
+newEmployeeForm.onsubmit = (e) => {
+  e.preventDefault();
+
+  if (!validateForm([...newEmployeeForm.elements])) {
+    return;
+  };
+
+  tableBody.insertAdjacentHTML('beforeend', `
+  <tr>
+    <td>${newEmployeeForm.elements.name.value}</td>
+    <td>${newEmployeeForm.elements.position.value}</td>
+    <td>${newEmployeeForm.elements.office.value}</td>
+    <td>${newEmployeeForm.elements.age.value}</td>
+    <td>$${newEmployeeForm.elements.salary.value
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+    </td>
+  </tr>
+  `);
+  newEmployeeForm.reset();
+  pushNotification('Success', 'New employee is successfully added', 'success');
+};
+
+// notification function
+
+const pushNotification = (title, description, type) => {
+  const notification = document.createElement('div');
+  const messageTitle = document.createElement('h2');
+  const messageText = document.createElement('p');
+
+  notification.classList.add('notification', type);
+  messageTitle.classList.add('title');
+  messageTitle.textContent = title;
+  messageText.textContent = description;
+
+  notification.append(messageTitle);
+  notification.append(messageText);
+  notification.style.top = '10px';
+  notification.style.right = '10px';
+  notification.dataset.qa = 'notification';
+
+  document.body.append(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 2000);
+};
+
+// editing table cells
+
+const handleEditInput = function(text) {
+  const editInput = document.querySelector('.cell-input');
+
+  editInput.focus();
+
+  const saveChanges = (e) => {
+    if (document.activeElement !== editInput || e.keyCode === 13) {
+      editInput.parentElement.textContent = editInput.value.trim() || text;
+      editInput.remove();
+    }
+  };
+
+  editInput.addEventListener('keydown', saveChanges);
+  editInput.addEventListener('blur', saveChanges);
+};
+
+// editing option
+
+tableBody.ondblclick = (e) => {
+  const text = e.target.firstChild.textContent;
+
+  e.target.firstChild.remove();
+
+  e.target.insertAdjacentHTML('afterbegin', `
+  <input class='cell-input' value='${text}'>
+  `);
+
+  handleEditInput(text);
+};
