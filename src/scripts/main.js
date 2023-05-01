b72e5314bca0ad0056c7c552aab63d2768b94c82
Victor Buldenko <gookidoo@gmail.com>
2023-05-01 13:10:36 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,200 @@
+/* eslint-disable max-len */
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tbody = document.querySelector('tbody');
+
+tbody.isSorted = false;
+tbody.currentIndex = 0;
+
+function tableSort(indexCol = tbody.currentIndex) {
+  const rowsForSort = [...tbody.querySelectorAll('tr')];
+
+  rowsForSort.sort((a, b) => {
+    const first = a.children[indexCol].innerText;
+    const second = b.children[indexCol].innerText;
+
+    if (isNaN(parseFloat(first.slice(1)))) {
+      return first.localeCompare(second);
+    } else {
+      if (first.slice(0, 1) === '$') {
+        return parseFloat(first.slice(1)) - parseFloat(second.slice(1));
+      }
+
+      return parseFloat(first) - parseFloat(second);
+    }
+  });
+
+  if (tbody.isSorted) {
+    rowsForSort.reverse().forEach((el) => tbody.append(el));
+    tbody.isSorted = false;
+  } else {
+    rowsForSort.forEach((el) => tbody.append(el));
+    tbody.isSorted = true;
+  }
+}
+
+table.addEventListener('click', (e) => {
+  if (e.target.tagName === 'TH') {
+    const place = e.target.closest('thead') ? 0 : table.rows.length - 1;
+
+    tbody.currentIndex = [...table.rows[place].children].indexOf(e.target);
+
+    tableSort(tbody.currentIndex);
+  }
+});
+
+tbody.addEventListener('click', (e) => {
+  const row = e.target.closest('TR');
+  const rowsForSort = [...tbody.querySelectorAll('tr')];
+
+  rowsForSort.forEach((item) => item.classList.remove('active'));
+  row.classList.add('active');
+});
+
+tbody.addEventListener('dblclick', (e) => {
+  if (e.target.tagName === 'TD') {
+    const cell = e.target;
+    const columnNum = [...cell.closest('TR').children].indexOf(cell);
+    const defaultValue = cell.innerText;
+    const selection = window.getSelection();
+    const range = document.createRange();
+
+    if (columnNum === 2) {
+      const select = createSelect(cityList, 'office', true);
+
+      cell.innerText = '';
+      cell.append(select);
+      cell.firstElementChild.classList.add('cell-input');
+
+      cell.addEventListener('change', () => {
+        cell.innerText = cell.firstElementChild.value;
+      });
+    } else {
+      cell.contentEditable = true;
+      cell.focus();
+      range.selectNodeContents(cell);
+      selection.removeAllRanges();
+      selection.addRange(range);
+
+      cell.addEventListener('blur', () => {
+        cell.innerText = cell.innerText.trim();
+
+        if (columnNum === 4 && cell.innerText.slice(0, 1) !== '$') {
+          cell.innerText = '$' + cell.innerText.trim();
+        }
+
+        if (columnNum === 3 && (+cell.innerText > 90 || +cell.innerText < 18)) {
+          cell.innerText = cell.innerText.slice(0, 2);
+        }
+      });
+    }
+
+    cell.addEventListener('blur', () => {
+      cell.innerText = cell.innerText.trim();
+      cell.contentEditable = false;
+      cell.classList.remove('cell-input');
+
+      if (cell.innerText === '') {
+        cell.innerText = defaultValue;
+      }
+    });
+
+    cell.addEventListener('keydown', eventCell => {
+      if (eventCell.key === 'Enter') {
+        cell.innerText = cell.innerText.trim();
+        cell.contentEditable = false;
+        cell.classList.remove('cell-input');
+
+        if (cell.innerText === '') {
+          cell.innerText = defaultValue;
+        }
+      };
+    });
+  }
+});
+
+function createInput(labelText, inputName, data, inputtype = 'text') {
+  const label = document.createElement('LABEL');
+
+  label.innerHTML = `${labelText} <input name =${inputName} type=${inputtype} data-qa=${data} required></input>`;
+  label.setAttribute('for', inputName);
+
+  if (inputName === 'name') {
+    label.children[0].minlength = '4';
+  }
+
+  if (inputName === 'age') {
+    label.children[0].min = '18';
+    label.children[0].max = '90';
+  }
+
+  return label;
+}
+
+function createSelect(arr, data, isEdit = false) {
+  const label = document.createElement('LABEL');
+  const select = document.createElement('SELECT');
+
+  for (let i = 0; i < arr.length; i++) {
+    const option = document.createElement('OPTION');
+
+    option.innerText = arr[i];
+    select.add(option, select[i]);
+  }
+
+  select.setAttribute('data-qa', data);
+
+  if (!isEdit) {
+    label.innerText = data[0].toLocaleUpperCase() + data.slice(1) + ':';
+    label.append(select);
+
+    return label;
+  }
+
+  return select;
+}
+
+const addEmployForm = document.createElement('FORM');
+const formButton = document.createElement('BUTTON');
+
+const cityList = [
+  'Tokyo',
+  'Singapore',
+  'London',
+  'New York',
+  'Edinburgh',
+  'San Francisco',
+];
+
+addEmployForm.classList.add('new-employee-form');
+addEmployForm.action = '#';
+addEmployForm.method = 'GET';
+formButton.innerText = 'Save to table';
+
+addEmployForm.append(createInput('Name:', 'name', 'name'));
+addEmployForm.append(createInput('Position:', 'position', 'position'));
+addEmployForm.append(createSelect(cityList, 'office'));
+addEmployForm.append(createInput('Age:', 'age', 'age', 'number'));
+addEmployForm.append(createInput('Salary:', 'salary', 'salary', 'number'));
+addEmployForm.append(formButton);
+
+addEmployForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const newRow = tbody.rows[0].cloneNode();
+  const rowLength = tbody.rows[0].children.length;
+
+  for (let i = 0; i < rowLength; i++) {
+    const cell = newRow.insertCell(i);
+
+    cell.innerText = e.target[i].value;
+  }
+
+  newRow.lastElementChild.innerText = '$' + newRow.lastElementChild.innerText;
+  tbody.append(newRow);
+  tableSort(tbody.currentIndex);
+  e.currentTarget.reset();
+});
+
+document.body.append(addEmployForm);
