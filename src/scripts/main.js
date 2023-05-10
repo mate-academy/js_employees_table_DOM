976e3ebb9cd8dfb636d2571ad9f850db4fb38f5d
Antonio S <soi4.toxa@ukr.net>
2023-05-10 08:53:52 +0300

    Employees-table-4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,7 +20,7 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   elementTitle.innerText = title;
   elementDescript.innerText = description;
 
-  notification.dataset.qa = 'notification'; // MA test
+  notification.dataset.qa = 'notification';
   notification.style.top = `${posTop}px`;
   notification.style.right = `${posRight}px`;
 
@@ -32,7 +32,6 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   setTimeout(() => notification.remove(), 2000);
 };
 
-// adding sort thead
 thead.addEventListener('click', e => {
   const target = e.target;
   const indexColumn = [...thead.firstElementChild.children].indexOf(target);
@@ -68,7 +67,6 @@ thead.addEventListener('click', e => {
   tbodyTrs.map(tr => tbody.append(tr));
 });
 
-// adding active row
 tbody.addEventListener('click', e => {
   const targetTr = e.target.parentElement;
 
@@ -87,15 +85,14 @@ tbody.addEventListener('click', e => {
   globalActiveRow = targetTr;
 });
 
-// adding form
 body.insertAdjacentHTML('beforeend', `
   <form action="" metod="get" class="new-employee-form">
     <label>Name:
-      <input name="name" data-qa="name">
+      <input name="name" data-qa="name" pattern="^[a-zA-Z]+$">
     </label>
 
     <label>Position:
-      <input name="position" data-qa="position">
+      <input name="position" data-qa="position"  pattern="^[a-zA-Z]+$">
     </label>
 
     <label>Office:
@@ -137,7 +134,6 @@ form.addEventListener('submit', e => {
       || !data.get('office')
       || !age
       || !salary):
-      // e.preventDefault();
 
       pushNotification(10, 10, 'Fill in all fields',
         'Necessary to fill all fields', 'error');
@@ -145,7 +141,6 @@ form.addEventListener('submit', e => {
       return;
 
     case (data.get('name').length < 4):
-      // e.preventDefault();
 
       pushNotification(10, 10, 'Too short Name',
         'Enter a longer Name. Longer 3 characters', 'error');
@@ -153,7 +148,6 @@ form.addEventListener('submit', e => {
       return;
 
     case (age < 18):
-      // e.preventDefault();
 
       pushNotification(10, 10, 'Too young Age',
         'You are too young for this shit.\n Min Age is 18', 'error');
@@ -161,7 +155,6 @@ form.addEventListener('submit', e => {
       return;
 
     case (age > 90):
-      // e.preventDefault();
 
       pushNotification(10, 10, 'Too old Age',
         'You are too old for this shit.\n Max age is 90', 'error');
@@ -182,63 +175,138 @@ form.addEventListener('submit', e => {
   </tr>
   `);
 
+  form.reset();
+
   pushNotification(10, 10, 'Person was appended',
     'There are even more of us', 'success');
 
-  updateVariables();
+  updatePageValues();
 });
 
-//  dblclick on cell
 tbody.addEventListener('dblclick', e => {
   const cell = e.target;
+  const cells = [...cell.parentElement.children];
 
   if (!cell.matches('td')) {
     return;
   }
 
   const initialText = cell.innerText;
+  const indexCell = cells.indexOf(cell);
 
   cell.innerText = '';
 
-  cell.insertAdjacentHTML('beforeend', `
-    <input class="cell-input" value="${initialText}">
-  `);
+  switch (true) {
+    case (indexCell === 2):
+      cell.insertAdjacentHTML('beforeend', `
+      <select name="office" data-qa="office" class="cell-input">
+        <option value="${initialText}" disablet>
+          ${initialText}
+        </option>
+        <option value="Tokyo">Tokyo</option>
+        <option value="Singapore">Singapore</option>
+        <option value="London">London</option>
+        <option value="New York">New York</option>
+        <option value="Edinburgh">Edinburgh</option>
+        <option value="San Francisco">San Francisco</option>
+      </select>
+    `);
+      break;
 
-  const inputForEdit = cell.lastElementChild;
+    case (indexCell === 3):
+      cell.insertAdjacentHTML('beforeend', `
+      <input
+        class="cell-input"
+        value="${initialText}"
+        type="number">
+      `);
+      break;
 
-  inputForEdit.focus();
+    case (indexCell === 4):
+      const initialSalary = initialText.slice(1).split(',').join('');
 
-  extraEventsForInput(cell, inputForEdit, initialText);
+      cell.insertAdjacentHTML('beforeend', `
+        <input
+          class="cell-input"
+          value="${initialSalary}"
+          type="number">
+      `);
+      break;
 
-  updateVariables();
+    default:
+      cell.insertAdjacentHTML('beforeend', `
+        <input
+          class="cell-input"
+          value="${initialText}"
+          pattern="^[a-zA-Z]+$"
+        >
+      `);
+      break;
+  }
+
+  const inputEdit = cell.lastElementChild;
+
+  inputEdit.focus();
+
+  addInputEvents(cell, inputEdit, initialText, indexCell);
+  updatePageValues();
 });
 
-function extraEventsForInput(target, input, initialValue) {
-  //  blur
+function addInputEvents(target, input, initial, indexTarget) {
   input.addEventListener('blur', () => {
-    removeAndAdd(target, input, initialValue);
+    updateCellValue(target, input, initial, indexTarget);
   });
 
-  //  keyup
   input.addEventListener('keyup', e => {
     if (e.code === 'Enter') {
-      removeAndAdd(target, input, initialValue);
+      updateCellValue(target, input, initial, indexTarget);
     }
   });
 };
 
-function removeAndAdd(target, input, initialValue) {
-  const value = (input.value) ? input.value : initialValue;
-  const inputInside = input;
+function updateCellValue(target, input, initialValue, indexTarget) {
+  let value;
 
-  if (inputInside) {
-    inputInside.remove();
+  if (input.value) {
+    value = [...input.value].join('');
+
+    switch (true) {
+      case indexTarget === 4:
+        value = `$${Number(value).toLocaleString()}`;
+        break;
+
+      case indexTarget === 3:
+        if (+value < 18 || +value > 90) {
+          pushNotification(10, 10, 'Incorrect data',
+            'Min Age is 18\n Max Age is 90', 'error');
+
+          value = initialValue;
+        }
+        break;
+
+      case indexTarget === 0:
+        if (value.length < 4) {
+          pushNotification(10, 10, 'Too short Name',
+            'Enter a longer Name. Longer 3 characters', 'error');
+
+          value = initialValue;
+        }
+        break;
+
+      default:
+        break;
+    }
+  } else {
+    value = initialValue;
   }
 
+  console.log(document.querySelector('.cell-input'));
+
+  input.remove();
   target.innerText = value;
 };
 
-function updateVariables() {
+function updatePageValues() {
   thead = document.querySelector('thead');
   tbodyTrs = [...document.querySelectorAll('tbody tr')];
   tbody = document.querySelector('tbody');
