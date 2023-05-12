4c1171d22001076f1486f3a6ba47de772a59a2d7
Oksana Tytanych <oksanatytanych@gmail.com>
2023-05-12 13:23:46 +0300

    Implement editing of table cells by double-clicking on it

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,103 +1,68 @@
 'use strict';
 
 const form = document.createElement('form');
+let newEmployy = [];
 
-form.className = 'new-employee-form';
+function createForm() {
+  const formField = [
+    {
+      name: 'name',
+      type: 'text',
+    },
+    {
+      name: 'position',
+      type: 'text',
+    },
+    {
+      name: 'age',
+      type: 'number',
+    },
+    {
+      name: 'salary',
+      type: 'number',
+    },
+  ];
 
-const formField = [
-  {
-    name: 'name',
-    type: 'text',
-  },
-  {
-    name: 'position',
-    type: 'text',
-  },
-  {
-    name: 'age',
-    type: 'number',
-  },
-  {
-    name: 'salary',
-    type: 'number',
-  },
-];
+  const selectValues = ['Tokyo', 'Singapore',
+    'London', 'New York', 'Edinburgh', 'San Francisco'];
 
-const selectValues = ['Tokyo', 'Singapore', 'London', 'New York',
-  'Edinburgh', 'San Francisco'];
+  form.className = 'new-employee-form';
 
-form.innerHTML = formField.map(field => `
-  <label>
-    ${field.name[0].toUpperCase() + field.name.slice(1)}:
-      <input
-        name="${field.name}"
-        type="${field.type}"
-        data-qa="${field.name}"
-        required
-      >
-  </label>
-`).join('');
+  form.innerHTML = formField.map(field => `
+    <label>
+      ${field.name[0].toUpperCase() + field.name.slice(1)}:
+        <input
+          name="${field.name}"
+          type="${field.type}"
+          data-qa="${field.name}"
+        >
+    </label>
+  `).join('');
 
-form.querySelector('[name="age"]').setAttribute('min', 18);
-form.querySelector('[name="age"]').setAttribute('max', 90);
+  form.querySelector('[name="age"]').setAttribute('min', 18);
+  form.querySelector('[name="age"]').setAttribute('max', 90);
 
-document.querySelector('table').insertAdjacentElement('afterend', form);
+  document.querySelector('table').insertAdjacentElement('afterend', form);
 
-form.children[1].insertAdjacentHTML('afterend', `
-  <label>Office:
-    <select name="office" data-qa="office" required>
-      ${selectValues.map(value => `
-      <option value="${value}">
-        ${value}
-      </option>`)}
-    </select>
-  </label>
-`);
+  form.children[1].insertAdjacentHTML('afterend', `
+    <label>Office:
+      <select name="office" data-qa="office">
+      <option value="" disabled selected>
+        ${selectValues.map(value => `
+        <option value="${value}">
+          ${value}
+        </option>`).join('')}
+      </select>
+    </label>
+  `);
 
-form.insertAdjacentHTML('beforeend', `
-  <button type="submit">
-    Save to table
-  </button>
-`);
-
-const button = document.querySelector('button');
-
-button.addEventListener('click', (evt) => {
-  evt.preventDefault();
-
-  const fName = form.querySelector('[name="name"]');
-  const fAge = form.querySelector('[name="age"]');
-
-  if (fName.value.length < 4) {
-    alert('Name minimum length is 4');
-    fName.focus();
-
-    return false;
-  } else if (fAge.value > 90 || fAge.value < 18) {
-    alert('Age value is less than 18 or more than 90');
-    fName.focus();
-
-    return false;
-  } else {
-    alert('New employee is successfully added to the table');
-
-    const newEmployy = [...form.querySelectorAll('label')].map(person => {
-      return person.children[0].value;
-    });
-
-    const salary = (Math.round(newEmployy[4] * 1000) / 1000).toFixed(3);
-
-    newEmployy[4] = '$' + salary.toString().replace('.', ',');
-    form.reset();
-
-    const newPerson = document.createElement('tr');
-
-    newPerson.innerHTML = newEmployy.map(el => `<td>${el}</td>`).join('');
-
-    document.querySelector('tbody')
-      .insertAdjacentElement('beforeend', newPerson);
-  }
-});
+  form.insertAdjacentHTML('beforeend', `
+    <button type="submit">
+      Save to table
+    </button>
+  `);
+}
+createForm();
 
 [...document.querySelectorAll('thead tr th')].forEach((element, index) => {
   element.addEventListener('click', (e) => {
@@ -135,7 +100,7 @@ button.addEventListener('click', (evt) => {
   });
 });
 
-[...document.querySelectorAll('tbody tr')].map(element => {
+[...document.querySelectorAll('tbody tr')].forEach(element => {
   element.addEventListener('click', (e) => {
     const activeElement = document.getElementsByClassName('active')[0];
 
@@ -144,5 +109,120 @@ button.addEventListener('click', (evt) => {
     }
 
     e.target.closest('tr').classList.add('active');
+  });
+});
+
+function pushNotification(title, type) {
+  const messageWindow = document.createElement('div');
+
+  messageWindow.classList.add('notification', type);
+  messageWindow.setAttribute('data-qa', 'notification');
+
+  messageWindow.innerHTML = `
+    <h2 class="title">${title}</h2>
+  `;
+
+  form.append(messageWindow);
+
+  setTimeout(() => {
+    messageWindow.remove();
+  }, 2000);
+};
+
+function valideData() {
+  if (newEmployy.includes('')) {
+    pushNotification('All fields must be filled', 'warning');
+  } else {
+    const fName = form.querySelector('[name="name"]');
+    const fAge = form.querySelector('[name="age"]');
+
+    if (fName.value.length < 4) {
+      pushNotification('Name minimum length is 4', 'error');
+      fName.focus();
+
+      return false;
+    } else if (fAge.value.includes(',') || fAge.value.includes('.')) {
+      pushNotification('Just an integer', 'error');
+      fAge.focus();
+
+      return false;
+    } else if (fAge.value > 90 || fAge.value < 18) {
+      pushNotification('Age value is less than 18 or more than 90', 'error');
+      fAge.focus();
+
+      return false;
+    } else {
+      pushNotification('New employee is successfully', 'success');
+
+      return true;
+    }
+  }
+}
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  newEmployy = [...form.querySelectorAll('label')].map(person => {
+    return person.children[0].value;
+  });
+
+  valideData();
+
+  if (valideData() === true) {
+    const newPerson = document.createElement('tr');
+
+    newEmployy[4] = '$' + (Math.round(newEmployy[4] * 1000) / 1000)
+      .toFixed(3).toString().replace('.', ',');
+    form.reset();
+
+    newPerson.innerHTML = newEmployy.map(el => `<td>${el}</td>`).join('');
+
+    document.querySelector('tbody')
+      .insertAdjacentElement('beforeend', newPerson);
+
+    newPerson.addEventListener('click', (e) => {
+      const activeElement = document.getElementsByClassName('active')[0];
+
+      if (activeElement) {
+        activeElement.classList.remove('active');
+      }
+
+      e.target.closest('tr').classList.add('active');
+    });
+  }
+});
+
+let targetData;
+
+[...document.querySelectorAll('td')].forEach(element => {
+  element.addEventListener('dblclick', (evt) => {
+    targetData = evt.target;
+
+    const newData = document.createElement('td');
+    const inputField = document.createElement('input');
+
+    inputField.classList.add('cell-input');
+    newData.insertAdjacentElement('afterbegin', inputField);
+    targetData.parentNode.replaceChild(newData, targetData);
+
+    newData.children[0].addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        if (e.target.value.length > 0) {
+          newData.innerHTML = newData.children[0].value;
+        } else {
+          newData.innerHTML = targetData.innerText;
+        }
+      }
+    });
+
+    // newData.children[0].addEventListener('blur', (e) => {
+    //   if (newData.children[0].value > 0) {
+    //     newData.innerHTML = newData.children[0].value;
+    //   } else {
+    //     newData.innerHTML = targetData.innerText;
+    //   }
+    // });
   });
 });
