2feb57aa54bf22f79faa25e11cd026f8ef73832a
Kristina Bekher <bekherkr@gmail.com>
2023-10-21 15:32:04 +0200

    add form element

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,6 +12,17 @@ const formatSalary = (salary) => {
   return parseInt(salary.slice(1).replace(/,/g, ''));
 };
 
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const body = document.body;
+const tableElement = document.querySelector('table');
+const tbody = tableElement.querySelector('tbody');
+const rows = Array.from(tbody.rows);
+const headers = document.querySelectorAll('thead th');
+
+// #region Sort table
 let isDescOrder = false;
 let currentSortField = null;
 
@@ -52,13 +63,112 @@ function sortTable(column) {
   });
 }
 
-const tableElement = document.querySelector('table');
-const tbody = tableElement.querySelector('tbody');
-const rows = Array.from(tbody.rows);
-const headers = document.querySelectorAll('thead th');
-
 headers.forEach((header, columnIndex) => {
   header.addEventListener('click', () => {
     sortTable(columnIndex);
   });
 });
+// #endregion
+
+// #region Select table row
+let currentSelectedRow = null;
+
+rows.forEach(row => {
+  row.addEventListener('click', () => {
+    if (currentSelectedRow !== null) {
+      currentSelectedRow.classList.remove('active');
+    }
+
+    if (currentSelectedRow !== row) {
+      row.classList.add('active');
+      currentSelectedRow = row;
+    } else {
+      currentSelectedRow = null;
+    }
+  });
+});
+// #endregion
+
+// #region Create form
+const formFields = [
+  {
+    name: 'name',
+    type: 'text',
+    tagName: 'input',
+  },
+  {
+    name: 'position',
+    type: 'text',
+    tagName: 'input',
+  },
+  {
+    name: 'office',
+    tagName: 'select',
+    options: [
+      'Tokyo',
+      'Singapore',
+      'London',
+      'New York',
+      'Edinburgh',
+      'San Francisco',
+    ],
+  },
+  {
+    name: 'age',
+    type: 'number',
+    tagName: 'input',
+  },
+  {
+    name: 'salary',
+    type: 'number',
+    tagName: 'input',
+  },
+];
+
+function createFormField(field) {
+  const formField = document.createElement('label');
+
+  formField.textContent = capitalize(field.name) + ':';
+
+  const formFieldContent = document.createElement(field.tagName);
+
+  formFieldContent.setAttribute('data-qa', field.name);
+  formFieldContent.setAttribute('name', field.name);
+
+  if (field.tagName === 'input') {
+    formFieldContent.type = field.type;
+  } else {
+    for (const option of field.options) {
+      const newOptionElement = document.createElement('option');
+
+      newOptionElement.value = option;
+      newOptionElement.textContent = option;
+
+      formFieldContent.appendChild(newOptionElement);
+    }
+  }
+
+  formField.appendChild(formFieldContent);
+
+  return formField;
+}
+
+const formElement = document.createElement('form');
+
+formElement.classList.add('new-employee-form');
+
+for (const field of formFields) {
+  const formFieldElement = createFormField(field);
+
+  formElement.appendChild(formFieldElement);
+}
+
+const submitButton = document.createElement('button');
+
+submitButton.textContent = 'Save to table';
+submitButton.type = 'submit';
+
+formElement.appendChild(submitButton);
+
+body.appendChild(formElement);
+// #endregion
