53ea43edf4cd3050b3ea176d2dfbd728a48d4eb4
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-22 17:02:49 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
 const thead = document.querySelector('thead');
@@ -109,8 +110,6 @@ body.insertAdjacentHTML('beforeend', `
         type="number"
         data-qa="age"
         required
-        min="18"
-        max="90"
       >
     </label>
 
@@ -122,8 +121,6 @@ body.insertAdjacentHTML('beforeend', `
         type="number"
         data-qa="salary"
         required
-        min="0"
-        max="999999"
       >
     </label>
 
@@ -159,6 +156,74 @@ const pushNotification = (posTop, posRight, title, description, type) => {
 
 const form = document.querySelector('.new-employee-form');
 
+const convertToDollars = (num) => {
+  return `$${(+num).toLocaleString('en-US')}`;
+};
+
+const nameChecker = (nameOfUser) => {
+  if (nameOfUser.trim().length < 4 || !isNaN(+nameOfUser)) {
+    pushNotification(10, 10,
+      'Error',
+      'Name should have at least 4 symbols and cannot be a number or empty',
+      'error',
+    );
+
+    return true;
+  }
+};
+
+const positionChecker = (positionOfUser) => {
+  if (positionOfUser.trim().length < 4 || !isNaN(+positionOfUser)) {
+    pushNotification(10, 10,
+      'Error',
+      'Position should have at least 4 symbols and cannot be a number or empty',
+      'error',
+    );
+
+    return true;
+  }
+};
+
+const ageChecker = (age) => {
+  if (+age < 18 || +age > 90) {
+    pushNotification(10, 10,
+      'Error',
+      'Age must be over 18 and under 90',
+      'error',
+    );
+
+    return true;
+  }
+};
+
+const salaryChecker = (salary) => {
+  if (+salary <= 0 || +salary > 999999) {
+    pushNotification(10, 10,
+      'Error',
+      'Salary must be over $0 and under $999,999',
+      'error',
+    );
+
+    return true;
+  }
+};
+
+const successNotification = () => {
+  pushNotification(10, 10,
+    'Success',
+    'User was added successfully',
+    'success',
+  );
+};
+
+const successNotificationUpdate = () => {
+  pushNotification(10, 10,
+    'Success',
+    'User was successfully updated',
+    'success',
+  );
+};
+
 form.addEventListener('submit', (e) => {
   const tableBody = document.querySelector('tbody');
 
@@ -168,43 +233,19 @@ form.addEventListener('submit', (e) => {
 
   const data = Object.fromEntries(dataFromServer.entries());
 
-  if (data.name.trim().length < 4 || !isNaN(+data.name)) {
-    pushNotification(10, 10,
-      'Error',
-      'Name should have at least 4 symbols and cannot be a number or empty',
-      'error',
-    );
-
+  if (nameChecker(data.name)) {
     return;
   }
 
-  if (data.position.trim().length < 4 || !isNaN(+data.position)) {
-    pushNotification(10, 10,
-      'Error',
-      'Position should have at least 4 symbols and cannot be a number or empty',
-      'error',
-    );
-
+  if (positionChecker(data.position)) {
     return;
   }
 
-  if (+data.age < 18 || +data.age > 90) {
-    pushNotification(10, 10,
-      'Error',
-      'Age should be less 90 and above 18',
-      'error',
-    );
-
+  if (ageChecker(data.age)) {
     return;
   }
 
-  if (+data.salary <= 0) {
-    pushNotification(10, 10,
-      'Error',
-      'Salary should be above 0$',
-      'error',
-    );
-
+  if (salaryChecker(data.salary)) {
     return;
   }
 
@@ -215,10 +256,6 @@ form.addEventListener('submit', (e) => {
     age,
     salary,
   } = data;
-
-  const convertToDollars = (num) => {
-    return `$${(+num).toLocaleString('en-US')}`;
-  };
 
   const normSalary = convertToDollars(salary);
 
@@ -232,11 +269,207 @@ form.addEventListener('submit', (e) => {
     </tr>
   `);
 
-  pushNotification(10, 10,
-    'Success',
-    'User was added successfully',
-    'success',
-  );
+  successNotification();
 
   form.reset();
+});
+
+let input = '';
+
+const handleChange = (e) => {
+  if (e.target.type === 'text'
+    || e.target.name === 'age'
+    || e.target.name === 'office'
+  ) {
+    input = e.target.value;
+  } else if (e.target.name === 'salary') {
+    input = convertToDollars(e.target.value);
+  }
+};
+
+const changesCheckers = (e) => {
+  if ((e.target.name === 'name') && (nameChecker(input))) {
+    e.target.parentElement.innerHTML = [...target.children][0].defaultValue;
+    input = target.innerHTML;
+
+    return true;
+  }
+
+  if ((e.target.name === 'position') && (positionChecker(input))) {
+    e.target.parentElement.innerHTML = [...target.children][0].defaultValue;
+    input = target.innerHTML;
+
+    return true;
+  }
+
+  if ((e.target.name === 'age') && (ageChecker(+input))) {
+    e.target.parentElement.innerHTML = [...target.children][0].defaultValue;
+    input = target.innerHTML;
+
+    return true;
+  }
+
+  if ((e.target.name === 'salary') && (salaryChecker(normalizeSalary(input)))
+  ) {
+    e.target.parentElement.innerHTML = convertToDollars(
+      [...target.children][0].defaultValue);
+    input = target.innerHTML;
+
+    return true;
+  }
+};
+
+const handleOnBlur = (e) => {
+  if (changesCheckers(e)) {
+    return;
+  }
+
+  e.target.parentElement.innerHTML = input;
+  successNotificationUpdate();
+};
+
+const handleKeyUp = (e) => {
+  if (e.key === 'Enter') {
+    if (changesCheckers(e)) {
+      return;
+    }
+
+    e.target.parentElement.innerHTML = input;
+    successNotificationUpdate();
+  } else if (e.key === 'Escape') {
+    e.target.parentElement.innerHTML = e.target.value;
+  }
+};
+
+let target;
+
+tbody.addEventListener('dblclick', (e) => {
+  e.preventDefault();
+
+  if (target) {
+    target.innerHTML = input;
+  }
+
+  const cell = e.target.closest('td');
+
+  if (!cell || !tbody.contains(cell)) {
+    return;
+  }
+
+  target = cell;
+
+  if (cell.cellIndex === 0) {
+    const cellText = cell.innerHTML;
+
+    input = cellText;
+
+    cell.innerHTML = `
+      <input
+        type="text"
+        name="name"
+        class="cell-input"
+        value="${cellText}"
+        onblur="handleOnBlur(event)"
+        oninput="handleChange(event)"
+        onkeyup="handleKeyUp(event)"
+      >
+    `;
+  }
+
+  if (cell.cellIndex === 1) {
+    const cellText = cell.innerHTML;
+
+    input = cellText;
+
+    cell.innerHTML = `
+      <input
+        type="text"
+        name="position"
+        class="cell-input"
+        value="${cellText}"
+        onblur="handleOnBlur(event)"
+        oninput="handleChange(event)"
+        onkeyup="handleKeyUp(event)"
+      >
+    `;
+  }
+
+  if (cell.cellIndex === 2) {
+    const cellText = cell.innerHTML;
+
+    input = cellText;
+
+    cell.innerHTML = `
+      <select
+        name="office"
+        onblur="handleOnBlur(event)"
+        oninput="handleChange(event)"
+        onkeyup="handleKeyUp(event)"
+        class="cell-input"
+      >
+        <option value="Tokyo" ${cellText === 'Tokyo' ? 'selected' : ''}>
+          Tokyo
+        </option>
+
+        <option value="Singapore" ${cellText === 'Singapore' ? 'selected' : ''}>
+          Singapore
+        </option>
+
+        <option value="London" ${cellText === 'London' ? 'selected' : ''}>
+          London
+        </option>
+
+        <option value="New-York" ${cellText === 'New York' ? 'selected' : ''}>
+        New York
+        </option>
+
+        <option value="Edinburgh" ${cellText === 'Edinburgh' ? 'selected' : ''}>
+          Edinburgh
+        </option>
+
+        <option
+          value="San-Francisco"
+          ${cellText === 'San Francisco' ? 'selected' : ''}
+        >
+          San Francisco
+        </option>
+      </select>
+    `;
+  }
+
+  if (cell.cellIndex === 3) {
+    const cellText = cell.innerHTML;
+
+    input = cellText;
+
+    cell.innerHTML = `
+      <input
+        type="number"
+        class="cell-input"
+        name="age"
+        value="${cellText}"
+        onblur="handleOnBlur(event)"
+        oninput="handleChange(event)"
+        onkeyup="handleKeyUp(event)"
+      >
+    `;
+  }
+
+  if (cell.cellIndex === 4) {
+    const cellText = normalizeSalary(cell.innerHTML);
+
+    input = cellText;
+
+    cell.innerHTML = `
+      <input
+        type="number"
+        class="cell-input"
+        name="salary"
+        value="${cellText}"
+        onblur="handleOnBlur(event)"
+        oninput="handleChange(event)"
+        onkeyup="handleKeyUp(event)"
+      >
+    `;
+  }
 });
