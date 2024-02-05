33ada4ae188d8c50b395236b6938ddda0dc22ab9
Arkadiusz <stefanski.a00@gmail.com>
2024-02-05 19:43:49 +0100

    fix sort by salary

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,54 +5,6 @@ const tableContent = document.querySelector('tbody');
 let lastSortedIndex = null;
 let isAscending = true;
 
-table.addEventListener('click', function(e) {
-  const header = e.target.closest('th');
-
-  if (!header) {
-    return;
-  }
-
-  const cellIndex = header.cellIndex;
-
-  if (cellIndex === lastSortedIndex) {
-    isAscending = !isAscending;
-  } else {
-    isAscending = true;
-    lastSortedIndex = cellIndex;
-  }
-
-  const sortedRows = sortRows(tableContent.children, cellIndex);
-
-  tableContent.append(...sortedRows);
-});
-
-function sortRows([...rows], index) {
-  const isSortingBySalary = index === 4;
-
-  rows.sort((a, b) => {
-    const dataA = a.cells[index].innerText;
-    const dataB = b.cells[index].innerText;
-
-    if (isSortingBySalary) {
-      const normalize = (data) => data.slice(1).replace(',', '');
-
-      if (isAscending) {
-        return normalize(dataA) - normalize(dataB);
-      } else {
-        return normalize(dataB) - normalize(dataA);
-      }
-    }
-
-    if (isAscending) {
-      return dataA.localeCompare(dataB);
-    } else {
-      return dataB.localeCompare(dataA);
-    }
-  });
-
-  return rows;
-}
-
 table.addEventListener('click', function(events) {
   const clickedCell = events.target;
   const rows = tableContent.querySelectorAll('tr');
@@ -168,3 +120,51 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 });
+
+table.addEventListener('click', function(e) {
+  const header = e.target.closest('th');
+
+  if (!header) {
+    return;
+  }
+
+  const cellIndex = header.cellIndex;
+
+  if (cellIndex === lastSortedIndex) {
+    isAscending = !isAscending;
+  } else {
+    isAscending = true;
+    lastSortedIndex = cellIndex;
+  }
+
+  const sortedRows = sortRows(tableContent.children, cellIndex);
+
+  tableContent.append(...sortedRows);
+});
+
+function sortRows([...rows], index) {
+  const isSortingBySalary = index === 4;
+
+  rows.sort((a, b) => {
+    const dataA = a.cells[index].innerText;
+    const dataB = b.cells[index].innerText;
+
+    if (isSortingBySalary) {
+      const normalize = (data) => data.slice(1).replace(',', '');
+
+      if (isAscending) {
+        return normalize(dataA) - normalize(dataB);
+      } else {
+        return normalize(dataB) - normalize(dataA);
+      }
+    }
+
+    if (isAscending) {
+      return dataA.localeCompare(dataB);
+    } else {
+      return dataB.localeCompare(dataA);
+    }
+  });
+
+  return rows;
+}
