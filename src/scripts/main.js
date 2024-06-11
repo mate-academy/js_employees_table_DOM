dcce510d67419add89dce21bbaa859ed682c6501
Vladyslav <vladbuldenko@gmail.com>
2024-06-12 00:16:06 +0300

    form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -52,4 +52,59 @@ document.addEventListener('DOMContentLoaded', () => {
       setActiveClass(row);
     }
   });
+
+  const form = document.createElement('form');
+
+  form.className = 'new-employee-form';
+
+  const formInputs = [
+    { name: 'name', label: 'Name', type: 'text' },
+    { name: 'position', label: 'Position', type: 'text' },
+    {
+      name: 'office',
+      label: 'Office',
+      type: 'select',
+      options: [
+        'Tokyo',
+        'Singapore',
+        'London',
+        'New York',
+        'Edinburgh',
+        'San Francisco',
+      ],
+    },
+    { name: 'age', label: 'Age', type: 'number' },
+    { name: 'salary', label: 'Salary', type: 'number' },
+  ];
+
+  formInputs.forEach(({ name, label, type, options }) => {
+    const inputLabel = document.createElement('label');
+
+    inputLabel.textContent = `${label}: `;
+
+    const input = document.createElement(
+      type === 'select' ? 'select' : 'input',
+    );
+
+    input.name = name;
+    input.setAttribute('data-qa', name);
+
+    if (type === 'select') {
+      options.forEach((optionText) => input.add(new Option(optionText)));
+    } else {
+      input.type = type;
+    }
+
+    inputLabel.appendChild(input);
+    form.appendChild(inputLabel);
+  });
+
+  form.appendChild(
+    Object.assign(document.createElement('button'), {
+      type: 'submit',
+      textContent: 'Save to table',
+    }),
+  );
+
+  document.body.appendChild(form);
 });
