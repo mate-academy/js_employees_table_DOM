7906324319f12fca1e350c416cd6f313273179e4
Anton Polishchuk <hardixxxxx@gmail.com>
2024-07-01 13:40:19 +0200

    finishing work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+import { onTableSort, setActiveRow } from './utils';
+import { Form } from './form';
+import { CellEdit } from './cell-edit';
+
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const { createForm: form } = new Form();
+const { editCell } = new CellEdit();
+
+tHead.addEventListener('click', (e) => onTableSort(tHead, tBody, e.target));
+
+tBody.addEventListener('click', (e) => setActiveRow(e.target, tBody));
+
+tBody.addEventListener('dblclick', (e) => editCell(e, tHead));
+
+document.body.append(form());
