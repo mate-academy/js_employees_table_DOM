fb748703d2b1a9253b4f2a6063f9bc609b339c44
Heorhii Savostikov <georgesavostikov@gmail.com>
2024-07-09 11:52:47 +0300

    added the option to select a row

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@
 
 const tableElement = document.querySelector('table');
 let sortedValue = '';
+let prevLink;
 
 tableElement.addEventListener('click', (e) => {
   const bodyElement = document.querySelector('tbody');
@@ -9,131 +10,155 @@ tableElement.addEventListener('click', (e) => {
   let sortedData = [];
   const newBodyElement = document.createElement('tbody');
 
-  const data = bodyRows.map((trElement) => {
-    const newObject = {
-      name: trElement.cells[0].textContent,
-      position: trElement.cells[1].textContent,
-      office: trElement.cells[2].textContent,
-      age: trElement.cells[3].textContent,
-      salary: trElement.cells[4].textContent,
-    };
+  const headElement = e.target.closest('thead');
 
-    return newObject;
-  });
+  if (headElement) {
+    const data = bodyRows.map((trElement) => {
+      const newObject = {
+        name: trElement.cells[0].textContent,
+        position: trElement.cells[1].textContent,
+        office: trElement.cells[2].textContent,
+        age: trElement.cells[3].textContent,
+        salary: trElement.cells[4].textContent,
+      };
 
-  bodyElement.remove();
-  tableElement.tHead.after(newBodyElement);
+      return newObject;
+    });
 
-  switch (e.target.textContent) {
-    case 'Name': {
-      sortedData = [...data].sort((el1, el2) => {
-        return el1.name.toLowerCase().localeCompare(el2.name.toLowerCase());
-      });
+    bodyElement.remove();
+    tableElement.tHead.after(newBodyElement);
 
-      if (sortedValue === 'Name') {
-        sortedData.reverse();
+    switch (e.target.textContent) {
+      case 'Name': {
+        sortedData = [...data].sort((el1, el2) => {
+          return el1.name.toLowerCase().localeCompare(el2.name.toLowerCase());
+        });
 
-        sortedValue = '';
-        break;
-      }
+        if (sortedValue === 'Name') {
+          sortedData.reverse();
 
-      sortedValue = e.target.textContent;
+          sortedValue = '';
+          break;
+        }
 
-      break;
-    }
-
-    case 'Position': {
-      sortedData = [...data].sort((el1, el2) => {
-        return el1.position
-          .toLowerCase()
-          .localeCompare(el2.position.toLowerCase());
-      });
-
-      if (sortedValue === 'Position') {
-        sortedData.reverse();
-
-        sortedValue = '';
-        break;
-      }
-
-      sortedValue = e.target.textContent;
-
-      break;
-    }
-
-    case 'Office': {
-      sortedData = [...data].sort((el1, el2) => {
-        return el1.office.toLowerCase().localeCompare(el2.office.toLowerCase());
-      });
-
-      if (sortedValue === 'Office') {
-        sortedData.reverse();
-
-        sortedValue = '';
+        sortedValue = e.target.textContent;
 
         break;
       }
 
-      sortedValue = e.target.textContent;
+      case 'Position': {
+        sortedData = [...data].sort((el1, el2) => {
+          return el1.position
+            .toLowerCase()
+            .localeCompare(el2.position.toLowerCase());
+        });
 
-      break;
-    }
+        if (sortedValue === 'Position') {
+          sortedData.reverse();
 
-    case 'Age': {
-      sortedData = [...data].sort((el1, el2) => +el1.age - +el2.age);
+          sortedValue = '';
+          break;
+        }
 
-      if (sortedValue === 'Age') {
-        sortedData.reverse();
-
-        sortedValue = '';
-
-        break;
-      }
-
-      sortedValue = e.target.textContent;
-
-      break;
-    }
-
-    case 'Salary': {
-      sortedData = [...data].sort(
-        (el1, el2) =>
-          +el1.salary.slice(1).split(',').join('') -
-          +el2.salary.slice(1).split(',').join(''),
-      );
-
-      if (sortedValue === 'Salary') {
-        sortedData.reverse();
-
-        sortedValue = '';
+        sortedValue = e.target.textContent;
 
         break;
       }
 
-      sortedValue = e.target.textContent;
+      case 'Office': {
+        sortedData = [...data].sort((el1, el2) => {
+          return el1.office
+            .toLowerCase()
+            .localeCompare(el2.office.toLowerCase());
+        });
 
-      break;
+        if (sortedValue === 'Office') {
+          sortedData.reverse();
+
+          sortedValue = '';
+
+          break;
+        }
+
+        sortedValue = e.target.textContent;
+
+        break;
+      }
+
+      case 'Age': {
+        sortedData = [...data].sort((el1, el2) => +el1.age - +el2.age);
+
+        if (sortedValue === 'Age') {
+          sortedData.reverse();
+
+          sortedValue = '';
+
+          break;
+        }
+
+        sortedValue = e.target.textContent;
+
+        break;
+      }
+
+      case 'Salary': {
+        sortedData = [...data].sort(
+          (el1, el2) =>
+            +el1.salary.slice(1).split(',').join('') -
+            +el2.salary.slice(1).split(',').join(''),
+        );
+
+        if (sortedValue === 'Salary') {
+          sortedData.reverse();
+
+          sortedValue = '';
+
+          break;
+        }
+
+        sortedValue = e.target.textContent;
+
+        break;
+      }
+
+      default: {
+        sortedData = [...data];
+      }
     }
 
-    default: {
-      sortedData = [...data];
-    }
+    const newElements = sortedData.map((el) => {
+      const trElement = document.createElement('tr');
+
+      for (const property in el) {
+        const thElement = document.createElement('td');
+
+        thElement.textContent = el[property];
+        trElement.append(thElement);
+      }
+
+      return trElement;
+    });
+
+    newElements.forEach((corEl) => {
+      newBodyElement.append(corEl);
+    });
   }
 
-  const newElements = sortedData.map((el) => {
-    const trElement = document.createElement('tr');
+  // -----------------------------------------------------------------
 
-    for (const property in el) {
-      const thElement = document.createElement('td');
+  const link = e.target.closest('tr');
 
-      thElement.textContent = el[property];
-      trElement.append(thElement);
+  if (bodyRows.includes(link)) {
+    if (prevLink && prevLink !== link) {
+      prevLink.classList.remove('active');
     }
 
-    return trElement;
-  });
-
-  newElements.forEach((corEl) => {
-    newBodyElement.append(corEl);
-  });
+    if (prevLink === link) {
+      link.classList.remove('active');
+      prevLink = undefined;
+    } else {
+      link.classList.add('active');
+      prevLink = link;
+    }
+  }
 });
