74f1a7073a4cd71b9ecac97940c7bd6d2fcbf609
Jane Shavrukova <eva.shavrukova@gmail.com>
2024-07-24 15:52:22 -0700

    Add form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-'use strict';
-
 function makeNumber(str) {
   return Number(str.replace(/[$,]/g, ''));
 }
@@ -14,6 +12,7 @@ function detectDataType(value) {
   }
 }
 
+const body = document.querySelector('body');
 const table = document.querySelector('table');
 const headers = document.querySelectorAll('th');
 const firstRow = table.querySelector('tbody tr');
@@ -74,4 +73,194 @@ tableRows.forEach((row) => {
 
     selectedRow = currentRow;
   });
+});
+
+const form = document.createElement('form');
+
+form.className = 'new-employee-form';
+form.action = '#';
+form.method = 'POST';
+
+form.innerHTML = `
+<form class="new-employee-form">
+  <label>
+    Name:
+    <input
+      data-qa="name"
+      type="text"
+      name="name"
+      required
+    />
+  </label>
+
+  <label>
+    Position:
+    <input
+      data-qa="position"
+      type="text"
+      name="position"
+      required
+    />
+  </label>
+
+  <label>
+    Office:
+    <select
+      data-qa="office"
+      name="office"
+      required
+    >
+      <option value="empty">Select office</option>
+      <option value="Tokyo">Tokyo</option>
+      <option value="Singapore">Singapore</option>
+      <option value="London">London</option>
+      <option value="New York">New York</option>
+      <option value="Edinburgh">Edinburgh</option>
+      <option value="San Francisco">San Francisco</option>
+    </select>
+  </label>
+
+  <label>
+    Age:
+    <input
+      data-qa="age"
+      type="number"
+      name="age"
+      required
+    />
+  </label>
+
+  <label>
+    Salary:
+    <input
+      data-qa="salary"
+      type="number"
+      name="salary"
+      required
+    />
+  </label>
+  <button type="submit">Save to table</button>
+</form>
+`;
+
+body.append(form);
+
+const pushNotification = (posTop, posRight, title, description, type = '') => {
+  const message = document.createElement('div');
+
+  message.className = 'notification';
+  message.dataset.qa = 'notification';
+  message.classList.add(type);
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+
+  message.innerHTML = `
+    <h2 class='title'>
+      ${title}
+    </h2>
+    <p>
+      ${description}
+    </p>
+  `;
+
+  body.append(message);
+
+  setTimeout(() => {
+    message.remove();
+  }, 5000);
+};
+
+const checkData = (data) => {
+  const { name: employeeName, position, office, age, salary } = data;
+
+  if (!employeeName || employeeName.trim().length < 4) {
+    pushNotification(
+      10,
+      10,
+      'Error!',
+      'Name must be at least 4 characters long',
+      'error',
+    );
+
+    return false;
+  }
+
+  if (!position) {
+    pushNotification(
+      10,
+      10,
+      'Error!',
+      'Position field cannot be empty!',
+      'error',
+    );
+
+    return false;
+  }
+
+  if (!office || office === 'empty') {
+    pushNotification(
+      10,
+      10,
+      'Error!',
+      'Office field cannot be empty!',
+      'error',
+    );
+
+    return false;
+  }
+
+  if (!age || age < 18 || age > 90) {
+    pushNotification(
+      10,
+      10,
+      'Error!',
+      'Age must be between 18 and 90',
+      'error',
+    );
+
+    return false;
+  }
+
+  if (!salary || isNaN(salary)) {
+    pushNotification(500, 10, 'Error!', 'Salary must be a number', 'error');
+
+    return false;
+  }
+
+  return true;
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const formObj = Object.fromEntries(formData.entries());
+
+  if (!checkData(formObj)) {
+    return;
+  }
+
+  const newRow = document.createElement('tr');
+
+  const formattedSalary = Number(formObj.salary).toLocaleString('en-US');
+
+  newRow.innerHTML = `
+    <td>${formObj.name}</td>
+    <td>${formObj.position}</td>
+    <td>${formObj.office}</td>
+    <td>${formObj.age}</td>
+    <td>$${formattedSalary}</td>
+    `;
+
+  tableBody.append(newRow);
+
+  pushNotification(
+    10,
+    10,
+    'Success!',
+    'New employee added to the table',
+    'success',
+  );
+
+  form.reset();
 });
