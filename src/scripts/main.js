8b798857893d91ed907f67dfc869b07100def08a
Igor Novikov <novikov.igor.20@gmail.com>
2024-08-08 23:26:15 +0300

    add dbclick and add show notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,12 @@
 // const table = document.querySelector('table');
 const theads = document.querySelectorAll('th');
 const tbody = document.querySelector('table tbody');
+const trArr = [...tbody.querySelectorAll('tr')];
 
 let sortOrder = 1;
 let lastColumnIndex = -1;
 
 function sortColumns(index) {
-  const trArr = [...tbody.querySelectorAll('tr')];
-
   if (lastColumnIndex !== index) {
     sortOrder = 1;
   } else {
@@ -53,7 +52,8 @@ tbody.addEventListener('click', (e) => {
   }
 });
 
-//* Form
+//* add a form to the document.
+//* Form allows users to add new employees to the spreadsheet.
 const form = document.createElement('form');
 
 form.className = 'new-employee-form';
@@ -73,12 +73,6 @@ const fealds = [
     qa: 'position',
   },
   {
-    label: 'Age',
-    name: 'age',
-    type: 'number',
-    qa: 'age',
-  },
-  {
     label: 'Office',
     name: ' office',
     type: 'select',
@@ -92,6 +86,12 @@ const fealds = [
       'Edinburgh',
       'San Francisco',
     ],
+  },
+  {
+    label: 'Age',
+    name: 'age',
+    type: 'number',
+    qa: 'age',
   },
   {
     label: 'Salary',
@@ -135,3 +135,106 @@ const submitButton = document.createElement('button');
 submitButton.type = 'submit';
 submitButton.textContent = 'Save to table';
 form.append(submitButton);
+
+//* Show notification if form data is invalid.
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const newRow = document.createElement('tr');
+  let valid = true;
+
+  formData.forEach((value, key) => {
+    const td = document.createElement('td');
+
+    if (key === 'salary') {
+      const normalisedSalary = parseFloat(value).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+      });
+
+      td.textContent = normalisedSalary;
+    } else {
+      td.textContent = value;
+    }
+
+    newRow.append(td);
+
+    if (!checkDataValidity(key, value)) {
+      valid = false;
+    }
+  });
+
+  if (valid) {
+    tbody.append(newRow);
+    trArr.push(newRow);
+    showNotification('New employee added successfully', 'success');
+    form.reset();
+  } else {
+    showNotification('Invalid data!', 'error');
+  }
+});
+
+function checkDataValidity(id, value) {
+  if (id === 'name') {
+    return value.trim().length >= 4;
+  }
+
+  if (id === 'age') {
+    return value >= 18 && value <= 90;
+  }
+
+  if (id === 'salary') {
+    return value > 0;
+  }
+
+  return !!value.trim();
+}
+
+const notificationContainer = document.createElement('div');
+
+notificationContainer.id = 'notification-container';
+document.body.appendChild(notificationContainer);
+
+function showNotification(message, type) {
+  const notification = document.createElement('div');
+
+  notification.className = `notification ${type}`;
+  notification.setAttribute('data-qa', 'notification');
+  notification.textContent = message;
+  notificationContainer.innerHTML = '';
+  notificationContainer.appendChild(notification);
+  setTimeout(() => notification.remove(), 5000);
+}
+
+//* Implement editing of table cells by double-clicking on it (optional) */
+tbody.addEventListener('dblclick', (e) => {
+  const cell = e.target;
+  const initialValue = cell.textContent;
+
+  if (cell.tagName.toLowerCase() === 'td') {
+    const input = document.createElement('input');
+
+    input.className = 'cell-input';
+    input.value = initialValue;
+    cell.textContent = '';
+    cell.appendChild(input);
+    input.focus();
+
+    input.addEventListener('blur', () => {
+      const newValue = input.value.trim();
+
+      cell.textContent = newValue || initialValue;
+    });
+
+    input.addEventListener('keypress', (ev) => {
+      if (ev.key === 'Enter') {
+        const newValue = input.value.trim();
+
+        cell.textContent = newValue || initialValue;
+      }
+    });
+  }
+});
