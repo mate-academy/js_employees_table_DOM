734ef212aed176db898fd6f7b426f16f23f5c392
Vitalii Venher <vengervn@gmail.com>
2024-08-13 00:45:06 +0300

    solution

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ Dear mate,
 this is the final task of JS Advanced course. Apply all the acquired skills and demonstrate what you are capable of!
 
 Let's get started. Briefly about the tasks:
-1. + Implement table sorting by clicking on the title (in two directions).
-2. + When user clicks on a row, it should become selected.
+1. Implement table sorting by clicking on the title (in two directions).
+2. When user clicks on a row, it should become selected.
 3. Write a script to add a form to the document. Form allows users to add new employees to the spreadsheet.
 4. Show notification if form data is invalid (use notification from the previous tasks).
 5. Implement editing of table cells by double-clicking on it. (optional)
@@ -25,17 +25,17 @@ Start table:
 ![Preview](./src/images/preview.png)
 
 ##### Implement table sorting by clicking on the title (in two directions)
-+ When users clicks on one of the table headers, table should be sorted in ASC order, the second click sorts it in DESC order.
-+ When users click on a new title, always sort in ASC order.
+- When users clicks on one of the table headers, table should be sorted in ASC order, the second click sorts it in DESC order.
+- When users click on a new title, always sort in ASC order.
 
 ##### When user clicks on a row, it should become selected.
-+ Use 'active' class for table row to indicate it is selected.
-+ Only one line can be selected at a time.
+- Use 'active' class for table row to indicate it is selected.
+- Only one line can be selected at a time.
 
 ##### Write a script to add a form to the document. Form allows users to add new employees to the spreadsheet.
-+ The form should have class `new-employee-form` (to apply correct styles).
-+ The form should have 4 inputs, 1 select and submit button.
-+ Put inputs inside labels:
+- The form should have class `new-employee-form` (to apply correct styles).
+- The form should have 4 inputs, 1 select and submit button.
+- Put inputs inside labels:
 ```html
 <label>Name: <input name="name" type="text"></label>
 ```
@@ -47,11 +47,11 @@ Start table:
  data-qa="age" 
  data-qa="salary" 
 ```
-+ Select should have 6 options: `Tokyo`, `Singapore`, `London`, `New York`, `Edinburgh`, `San Francisco`.
-+ Use texts for labels and buttons from the screenshot below.
+- Select should have 6 options: `Tokyo`, `Singapore`, `London`, `New York`, `Edinburgh`, `San Francisco`.
+- Use texts for labels and buttons from the screenshot below.
 - Age and salary inputs should have a number type. Don't forget to convert the string from salary input to correct value like in the table.
 - Click on `Save to table` should add a new employee to the table.
-+ All fields are required.
+- All fields are required.
 
 ##### Show notification if form data is invalid.
 - Click on `Save to table` should run validation for form inputs. If data is valid, add a new employee to the table.
@@ -61,7 +61,7 @@ Start table:
 - Notification titles and descriptions are up to you.
 - Add qa attribute for notification: `data-qa="notification"` and class `error`/`success` depending on the result.
 
-##### Implement editing of table cells by double-clicking on it (optional). 
+##### Implement of table cells by double-clicking on it (optional). 
 - Double click on the cell of the table, should remove text, and append input with `cell-input` class.
 - The input value should contain replaced by input text.
 - Only one cell can be edited at the time.
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -47,14 +47,144 @@ const stringToDigit = (string) => {
   return +string.replace(/\D/g, '');
 };
 
-rows.forEach((row) => {
-  row.addEventListener('click', () => {
-    rows.forEach((el) => el.classList.remove('active'));
-    row.classList.add('active');
+function addActiv() {
+  rows.forEach((row) => {
+    row.addEventListener('click', () => {
+      rows.forEach((el) => el.classList.remove('active'));
+      row.classList.add('active');
+    });
   });
-});
+}
+
+function validationForm(e) {
+  e.preventDefault();
+
+  const invalidFields = [...form.querySelectorAll(':invalid')];
+
+  [...form.elements].forEach((field) => {
+    if (field.checkValidity()) {
+      field.style.border = '';
+      field.style.backgroundColor = '';
+    }
+  });
+
+  if (form.checkValidity()) {
+    notification.classList.remove('error');
+    notification.classList.add('success');
+    notification.innerText = 'The employee is added to the table';
+    notification.style.display = '';
+    document.body.prepend(notification);
+    addEmployee();
+  } else {
+    notification.textContent = '';
+  }
+
+  invalidFields.forEach((field) => {
+    if (!field.checkValidity()) {
+      notification.classList.remove('success');
+      notification.classList.add('error');
+      notification.style.display = '';
+      document.body.prepend(notification);
+
+      field.style.border = '2px solid red';
+      field.style.backgroundColor = '#ffe6e6';
+
+      if (field.validity.valueMissing) {
+        notification.textContent = 'Fill in this field';
+      } else if (field.validity.tooShort) {
+        notification.textContent = `Text must be at least 4 characters long (you entered ${field.value.length} characters)`;
+      } else if (field.validity.rangeUnderflow) {
+        notification.textContent = 'Value must be greater than or equal to 18';
+      } else if (field.validity.rangeOverflow) {
+        notification.textContent = 'The value must be less than or equal to 90';
+      }
+    }
+  });
+
+  setTimeout(() => {
+    notification.style.display = 'none';
+  }, 2000);
+}
+
+function addEmployee() {
+  const formData = new FormData(form);
+  const formDataArr = [];
+
+  formData.forEach((value, key) => {
+    if (key === 'salary') {
+      const dataValue = +value;
+      const formatedValue = '$' + dataValue.toLocaleString('en-US');
+
+      formDataArr.push(formatedValue);
+    } else {
+      formDataArr.push(value);
+    }
+  });
+
+  const newRow = tableBody.insertRow();
+
+  formDataArr.forEach((el) => {
+    newRow.insertCell().textContent = el;
+  });
+
+  rows.push(newRow);
+
+  addActiv();
+
+  form.reset();
+}
+
+function editingCells() {
+  const tableCells = [...tableBody.querySelectorAll('td')];
+
+  tableCells.forEach((cell) => {
+    cell.addEventListener('dblclick', (e) => {
+      const activeInput = document.querySelector('.cell-input');
+
+      if (activeInput) {
+        const activeCell = activeInput.parentElement;
+
+        activeCell.textContent =
+          activeInput.value.trim() || activeInput.dataset.prevVal;
+        activeInput.remove();
+      }
+
+      const prevContent = cell.textContent;
+
+      cell.textContent = '';
+
+      const input = document.createElement('input');
+
+      input.classList.add('cell-input');
+      input.value = prevContent;
+      input.setAttribute('data-prev-val', prevContent);
+      cell.append(input);
+      input.focus();
+
+      //   cell.setAttribute('contenteditable', 'true');
+      //   cell.focus();
+
+      input.addEventListener('blur', () => {
+        cell.textContent = input.value.trim() || prevContent;
+        input.remove();
+
+        // cell.removeAttribute('contenteditable');
+      });
+
+      input.addEventListener('keyup', (e) => {
+        if (e.key === 'Enter') {
+          input.blur();
+
+          //   cell.blur();
+        }
+      });
+    });
+  });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
+  addActiv();
+
   let asc = true;
   let prevIndecs;
 
@@ -85,53 +215,6 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   form.addEventListener('submit', validationForm);
+
+  editingCells();
 });
-
-function validationForm(e) {
-  e.preventDefault();
-
-  const invalidFields = [...form.querySelectorAll(':invalid')];
-
-  [...form.elements].forEach((field) => {
-    if (field.checkValidity()) {
-      field.style.border = '';
-      field.style.backgroundColor = '';
-    }
-  });
-
-  if (form.checkValidity()) {
-    notification.classList.remove('error');
-    notification.classList.add('success');
-    notification.innerText = 'The employee is added to the table';
-    notification.style.display = 'block';
-    document.body.prepend(notification);
-  } else {
-    notification.textContent = '';
-  }
-
-  invalidFields.forEach((field) => {
-    if (!field.checkValidity()) {
-      notification.classList.remove('success');
-      notification.classList.add('error');
-      notification.style.display = 'block';
-      document.body.prepend(notification);
-
-      field.style.border = '2px solid red';
-      field.style.backgroundColor = '#ffe6e6';
-
-      if (field.validity.valueMissing) {
-        notification.textContent = 'Fill in this field';
-      } else if (field.validity.tooShort) {
-        notification.textContent = `Text must be at least 4 characters long (you entered ${field.value.length} characters)`;
-      } else if (field.validity.rangeUnderflow) {
-        notification.textContent = 'Value must be greater than or equal to 18';
-      } else if (field.validity.rangeOverflow) {
-        notification.textContent = 'The value must be less than or equal to 90';
-      }
-    }
-  });
-
-  setTimeout(() => {
-    notification.style.display = 'none';
-  }, 2000);
-}
