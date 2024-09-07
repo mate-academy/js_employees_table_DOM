9eb3147a4c59a77f47d5336bdf08741f6bb3adde
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-09-07 21:07:50 +0700

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,6 @@
 'use strict';
 
-// ##### Implement table sorting by clicking on the title (in two directions)
-// - When users clicks on one of the table headers, table should be sorted
-// in ASC order, the second click sorts it in DESC order.
-// - When users click on a new title, always sort in ASC order.
+// 1. Implement table sorting by clicking on the title (in two directions).
 
 const tbody = document.querySelector('tbody');
 const thead = document.querySelector('thead');
@@ -54,9 +51,7 @@ thead.addEventListener('click', (e) => {
   lastClicked = e.target.textContent;
 });
 
-// ##### When user clicks on a row, it should become selected.
-// - Use 'active' class for table row to indicate it is selected.
-// - Only one line can be selected at a time.
+// 2. When user clicks on a row, it should become selected.
 
 tbody.addEventListener('click', (e) => {
   const { target } = e;
@@ -68,29 +63,8 @@ tbody.addEventListener('click', (e) => {
   target.closest('tr').classList.add('active');
 });
 
-// ##### Write a script to add a form to the document.
+// 3. Write a script to add a form to the document.
 // Form allows users to add new employees to the spreadsheet.
-// - The form should have class `new-employee-form` (to apply correct styles).
-// - The form should have 4 inputs, 1 select and submit button.
-// - Put inputs inside labels:
-//   ```html
-// <label>Name: <input name="name" type="text"></label>
-// ```
-//   - Add qa attributes for each input field:
-//   ```
-//  data-qa="name"
-//  data-qa="position"
-//  data-qa="office"
-//  data-qa="age"
-//  data-qa="salary"
-// ```
-//   - Select should have 6 options:
-//   `Tokyo`, `Singapore`, `London`, `New York`, `Edinburgh`, `San Francisco`.
-// - Use texts for labels and buttons from the screenshot below.
-// - Age and salary inputs should have a number type. Don't forget to
-// convert the string from salary input to correct value like in the table.
-// - Click on `Save to table` should add a new employee to the table.
-// - All fields are required.
 
 // Create form html
 const html = `<form method="get" action="#" class="new-employee-form" onsubmit="jsFunction();return false">
@@ -198,6 +172,7 @@ btn.addEventListener('click', (e) => {
   document.querySelector('[name = "salary"]').value = '';
 });
 
+// 4. Show notification if form data is invalid.
 // Notification function
 const pushNotification = (title, description, type) => {
   const message = document.createElement('div');
@@ -218,15 +193,7 @@ const pushNotification = (title, description, type) => {
   setTimeout(() => message.remove(), 5000);
 };
 
-// ##### Implement editing of table cells by double-clicking on it (optional).
-// - Double click on the cell of the table, should remove text, and append input
-//   with `cell-input` class.
-// - The input value should contain replaced by input text.
-// - Only one cell can be edited at the time.
-// - On blur save changes to table cell. Remove input and set new text.
-// - On 'Enter' keypress, save changes to the table cell.
-//   Remove input and set new text in the table cell.
-// - If an input is empty on submitting return initial value.
+// 5. Implement editing of table cells by double-clicking on it. (optional)
 
 tbody.addEventListener('dblclick', (e) => {
   const { target } = e;
