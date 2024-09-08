d2d7ec3da9e9611703417a9ceb67bd27ac9b6ecb
Mikhailo Koshilka <mkosilka@gmail.com>
2024-09-08 09:00:32 +0100

    Fix code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Сортування
+// Implement sorting of cells
 const tableBody = document.querySelector('tbody');
 const allRows = Array.from(tableBody.querySelectorAll('tr'));
 let lastSortedColumn = -1;
@@ -15,11 +15,11 @@ document.addEventListener('click', (eve) => {
 
   const columnIndex = target.cellIndex;
 
-  // Якщо це той самий стовпець, змінюємо напрямок сортування
+  // If the same column, reverse sort direction
   if (columnIndex === lastSortedColumn) {
     sortDirection *= -1;
   } else {
-    // Якщо інший стовпець, сортуємо за зростанням
+    // If a different column is clicked, sort in ascending order by default
     sortDirection = 1;
   }
 
@@ -39,14 +39,18 @@ function sortTable(columnIndex) {
     const bValue = cleanValue(b.cells[columnIndex].textContent);
 
     return isNumeric
-      ? (parseFloat(aValue) - parseFloat(bValue)) * sortDirection
-      : aValue.localeCompare(bValue) * sortDirection;
+      ? parseFloat(aValue) - parseFloat(bValue)
+      : aValue.localeCompare(bValue);
   });
+
+  if (sortDirection === -1) {
+    allRows.reverse();
+  }
 
   allRows.forEach((row) => tableBody.appendChild(row));
 }
 
-// Робити row активним
+// Make row active
 let lastActiv;
 
 tableBody.addEventListener('click', (eve) => {
@@ -64,18 +68,15 @@ tableBody.addEventListener('click', (eve) => {
   lastActiv = target;
 });
 
-// Додавання форми
+// Implement form
 
 const newForm = document.createElement('form');
-
 newForm.classList.add('new-employee-form');
 
 const nameLabel = document.createElement('label');
-
 nameLabel.textContent = 'Name: ';
 
 const nameInput = document.createElement('input');
-
 nameInput.setAttribute('type', 'text');
 nameInput.setAttribute('name', 'name');
 nameInput.setAttribute('data-qa', 'name');
@@ -83,11 +84,9 @@ nameLabel.appendChild(nameInput);
 newForm.appendChild(nameLabel);
 
 const positionLabel = document.createElement('label');
-
 positionLabel.textContent = 'Position: ';
 
 const positionInput = document.createElement('input');
-
 positionInput.setAttribute('type', 'text');
 positionInput.setAttribute('name', 'position');
 positionInput.setAttribute('data-qa', 'position');
@@ -95,11 +94,9 @@ positionLabel.appendChild(positionInput);
 newForm.appendChild(positionLabel);
 
 const officeLable = document.createElement('label');
-
 officeLable.textContent = 'Office: ';
 
 const officeSelest = document.createElement('select');
-
 officeSelest.setAttribute('name', 'office');
 officeSelest.setAttribute('data-qa', 'office');
 
@@ -112,6 +109,7 @@ const cities = [
   'San Francisco',
 ];
 
+// Using forEach to quick add options to form
 cities.forEach((city) => {
   const option = document.createElement('option');
 
@@ -123,11 +121,9 @@ officeLable.appendChild(officeSelest);
 newForm.appendChild(officeLable);
 
 const ageLabel = document.createElement('label');
-
 ageLabel.textContent = 'Age: ';
 
 const ageInput = document.createElement('input');
-
 ageInput.setAttribute('type', 'number');
 ageInput.setAttribute('name', 'age');
 ageInput.setAttribute('data-qa', 'age');
@@ -135,28 +131,25 @@ ageLabel.appendChild(ageInput);
 newForm.appendChild(ageLabel);
 
 const salaryLabel = document.createElement('label');
-
 salaryLabel.textContent = 'Salary: ';
 
 const salaryInput = document.createElement('input');
-
 salaryInput.setAttribute('type', 'number');
 salaryInput.setAttribute('name', 'salary');
 salaryInput.setAttribute('data-qa', 'salary');
 salaryLabel.appendChild(salaryInput);
 newForm.appendChild(salaryLabel);
 
-// Створюю кнопку save
+// Create button save
 
 const button = document.createElement('button');
-
 button.textContent = 'Save to table';
 button.setAttribute('type', 'submit');
 
 newForm.addEventListener('submit', (eve) => {
   eve.preventDefault();
 
-  const nameValue = nameInput.value;
+  const nameValue = nameInput.value.trim();
 
   if (nameValue.length < 4) {
     PushNotification('error', 'Name is too small', 'Make it longer!');
@@ -164,9 +157,8 @@ newForm.addEventListener('submit', (eve) => {
     return;
   }
 
-  const positionValue = positionInput.value;
+  const positionValue = positionInput.value.trim();
   const officeValue = officeSelest.value;
-
   const ageValue = ageInput.value;
 
   if (ageValue < 18 || ageValue > 90) {
@@ -188,29 +180,23 @@ newForm.addEventListener('submit', (eve) => {
   }
 
   const tr = document.createElement('tr');
-
   const rowName = document.createElement('td');
-
   rowName.textContent = nameValue;
   tr.appendChild(rowName);
 
   const rowPosition = document.createElement('td');
-
   rowPosition.textContent = positionValue;
   tr.appendChild(rowPosition);
 
   const rowOffice = document.createElement('td');
-
   rowOffice.textContent = officeValue;
   tr.appendChild(rowOffice);
 
   const rowAge = document.createElement('td');
-
   rowAge.textContent = ageValue;
   tr.appendChild(rowAge);
 
   const rowSalary = document.createElement('td');
-
   rowSalary.textContent = `${formattedSalary}`;
   tr.appendChild(rowSalary);
 
@@ -223,28 +209,24 @@ newForm.addEventListener('submit', (eve) => {
   allRows.push(tr);
 });
 
-// Функція Notification
+// Implement Notification function
 
 function PushNotification(type, message, reason) {
   const block = document.createElement('div');
-
   block.classList.add('notification', type);
   block.setAttribute('data-qa', 'notification');
 
   const blockTitle = document.createElement('h2');
-
   blockTitle.classList.add('title');
   blockTitle.textContent = reason;
   block.appendChild(blockTitle);
 
   const text = document.createElement('p');
-
   text.textContent = message;
   block.appendChild(text);
 
   block.style.top = '10px';
   block.style.right = '10px';
-
   document.body.appendChild(block);
 
   setTimeout(() => {
@@ -255,6 +237,7 @@ function PushNotification(type, message, reason) {
 newForm.appendChild(button);
 document.body.appendChild(newForm);
 
+// Implement redaction of row
 tableBody.addEventListener('dblclick', (even) => {
   const target = even.target.closest('td');
 
@@ -267,11 +250,9 @@ tableBody.addEventListener('dblclick', (even) => {
   }
 
   const initialText = target.textContent;
-
   target.textContent = '';
 
   const input = document.createElement('input');
-
   input.classList.add('cell-input');
   input.value = initialText;
   target.appendChild(input);
