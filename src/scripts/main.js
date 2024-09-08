03af2d2d37039edd3f0d464352b29a7191e260b1
Ostap Kushnir <kushnirostap04@gmail.com>
2024-09-08 21:57:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,402 @@
 'use strict';
 
-// write code here
+const body = document.querySelector('body');
+const table = body.querySelector('table');
+
+const thead = table.querySelector('thead');
+const trThead = thead.querySelector('tr');
+const thThead = trThead.querySelectorAll('th');
+
+const tbody = table.querySelector('tbody');
+const trTbody = tbody.querySelectorAll('tr');
+
+const arrayOfPeople = [];
+const people = [];
+
+let isAscending = true;
+let activePerson = null;
+let clickTimeout = null;
+
+trTbody.forEach((tr) => {
+  const tds = tr.querySelectorAll('td');
+
+  tds.forEach((td) => {
+    arrayOfPeople.push(td.textContent);
+  });
+});
+
+for (let i = 0; i < arrayOfPeople.length; i += 5) {
+  const person = {
+    name: arrayOfPeople[i],
+    position: arrayOfPeople[i + 1],
+    office: arrayOfPeople[i + 2],
+    age: +arrayOfPeople[i + 3],
+    salary: parseInt(arrayOfPeople[i + 4].replace(/[$,]/g, '')),
+  };
+
+  people.push(person);
+}
+
+thThead.forEach((th) => {
+  th.addEventListener('click', (e) => {
+    const typeOfSort = e.currentTarget.textContent;
+
+    switch (typeOfSort) {
+      case 'Name':
+        people.sort(
+          (a, b) =>
+            isAscending
+              ? a.name.localeCompare(b.name)
+              : b.name.localeCompare(a.name),
+          // eslint-disable-next-line
+        );
+        break;
+
+      case 'Position':
+        people.sort(
+          (a, b) =>
+            isAscending
+              ? a.position.localeCompare(b.position)
+              : b.position.localeCompare(a.position),
+          // eslint-disable-next-line
+        );
+        break;
+
+      case 'Office':
+        people.sort(
+          (a, b) =>
+            isAscending
+              ? a.office.localeCompare(b.office)
+              : b.office.localeCompare(a.office),
+          // eslint-disable-next-line
+        );
+        break;
+
+      case 'Age':
+        people.sort((a, b) => (isAscending ? a.age - b.age : b.age - a.age));
+        break;
+
+      case 'Salary':
+        people.sort(
+          (a, b) => (isAscending ? a.salary - b.salary : b.salary - a.salary),
+          // eslint-disable-next-line
+        );
+        break;
+
+      default:
+        break;
+    }
+
+    updateTable();
+    isAscending = !isAscending;
+  });
+});
+
+function updateTable() {
+  tbody.innerHTML = '';
+
+  people.forEach((person) => {
+    const newTr = document.createElement('tr');
+
+    if (person === activePerson) {
+      newTr.className = 'active';
+    }
+
+    newTr.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.position}</td>
+    <td>${person.office}</td>
+    <td>${person.age}</td>
+    <td>$${person.salary.toLocaleString('en-US')}</td>
+    `;
+
+    newTr.addEventListener('click', () => {
+      clearTimeout(clickTimeout);
+
+      clickTimeout = setTimeout(() => {
+        if (activePerson !== person) {
+          activePerson = person;
+        }
+
+        updateTable();
+      }, 200);
+    });
+
+    const allTd = newTr.querySelectorAll('td');
+
+    allTd.forEach((td, cellIndex) => {
+      td.addEventListener('dblclick', () => {
+        clearTimeout(clickTimeout);
+
+        const ctdCopy = td.textContent;
+
+        const input = document.createElement('input');
+
+        input.className = 'cell-input';
+
+        td.textContent = '';
+
+        input.value = ctdCopy;
+
+        input.addEventListener('blur', () => {
+          const newValue =
+            input.value.trim().length === 0 ? ctdCopy : input.value;
+
+          td.textContent = newValue;
+
+          switch (cellIndex) {
+            case 0:
+              person.name = newValue;
+              break;
+
+            case 1:
+              person.position = newValue;
+              break;
+
+            case 2:
+              person.office = newValue;
+              break;
+
+            case 3:
+              person.age = +newValue;
+              break;
+
+            case 4:
+              person.salary = parseInt(newValue.replace(/[$,]/g, ''));
+              break;
+          }
+        });
+
+        input.addEventListener('keypress', (e) => {
+          if (e.key === 'Enter') {
+            const newValue =
+              input.value.trim().length === 0 ? ctdCopy : input.value;
+
+            td.textContent = newValue;
+
+            switch (cellIndex) {
+              case 0:
+                person.name = newValue;
+                break;
+
+              case 1:
+                person.position = newValue;
+                break;
+
+              case 2:
+                person.office = newValue;
+                break;
+
+              case 3:
+                person.age = +newValue;
+                break;
+
+              case 4:
+                person.salary = parseInt(newValue.replace(/[$,]/g, ''));
+                break;
+            }
+          }
+        });
+
+        td.appendChild(input);
+        input.focus();
+      });
+    });
+
+    tbody.appendChild(newTr);
+  });
+}
+
+updateTable();
+
+const form = document.createElement('form');
+
+form.className = 'new-employee-form';
+
+const labelName = document.createElement('label');
+const labelPosition = document.createElement('label');
+const labelOffice = document.createElement('label');
+const labelAge = document.createElement('label');
+const labelSalary = document.createElement('label');
+
+labelName.textContent = 'Name:';
+labelPosition.textContent = 'Position:';
+labelOffice.textContent = 'Office:';
+labelAge.textContent = 'Age:';
+labelSalary.textContent = 'Salary:';
+
+const inputName = document.createElement('input');
+const inputPosition = document.createElement('input');
+const inputAge = document.createElement('input');
+const inputSalary = document.createElement('input');
+
+inputName.setAttribute('name', 'name');
+inputName.setAttribute('type', 'text');
+inputName.setAttribute('data-qa', 'name');
+inputName.setAttribute('required', '');
+
+inputPosition.setAttribute('name', 'position');
+inputPosition.setAttribute('type', 'text');
+inputPosition.setAttribute('data-qa', 'position');
+inputPosition.setAttribute('required', '');
+
+inputAge.setAttribute('name', 'age');
+inputAge.setAttribute('type', 'number');
+inputAge.setAttribute('data-qa', 'age');
+inputAge.setAttribute('required', '');
+
+inputSalary.setAttribute('name', 'salary');
+inputSalary.setAttribute('type', 'number');
+inputSalary.setAttribute('data-qa', 'salary');
+inputSalary.setAttribute('required', '');
+
+const select = document.createElement('select');
+
+select.setAttribute('name', 'select');
+select.setAttribute('data-qa', 'office');
+select.setAttribute('required', '');
+
+const optionTokyo = document.createElement('option');
+const optionSingapore = document.createElement('option');
+const optionLondon = document.createElement('option');
+const optionNewYork = document.createElement('option');
+const optionEdinburgh = document.createElement('option');
+const optionSanFrancisco = document.createElement('option');
+
+optionTokyo.setAttribute('value', 'Tokyo');
+optionSingapore.setAttribute('value', 'Singapore');
+optionLondon.setAttribute('value', 'London');
+optionNewYork.setAttribute('value', 'New York');
+optionEdinburgh.setAttribute('value', 'Edinburgh');
+optionSanFrancisco.setAttribute('value', 'San Francisco');
+
+optionTokyo.textContent = 'Tokyo';
+optionSingapore.textContent = 'Singapore';
+optionLondon.textContent = 'London';
+optionNewYork.textContent = 'New York';
+optionEdinburgh.textContent = 'Edinburgh';
+optionSanFrancisco.textContent = 'San Francisco';
+
+const button = document.createElement('button');
+
+button.setAttribute('name', 'button');
+button.setAttribute('type', 'submit');
+
+button.textContent = 'Save to table';
+
+body.appendChild(form);
+
+form.appendChild(labelName);
+form.appendChild(labelPosition);
+form.appendChild(labelOffice);
+form.appendChild(labelAge);
+form.appendChild(labelSalary);
+form.appendChild(button);
+
+labelName.appendChild(inputName);
+labelPosition.appendChild(inputPosition);
+labelOffice.appendChild(select);
+labelAge.appendChild(inputAge);
+labelSalary.appendChild(inputSalary);
+
+select.appendChild(optionTokyo);
+select.appendChild(optionSingapore);
+select.appendChild(optionLondon);
+select.appendChild(optionNewYork);
+select.appendChild(optionEdinburgh);
+select.appendChild(optionSanFrancisco);
+
+function clear() {
+  inputName.value = '';
+  inputPosition.value = '';
+  select.value = 'Tokyo';
+  inputAge.value = '';
+  inputSalary.value = '';
+}
+
+const pushNotification = (posTop, posRight, title, description, type) => {
+  const div = document.createElement('div');
+  const titleOfMessage = document.createElement('h2');
+  const p = document.createElement('p');
+
+  div.classList.add('notification', `${type}`);
+  titleOfMessage.classList.add('title');
+  div.setAttribute('data-qa', 'notification');
+
+  titleOfMessage.textContent = title;
+  p.textContent = description;
+
+  div.style.top = `${posTop}px`;
+  div.style.right = `${posRight}px`;
+
+  body.appendChild(div);
+  div.appendChild(titleOfMessage);
+  div.appendChild(p);
+
+  setTimeout(() => {
+    div.style.visibility = 'hidden';
+  }, 2000);
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const nameValue = inputName.value.trim();
+  const positionValue = inputPosition.value.trim();
+  const ageValue = +inputAge.value;
+
+  if (nameValue.length < 4) {
+    pushNotification(
+      10,
+      10,
+      'Name error',
+      'Name should be more than 3 letters',
+      'error',
+    );
+  } else if (positionValue.length === 0) {
+    pushNotification(
+      10,
+      10,
+      'Position error',
+      'You enter wrong position',
+      'error',
+    );
+  } else if (ageValue < 18) {
+    pushNotification(
+      10,
+      10,
+      'Age error',
+      'Employee is still young for this job',
+      'error',
+    );
+  } else if (ageValue > 90) {
+    pushNotification(
+      10,
+      10,
+      'Age error',
+      'Employee is obsolete for this job',
+      'error',
+    );
+  } else {
+    pushNotification(
+      10,
+      10,
+      'Employee added',
+      'Congratulations new employee was added',
+      'success',
+    );
+
+    const person = {
+      name: inputName.value.trim(),
+      position: inputPosition.value.trim(),
+      office: select.value,
+      age: +inputAge.value,
+      salary: +inputSalary.value,
+    };
+
+    people.push(person);
+    updateTable();
+
+    clear();
+  }
+});
