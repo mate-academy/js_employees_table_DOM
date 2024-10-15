3c894130edeba93c4482f9d73a15c76f2166fdf6
Ivan Dovzhanyn <ivan.dovzhanyn@gmail.com>
2024-10-15 14:59:10 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,34 +4,9 @@ const tableHeaders = document.querySelectorAll('th');
 const rowsArray = Array.from(document.querySelectorAll('tbody tr'));
 const sortDirection = {};
 
-// tableHeaders.forEach((header, index) => {
-//   sortDirection[index] = true;
-
-//   header.addEventListener('click', (e) => {
-//     const sortedRows = rowsArray.sort((rowA, rowB) => {
-//       const cellA = rowA.cells[index].textContent.trim();
-//       const cellB = rowB.cells[index].textContent.trim();
-
-//       if (index === 4) {
-//         const salaryA = parseFloat(cellA.replace(/[$,]/g, ''));
-//         const salaryB = parseFloat(cellB.replace(/[$,]/g, ''));
-
-//         return sortDirection[index] ? salaryA - salaryB : salaryB - salaryA;
-//       } else {
-//         return sortDirection[index]
-//           ? cellA.localeCompare(cellB)
-//           : cellB.localeCompare(cellA);
-//       }
-//     });
-
-//     const tbody = document.querySelector('tbody');
-
-//     tbody.innerHTML = '';
-//     sortedRows.forEach((row) => tbody.appendChild(row));
-
-//     sortDirection[index] = !sortDirection[index];
-//   });
-// });
+function formatSalary(cellValue) {
+  return parseFloat(cellValue.replace(/[$,]/g, ''));
+}
 
 function sortRows(index, isAscending, rowsArr) {
   return rowsArr.sort((rowA, rowB) => {
@@ -39,8 +14,8 @@ function sortRows(index, isAscending, rowsArr) {
     const cellB = rowB.cells[index].textContent.trim();
 
     if (index === 4) {
-      const salaryA = parseFloat(cellA.replace(/[$,]/g, ''));
-      const salaryB = parseFloat(cellB.replace(/[$,]/g, ''));
+      const salaryA = formatSalary(cellA);
+      const salaryB = formatSalary(cellB);
 
       return isAscending ? salaryA - salaryB : salaryB - salaryA;
     } else {
@@ -59,7 +34,7 @@ function updateTable(sortedRows) {
   sortedRows.forEach((row) => tbody.appendChild(row));
 }
 
-function handleHeaderClick(index, arraySort, directionSort) {
+function handleSortAndUpdate(index, arraySort, directionSort) {
   const isAscending = directionSort[index];
 
   const sortedRows = sortRows(index, isAscending, arraySort);
@@ -73,7 +48,7 @@ tableHeaders.forEach((header, index) => {
   sortDirection[index] = true;
 
   header.addEventListener('click', () => {
-    handleHeaderClick(index, rowsArray, sortDirection);
+    handleSortAndUpdate(index, rowsArray, sortDirection);
   });
 });
 
@@ -86,71 +61,6 @@ rows.forEach((row) => {
     row.classList.add('active');
   });
 });
-
-// const form = document.createElement('form');
-
-// form.classList.add('new-employee-form');
-
-// const nameLabel = document.createElement('label');
-
-// nameLabel.innerHTML =
-//   'Name: <input name="name" type="text" data-qa="name" required>';
-// form.appendChild(nameLabel);
-
-// const positionLabel = document.createElement('label');
-
-// positionLabel.innerHTML =
-//   'Position: <input name="position" type="text" data-qa="position">';
-// form.appendChild(positionLabel);
-
-// const ageLabel = document.createElement('label');
-
-// ageLabel.innerHTML =
-//   'Age: <input name="age" type="number" data-qa="age" required>';
-// form.appendChild(ageLabel);
-
-// const salaryLabel = document.createElement('label');
-
-// salaryLabel.innerHTML =
-//   'Salary: <input name="salary" type="number" data-qa="salary" required>';
-// form.appendChild(salaryLabel);
-
-// const officeSelectLabel = document.createElement('label');
-
-// officeSelectLabel.innerHTML = 'Office: ';
-
-// const officeSelect = document.createElement('select');
-
-// officeSelect.name = 'office';
-// officeSelect.setAttribute('data-qa', 'office');
-
-// const cities = [
-//   'Tokyo',
-//   'Singapore',
-//   'London',
-//   'New York',
-//   'Edinburgh',
-//   'San Francisco',
-// ];
-
-// cities.forEach((city) => {
-//   const option = document.createElement('option');
-
-//   option.value = city;
-//   option.textContent = city;
-//   officeSelect.appendChild(option);
-// });
-
-// officeSelectLabel.appendChild(officeSelect);
-// form.appendChild(officeSelectLabel);
-
-// const button = document.createElement('button');
-
-// button.type = 'submit';
-// button.textContent = 'Save to table';
-// form.appendChild(button);
-
-// document.body.appendChild(form);
 
 const formHTML = `
   <form class="new-employee-form">
@@ -190,45 +100,6 @@ form.addEventListener('submit', (e) => {
   validateForm();
 });
 
-// function validateForm() {
-//   const nameEmployee = form.elements.name.value;
-//   const position = form.elements.position.value;
-//   const office = form.elements.office.value;
-//   const age = parseInt(form.elements.age.value, 10);
-//   const salary = parseFloat(form.elements.salary.value);
-
-//   if (nameEmployee.length < 4) {
-//     showNotification(
-//       'Error',
-//       'Name must be at least 4 characters long.',
-//       'error',
-//     );
-
-//     return;
-//   }
-
-//   if (!position) {
-//     showNotification('Error', 'Please choose a valid position.', 'error');
-
-//     return;
-//   }
-
-//   if (age < 18 || age > 90) {
-//     showNotification('Error', 'Age must be between 18 and 90.', 'error');
-
-//     return;
-//   }
-
-//   if (isNaN(salary) || salary <= 0) {
-//     showNotification('Error', 'Salary must be a positive number.', 'error');
-
-//     return;
-//   }
-
-//   addEmployeeToTable(nameEmployee, position, office, age, salary);
-//   showNotification('Success', 'Employee added successfully!', 'success');
-// }
-
 function validateForm() {
   const nameEmployee = form.elements.name.value;
   const position = form.elements.position.value;
@@ -249,7 +120,7 @@ function validateForm() {
     return;
   }
 
-  addEmployeeToTable(nameEmployee, position, office, age, salary);
+  addEmployeeAndResetForm(nameEmployee, position, office, age, salary);
   showNotification('Success', 'Employee added successfully!', 'success');
 }
 
@@ -273,31 +144,6 @@ function validateEmployeeDetails(nameWorker, position, age, salary) {
   return null;
 }
 
-// function showNotification(title, message, type) {
-//   const notification = document.createElement('div');
-
-//   notification.classList.add('notification');
-//   notification.classList.add(type);
-//   notification.setAttribute('data-qa', 'notification');
-
-//   const titleElement = document.createElement('span');
-
-//   titleElement.classList.add('title');
-//   titleElement.textContent = title;
-
-//   const messageElement = document.createElement('span');
-
-//   messageElement.textContent = message;
-
-//   notification.appendChild(titleElement);
-//   notification.appendChild(messageElement);
-//   document.body.appendChild(notification);
-
-//   setTimeout(() => {
-//     notification.style.visibility = 'hidden';
-//   }, 1000);
-// }
-
 function showNotification(title, message, type) {
   const notification = document.createElement('div');
 
@@ -315,7 +161,7 @@ function showNotification(title, message, type) {
   }, 1000);
 }
 
-function addEmployeeToTable(nameEmployee, position, office, age, salary) {
+function addEmployeeAndResetForm(nameEmployee, position, office, age, salary) {
   const tbody = document.querySelector('tbody');
   const newRow = document.createElement('tr');
 
