9ba014766362bbde57d74d54ac00feb66a8aff1a
Serhii-Khobotov <hobbitura@gmail.com>
2024-11-11 16:29:38 +0200

    implention employees table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,12 @@
 'use strict';
 
-//Sorting by header of column: 
+// Sorting by header of column:
 
 const body = document.querySelector('body');
 const table = document.querySelector('table');
 const headers = table.querySelectorAll('th');
 const tBody = table.querySelector('tbody');
 const cells = tBody.querySelectorAll('td');
-const td = document.createElement('td');
 let rows = [...tBody.querySelectorAll('tr')];
 const sortOrder = Array(headers.length).fill(true);
 
@@ -28,8 +27,8 @@ const setSortTable = (columnIndex) => {
     const aValue = a.cells[columnIndex].textContent.trim();
     const bValue = b.cells[columnIndex].textContent.trim();
 
-    if (+aValue) {
-      return sortOrder[columnIndex] ? +aValue - bValue : bValue - aValue;
+    if (!isNaN(aValue) && !isNaN(aValue)) {
+      return sortOrder[columnIndex] ? aValue - bValue : bValue - aValue;
     }
 
     if (columnIndex === 4) {
@@ -39,7 +38,9 @@ const setSortTable = (columnIndex) => {
       return sortOrder[columnIndex] ? aSalary - bSalary : bSalary - aSalary;
     }
 
-    return sortOrder[columnIndex] ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+    return sortOrder[columnIndex]
+      ? aValue.localeCompare(bValue)
+      : bValue.localeCompare(aValue);
   });
 
   tBody.innerHTML = '';
@@ -47,23 +48,24 @@ const setSortTable = (columnIndex) => {
   sortOrder[columnIndex] = !sortOrder[columnIndex];
 };
 
-//Selecting row:
+// Selecting row:
 
 [...tBody.rows].forEach((row) => {
   row.addEventListener('click', (e) => {
     const rowsActive = tBody.querySelectorAll('tr.active');
-    rowsActive.forEach(rowActive => {
+
+    rowsActive.forEach((rowActive) => {
       rowActive.classList.remove('active');
     });
 
     row.classList.toggle('active');
   });
+});
 
-})
-
-//Create form: 
+// Create form:
 
 const form = document.createElement('form');
+
 form.className = 'new-employee-form';
 
 form.innerHTML = `
@@ -95,14 +97,14 @@ form.innerHTML = `
 
 body.append(form);
 
-form.querySelector('button').addEventListener('click', (e) => {
-  // e.preventDefault();
+// Add new row:
 
+form.querySelector('button').addEventListener('click', () => {
   if (!checkFormFields()) {
     return;
   }
-  
-  const newRow = tBody.insertRow();
+
+  const newRow = document.createElement('tr');
 
   if (
     form.elements.name.value &&
@@ -111,25 +113,30 @@ form.querySelector('button').addEventListener('click', (e) => {
     form.elements.age.value &&
     form.elements.salary.value
   ) {
-      newRow.insertCell(0).textContent = form.elements.name.value.trim();  
-      newRow.insertCell(1).textContent = form.elements.position.value.trim();
-      newRow.insertCell(2).textContent = form.elements.office.value.trim();
-      newRow.insertCell(3).textContent = parseInt(form.elements.age.value, 10);
-      let salaryFormated = parseFloat(form.elements.salary.value.replace(/[^0-9.]/g, ''));
-      newRow.insertCell(4).textContent = `$${new Intl.NumberFormat("us-IN").format(salaryFormated)}`;
+    newRow.insertCell(0).textContent = form.elements.name.value.trim();
+    newRow.insertCell(1).textContent = form.elements.position.value.trim();
+    newRow.insertCell(2).textContent = form.elements.office.value.trim();
+    newRow.insertCell(3).textContent = parseInt(form.elements.age.value, 10);
+
+    const salaryFormatted = parseFloat(
+      form.elements.salary.value.replace(/[^0-9.]/g, ''),
+    );
+
+    newRow.insertCell(4).textContent =
+      `$${new Intl.NumberFormat('us-IN').format(salaryFormatted)}`;
+
+    showNotification(
+      'Success!',
+      'New employee is successfuly added',
+      'success',
+    );
+    form.reset();
   }
 
-  checkFormFields();
   tBody.append(newRow);
-  
-  showNotification(
-    'Success!',
-    'New employee is successfuly added',
-    'success'
-  )
-
-  form.reset();
 });
+
+// Appearance of notifications:
 
 function showNotification(title, message, type) {
   const notification = document.createElement('div');
@@ -153,11 +160,7 @@ function checkFormFields() {
   const employeeName = form.elements.name.value.trim();
 
   if (employeeName.length > 0 && employeeName.length < 4) {
-    showNotification(
-      'Error!',
-      'The minimum name length is 4 letters',
-      'error'
-    );
+    showNotification('Error!', 'The minimum name length is 4 letters', 'error');
 
     return false;
   }
@@ -165,23 +168,15 @@ function checkFormFields() {
   const employeeAge = parseInt(form.elements.age.value, 10);
 
   if (employeeAge < 18 || employeeAge > 90) {
-    showNotification(
-      'Error!',
-      'You are too young/old for this',
-      'error'
-    )
+    showNotification('Error!', 'You are too young/old for this', 'error');
 
     return false;
   }
 
-  [...form.elements].every(input => {
+  [...form.elements].every((input) => {
     if (input.tagName === 'INPUT' || input.tagName === 'SELECT') {
       if (!input.value.trim()) {
-        showNotification(
-          'Error!',
-          'Fill in all fields',
-          'error'
-        )
+        showNotification('Error!', 'Fill in all fields', 'error');
 
         return false;
       }
@@ -193,22 +188,15 @@ function checkFormFields() {
   return true;
 }
 
-//Making editable table content:
-
-cells.forEach(cell => {
+cells.forEach((cell) => {
+  const td = document.createElement('td');
   const editCell = document.createElement('input');
+
   editCell.classList.add('cell-input');
-  
+
   cell.addEventListener('dblclick', () => {
     cell.replaceWith(editCell);
   });
-
-  editCell.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-      editCell.replaceWith(td);
-      td.textContent = editCell.value;
-    }
-  })
 
   editCell.addEventListener('blur', (e) => {
     if (editCell.value !== '') {
@@ -217,5 +205,12 @@ cells.forEach(cell => {
     } else {
       editCell.replaceWith(cell);
     }
-  })
+  });
+
+  editCell.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      editCell.replaceWith(td);
+      td.textContent = editCell.value;
+    }
+  });
 });
