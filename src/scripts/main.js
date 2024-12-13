22536c1dd4a75b1a591a833ce45c24143ba1d285
Vasyl Zhyliakov <snak87@gmail.com>
2024-12-13 18:26:56 +0200

    sort

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // write code here
+const table = document.querySelector('table');
+const headers = table.tHead.querySelectorAll('th');
+const body = table.querySelector('tbody');
+
+function getSalary(query) {
+  return +query.slice(1).replaceAll(',', '');
+}
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const headerName = header.innerText;
+
+    const rows = [...body.querySelectorAll('tr')];
+
+    rows.sort((a, b) => {
+      const cellA = a.cells[index].innerText;
+      const cellB = b.cells[index].innerText;
+
+      switch (headerName) {
+        case 'Name':
+        case 'Position':
+          return cellA.localeCompare(cellB);
+
+        case 'Age':
+          return +cellA - +cellB;
+
+        case 'Salary':
+          return getSalary(cellA) - getSalary(cellB);
+      }
+    });
+
+    rows.forEach((row) => body.append(row));
+  });
+});
+
