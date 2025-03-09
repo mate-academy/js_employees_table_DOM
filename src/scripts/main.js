1895ccb72fd7b952397b8f11fc2224cce009fae7
Samarash Eugene <samarasha95@gmail.com>
2025-03-09 21:55:33 +0200

    sort function is implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+let isACS = true;
+
+table.querySelector('thead tr').addEventListener('click', (e) => {
+  const tbody = table.querySelector('tbody');
+  const rows = [...tbody.rows];
+  const title = e.target.textContent;
+  const tHead = table.querySelector('thead');
+  const headers = [...tHead.querySelectorAll('tr th')];
+  const titles = [];
+
+  headers.forEach((header) => {
+    titles.push(header.innerText);
+  });
+
+  const indexTitle = titles.indexOf(title);
+
+  rows.sort((a, b) => {
+    const aEl = a.children[indexTitle].textContent;
+    const bEl = b.children[indexTitle].textContent;
+
+    if (title === 'Salary') {
+      const aSalary = parseFloat(
+        a.children[indexTitle].textContent.split('$')[1],
+      );
+      const bSalary = parseFloat(
+        b.children[indexTitle].textContent.split('$')[1],
+      );
+
+      return isACS ? aSalary - bSalary : bSalary - aSalary;
+    }
+
+    return isACS ? aEl.localeCompare(bEl) : bEl.localeCompare(aEl);
+  });
+
+  rows.forEach((row) => {
+    tbody.append(row);
+  });
+
+  isACS = !isACS;
+});
