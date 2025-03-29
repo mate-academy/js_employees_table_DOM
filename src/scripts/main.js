5a10a9f3eefbfd12d14814c0f1d6061f23ef5aa4
Andrii Filipenko <andreyfil1337@gmail.com>
2025-03-29 15:35:59 +0200

    add double-ordered sort and row selection

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,90 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-shadow */
 'use strict';
 
-// write code here
+const head = document.querySelector('thead');
+const rowHead = head.querySelector('tr');
+const body = document.querySelector('tbody');
+const rowsBody = [...body.querySelectorAll('tr')];
+
+document.prevColumn = null;
+document.prevRow = null;
+
+function compare(element1, element2, regime, orderAscending) {
+  if (orderAscending) {
+    switch (regime) {
+      case 'Age':
+        return +element1 - +element2;
+      case 'Salary':
+        const firstFormated = +element1.replaceAll(/[^0-9]+/g, '');
+        const secondFormated = +element2.replaceAll(/[^0-9]+/g, '');
+
+        return firstFormated - secondFormated;
+      default:
+        return element1.localeCompare(element2);
+    }
+  } else {
+    switch (regime) {
+      case 'Age':
+        return +element2 - +element1;
+      case 'Salary':
+        const firstFormated = +element1.replaceAll(/[^0-9]+/g, '');
+        const secondFormated = +element2.replaceAll(/[^0-9]+/g, '');
+
+        return secondFormated - firstFormated;
+      default:
+        return element2.localeCompare(element1);
+    }
+  }
+}
+
+function prevCheck(prevElem, curentElem) {
+  if (document[prevElem] !== curentElem) {
+    if (document[prevElem] !== null) {
+      document[prevElem].removeAttribute('class');
+    }
+    document[prevElem] = curentElem;
+  }
+}
+
+function tableSort(event) {
+  const target = event.target;
+
+  if (!target.matches('th')) {
+    return;
+  }
+
+  prevCheck('prevColumn', target);
+
+  const index = target.cellIndex;
+
+  const orderAscending = !target.classList.toggle(
+    'desc',
+    !target.classList.toggle('asc'),
+  );
+
+  rowsBody.sort((row1, row2) => {
+    const firstValue = [...row1.children][index].innerText;
+    const secondValue = [...row2.children][index].innerText;
+    const regime = target.innerText;
+
+    return compare(firstValue, secondValue, regime, orderAscending);
+  });
+
+  rowsBody.forEach((row) => body.append(row));
+}
+
+function rowSelect(event) {
+  const row = event.target.closest('tr');
+
+  if (!row) {
+    return;
+  }
+
+  prevCheck('prevRow', row);
+
+  row.classList.add('active');
+}
+
+rowHead.addEventListener('click', tableSort);
+body.addEventListener('click', rowSelect);
